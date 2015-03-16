237bfc73b6e90d9becda9d5ff6a93b6105cd7543
cquest <cquest@openstreetmap.fr>
2015-03-16 14:58:34 +0100

    HTTPS support

--- adresse/app.py
+++ adresse/app.py
@@ -7,8 +7,8 @@ from .constants import DEPARTEMENTS
 app = Flask(__name__)
 
 SITE_URL = os.environ.get('SITE_URL', 'http://adresse.data.gouv.fr')
-API_URL = os.environ.get('API_URL', 'http://api.adresse.data.gouv.fr')
-TILE_URL = os.environ.get('TILE_URL', 'http://wxs.ign.fr/14repeswer1lgaj7p7yergsz/geoportail/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=GEOGRAPHICALGRIDSYSTEMS.PLANIGN&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&FORMAT=image%2Fjpeg')  # noqa
+API_URL = os.environ.get('API_URL', '//api-adresse.data.gouv.fr')
+TILE_URL = os.environ.get('TILE_URL', '//wxs.ign.fr/14repeswer1lgaj7p7yergsz/geoportail/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=GEOGRAPHICALGRIDSYSTEMS.PLANIGN&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&FORMAT=image%2Fjpeg')  # noqa
 
 
 @app.route('/')
