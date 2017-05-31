46f5fcf397c697c33c01e9d26ff351546a611922
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-05-31 11:20:13 +0200

    Migrate to Jekyll

--- adresse/default.py
+++
@@ -1,9 +0,0 @@
-import os
-
-DEBUG = True
-TESTING = True
-SITE_URL = os.environ.get('SITE_URL', '//adresse.data.gouv.fr')
-API_URL = os.environ.get('API_URL', '//api-adresse.data.gouv.fr')
-CRUD_API_URL = os.environ.get('CRUD8API_URL', '//ban-dev.data.gouv.fr')
-TILE_URL = os.environ.get('TILE_URL', '//wxs.ign.fr/14repeswer1lgaj7p7yergsz/geoportail/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=GEOGRAPHICALGRIDSYSTEMS.PLANIGN&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&FORMAT=image%2Fjpeg')  # noqa
-DELIVERY_CONTROL_FILE = None
--- adresse/run.py
+++
@@ -1,5 +0,0 @@
-import os
-
-from adresse import app
-
-app.run(debug=os.environ.get('DEBUG', True), host='0.0.0.0')
