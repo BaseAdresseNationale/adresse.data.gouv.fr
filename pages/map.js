7b3e379f8e011d5806cac3af3fbd0b642e7b0e6e
Bertrand Marron <bertrand.marron@gmail.com>
2017-10-27 15:53:19 +0200

    Add loading component to map

--- pages/map.js
+++ pages/map.js
@@ -2,7 +2,14 @@ import React from 'react'
 import dynamic from 'next/dynamic'
 import Page from '../layouts/main'
 
-const LeafletMap = dynamic(import('../components/map'), { ssr: false })
+const LeafletMap = dynamic(import('../components/map'), {
+  ssr: false,
+  loading: () => (
+    <div style={{ textAlign: 'center', paddingTop: 20 }}>
+      Chargement…
+    </div>
+  )
+})
 
 const Map = () => (
   <Page showFooter={false}>
