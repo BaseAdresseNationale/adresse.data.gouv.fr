9f4b2414295bd15642ccc3107226f5c6bd22c7e5
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-11-16 23:03:57 +0100

    Update partners

--- components/home/partners.js
+++ components/home/partners.js
@@ -3,30 +3,21 @@ import Link from 'next/link'
 import Section from '../section'
 
 export default () => (
-  <Section title='Nos partenaires' subtitle=''>
+  <Section title='En partenariat avec' subtitle=''>
     <div className='main'>
       <div>
-        <p>IGN</p>
         <Link href='http://ign.fr/'>
           <a><img className='logo' src='/static/images/logos/IGN.jpg' alt='IGN' /></a>
         </Link>
       </div>
       <div>
-        <p>La Poste</p>
         <Link href='http://www.laposte.fr/'>
           <a><img className='logo' src='/static/images/logos/laposte.jpg' alt='La Poste' /></a>
         </Link>
       </div>
       <div>
-        <p>OpenStreetMap France</p>
         <Link href='http://openstreetmap.fr/'>
           <a><img className='logo' src='/static/images/logos/OSM.png' alt='OpenStreetMap France' /></a>
-        </Link>
-      </div>
-      <div>
-        <p>SGMAP / Etalab</p>
-        <Link href='http://www.modernisation.gouv.fr/'>
-          <a><img className='logo' src='/static/images/logos/SGMAP.png' alt='OpenStreetMap France' /></a>
         </Link>
       </div>
     </div>
