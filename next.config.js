01a00ea8cf725979ca0a76cddb831ae138e8691b
Bertrand Marron <bertrand.marron@gmail.com>
2017-11-17 17:09:02 +0100

    Add bundle analyzer
    Generated with `yarn build`.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
+const {join} = require('path')
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
+
 module.exports = {
+  webpack(config, {dev}) {
+    if (!dev) {
+      config.plugins.push(new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        openAnalyzer: false,
+        reportFilename: join(__dirname, 'reports/bundles.html'),
+        defaultSizes: 'gzip'
+      }))
+    }
+
+    return config
+  },
+
   exportPathMap() {
     return {
       '/': {page: '/'},
