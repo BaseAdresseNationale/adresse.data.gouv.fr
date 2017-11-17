a7d080cedbeda32837d76377bf0984b3ff4c736b
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-11-17 16:38:28 +0100

    Fix geocoded file name

--- components/csv/geocoder.js
+++ components/csv/geocoder.js
@@ -67,7 +67,7 @@ class Geocoder extends React.Component {
   }
 
   render() {
-    const {url, status, error} = this.state
+    const {url, status, error, file} = this.state
     return (
       <div className='geocoder'>
         {!status && <Button onClick={() => this.geocodage()}>Lancer le géocadage</Button>}
@@ -78,7 +78,7 @@ class Geocoder extends React.Component {
             </div>
           </Button>}
         {url &&
-          <a href={url} download>
+          <a href={url} download={geocodedFileName(file.name)}>
             <Button>Télécharger</Button>
           </a>
         }
@@ -105,6 +105,13 @@ class Geocoder extends React.Component {
 Geocoder.propTypes = {
   file: PropTypes.object.isRequired,
   columns: PropTypes.array.isRequired
+}
+
+function geocodedFileName(originalFileName = 'file') {
+  if (originalFileName.toLowerCase().endsWith('.csv')) {
+    originalFileName = originalFileName.substr(0, originalFileName.length - 4)
+  }
+  return originalFileName + '.geocoded.csv'
 }
 
 export default Geocoder
