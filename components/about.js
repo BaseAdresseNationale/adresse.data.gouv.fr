e795400c77582757893add935d643a73f82924d3
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-11-17 11:00:08 +0100

    Reword about

--- components/about.js
+++ components/about.js
@@ -11,12 +11,11 @@ const About = () => (
         <p>La Base Adresse Nationale est une base de données qui a pour but de référencer l’intégralité des adresses du territoire français.</p>
         <p>Elle est constituée par la collaboration entre:</p>
         <ul>
-          <li>des acteurs nationaux tels que l’<a href='http://ign.fr/'>IGN</a> et <a href='http://legroupe.laposte.fr/'>La Poste</a>,</li>
+          <li>des acteurs nationaux tels que <a href='https://www.etalab.gouv.fr'>Etalab</a>, l’<a href='http://ign.fr/' title='Institut national de l’information géographique et forestière'>IGN</a>, la <a href='https://portail.dgfip.finances.gouv.fr' title='Direction générale des finances publiques'>DGFiP</a> et <a href='http://legroupe.laposte.fr/'>La Poste</a>,</li>
           <li>des acteurs locaux tels que les collectivités, les communes, les SDIS,</li>
           <li>des citoyens par exemple à travers le projet <a href='http://osm.org/'>OpenStreetMap</a> et l’association <a href='http://openstreetmap.fr/'>OpenStreetMap France</a>.</li>
         </ul>
         <p>Le projet est co-gouverné par <a href='http://www.modernisation.gouv.fr/laction-publique-se-transforme/en-ouvrant-les-donnees-publiques/administrateur-general-des-donnees-chief-data-officer-interview-henri-verdier'>l’Administrateur Général des Données</a> et le <a href='http://cnig.gouv.fr/'>Conseil National de l’Information Géographique</a>.</p>
-        <p>Le site adresse.data.gouv.fr est développé par la mission <a href='http://www.etalab.gouv.fr/'>Etalab</a> du <a href='http://www.modernisation.gouv.fr/'>Secrétariat Général à la Modernisation de l’Action Publique</a> (SGMAP).</p>
       </div>
       <div className='prose'>
         <p><a href='/faq'>Consultez la FAQ pour en savoir plus...</a></p>
