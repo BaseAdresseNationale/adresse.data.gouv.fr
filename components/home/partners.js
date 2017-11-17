a0ce734137072f822d04d23b095755ad7c917882
tmerlier <tmerlier@student.42.fr>
2017-11-17 10:58:02 +0100

    Update partners

--- components/home/partners.js
+++ components/home/partners.js
@@ -6,18 +6,23 @@ export default () => (
   <Section title='En partenariat avec' subtitle=''>
     <div className='main'>
       <div>
+        <Link href='https://portail.dgfip.finances.gouv.fr/portail/accueilIAM.pl/'>
+          <a><img className='logo' src='/static/images/logos/dgfip.png' alt='DGFIP' /></a>
+        </Link>
+      </div>
+      <div>
         <Link href='http://ign.fr/'>
           <a><img className='logo' src='/static/images/logos/IGN.jpg' alt='IGN' /></a>
         </Link>
       </div>
       <div>
-        <Link href='http://www.laposte.fr/'>
-          <a><img className='logo' src='/static/images/logos/laposte.jpg' alt='La Poste' /></a>
+        <Link href='http://openstreetmap.fr/'>
+          <a><img className='logo' src='/static/images/logos/OSM.png' alt='OpenStreetMap France' /></a>
         </Link>
       </div>
       <div>
-        <Link href='http://openstreetmap.fr/'>
-          <a><img className='logo' src='/static/images/logos/OSM.png' alt='OpenStreetMap France' /></a>
+        <Link href='http://www.laposte.fr/'>
+          <a><img className='logo' src='/static/images/logos/laposte.jpg' alt='La Poste' /></a>
         </Link>
       </div>
     </div>
@@ -28,10 +33,11 @@ export default () => (
         grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
         grid-row-gap: 0.6em;
         text-align: center;
+        align-items: center;
       }
 
       .logo {
-        width: 130px;
+        height: 130px;
       }
     `}</style>
   </Section>
