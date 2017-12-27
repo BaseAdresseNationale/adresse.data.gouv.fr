c03e13559fac2ca1db340f5836e74aea2b825256
tmerlier <tmerlier@student.42.fr>
2017-12-27 14:26:37 +0100

    Update theme colors

--- styles/colors.js
+++ styles/colors.js
@@ -1,10 +1,22 @@
 export default ({
-  blue: '#304458',
-  darkBlue: '#2c3e50',
+  blue: '#0081D5',
+  lightBlue: '#1E90DA',
+  darkBlue: '#006CB0',
+  lighterBlue: '#B4E1FA',
+
   white: '#fff',
-  almostBlack: '#404040',
-  black: '#1c1c1c',
-  lightGrey: '#e7e7e7',
-  grey: '#dadada',
-  darkGrey: '#cdcdcd'
+  lighterGrey: '#EBEFF3',
+  lightGrey: '#C9D3DF',
+  grey: '#adb9c9',
+  darkGrey: '#8393A7',
+  darkerGrey: '#53657D',
+  almostBlack: '#26353f',
+  black: '#222',
+
+  green: '#03BD5B',
+  lightGreen: '#DAF5E7',
+  orange: '#FF9947',
+  lightOrange: '#FFF0E4',
+  red: '#D1335B',
+  lightRed: '#F8E1E7'
 })
