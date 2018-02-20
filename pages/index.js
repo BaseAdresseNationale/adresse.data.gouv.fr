9c43f437991f91e798589699589f0c8779d83db2
tmerlier <tmerlier@student.42.fr>
2018-02-20 11:25:41 +0100

    Remove DataTools section

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Page from '../layouts/main'
 import Hero from '../components/hero'
 
 import Pitch from '../components/home/pitch'
-import DataAndTools from '../components/home/data-and-tools'
 import Subscribe from '../components/home/subscribe'
 
 export default () => (
@@ -12,7 +11,6 @@ export default () => (
       tagline='Un référentiel national ouvert : de l’adresse à la coordonnée géographique' />
 
     <Pitch />
-    <DataAndTools />
     <Subscribe />
   </Page>
 )
