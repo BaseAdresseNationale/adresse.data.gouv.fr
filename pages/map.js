3a8613ea2a941aeccaf0d7938aac587937862181
tmerlier <tmerlier@student.42.fr>
2018-05-31 14:55:48 +0200

    Change title and add loader to /map

--- pages/map.js
+++ pages/map.js
@@ -2,16 +2,27 @@ import React from 'react'
 import dynamic from 'next/dynamic'
 import Page from '../layouts/main'
 
+import Loader from '../components/loader'
+
 const MapSearch = dynamic(import('../components/map-search'), {
   ssr: false,
   loading: () => (
-    <div style={{textAlign: 'center', paddingTop: 20}}>
-      Chargement…
-    </div >
+    <div className={'centered'}>
+      <Loader />
+      <p>Chargement…</p>
+      <style jsx>{`
+          .centered {
+             position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+          }
+        `}</style>
+    </div>
     )
 })
 
-const title = 'Carte interactive'
+const title = 'Recherche d’adresses dans la Base Adresse Nationale'
 const description = 'Cherchez des adresses et lieux-dits, zoomez et déplacez la carte pour faire un géocodage inversé.'
 
 const Map = () => (
