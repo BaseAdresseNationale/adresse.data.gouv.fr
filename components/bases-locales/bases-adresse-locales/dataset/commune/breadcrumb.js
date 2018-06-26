91c5c8bacd88ddbdc6ea1a43645f98044ddadf7a
tmerlier <tmerlier@student.42.fr>
2018-06-26 12:29:56 +0200

    breadcrumb component

---
+++ components/bases-locales/bases-adresse-locales/dataset/commune/breadcrumb.js
@@ -0,0 +1,48 @@
+import React, {Fragment} from 'react'
+import PropTypes from 'prop-types'
+import Link from 'next/link'
+import FaAngleRight from 'react-icons/lib/fa/angle-right'
+
+import theme from '../../../../../styles/theme'
+
+class Breadcrumb extends React.Component {
+  static propTypes = {
+    current: PropTypes.string.isRequired,
+    links: PropTypes.array.isRequired
+  }
+
+  render() {
+    const {links, current} = this.props
+
+    return (
+      <div className='breadcrumb'>
+        {links.map((link, idx) => (
+          <Fragment key={link.link}>
+            <Link href={link.href}>
+              <a>{link.link}</a>
+            </Link>
+            {idx < links.length && <FaAngleRight />}
+          </Fragment>
+        ))}
+        {current}
+        <style jsx>{`
+          .breadcrumb {
+            display: flex;
+            align-items: center;
+          }
+
+          a {
+            color: ${theme.darkText};
+            text-decoration: none;
+          }
+
+          a:hover {
+            text-decoration: underline;
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
+
+export default Breadcrumb
