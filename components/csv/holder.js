4e5d03f8605d7703e4352880ec62e9e2e2aa5de5
tmerlier <tmerlier@student.42.fr>
2018-07-13 11:12:46 +0200

    fix holder style

--- components/csv/holder.js
+++ components/csv/holder.js
@@ -4,8 +4,6 @@ import Dropzone from 'react-dropzone'
 import FaFile from 'react-icons/lib/fa/file'
 import FaPlus from 'react-icons/lib/fa/plus'
 
-import theme from '../../styles/theme'
-
 // Unable to pass the css by className, maybe a react-dropzone bug ¯\_(ツ)_/¯
 const style = {
   width: '100%',
@@ -70,8 +68,8 @@ class Holder extends React.Component {
               justify-content: center;
             }
 
-            .centered .dropzone-active {
-              background: ${theme.backgroundGrey}80;
+            .centered.dropzone-active {
+              background-color: #EBEFF380;
             }
 
             .centered:hover {
