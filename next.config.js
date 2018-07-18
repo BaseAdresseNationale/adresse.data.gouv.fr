feaa19b51fd8b65eee9b9dba446f0932b84d4e82
Bertrand Marron <bertrand.marron@gmail.com>
2018-07-18 14:00:44 +0200

    Move common deps to common bundle

--- next.config.js
+++ next.config.js
@@ -1,9 +1,34 @@
 const {join} = require('path')
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
+
+const commonDependencies = [
+  '/next/',
+  '/lodash/',
+
+  '/components/hoc/',
+
+  '/pages/_error.js'
+]
 
 module.exports = {
   webpack(config, {dev, isServer}) {
     if (!dev && !isServer) {
-      const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
+      const commonPlugin = config.plugins.find(p =>
+        p.constructor.name === 'CommonsChunkPlugin' && p.filenameTemplate === 'static/commons/main-[chunkhash].js'
+      )
+
+      if (commonPlugin) {
+        const {minChunks} = commonPlugin
+
+        commonPlugin.minChunks = (module, count) => {
+          if (module.resource && commonDependencies.some(c => module.resource.includes(c))) {
+            return true
+          }
+
+          return minChunks(module, count)
+        }
+      }
+
       config.plugins.push(new BundleAnalyzerPlugin({
         analyzerMode: 'static',
         openAnalyzer: false,
