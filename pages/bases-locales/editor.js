0821b7552149d1342de0859b74fc506535f16dd6
tmerlier <tmerlier@student.42.fr>
2018-07-18 16:31:03 +0200

    remove dynamic import

--- pages/bases-locales/editor.js
+++ pages/bases-locales/editor.js
@@ -1,32 +1,14 @@
-import dynamic from 'next/dynamic'
+import React from 'react'
 import FaEdit from 'react-icons/lib/fa/edit'
 import Page from '../../layouts/main'
 
 import Head from '../../components/head'
 import Section from '../../components/section'
-import LoadingContent from '../../components/loading-content'
 
-const loadingStyle = {
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  width: '100%',
-  height: '100%'
-}
+import Editor from '../../components/bases-locales/editor'
 
 const title = 'Éditer une fichier base locale'
 const description = 'Cet outil permet de réaliser de vérifier la conformité d’un fichier BAL et d’en éditer les champs.'
-
-const Editor = dynamic(import('../../components/bases-locales/editor'), {
-  loading: () => (
-    <div style={loadingStyle}>
-      <LoadingContent loading>
-        Chargement…
-          </LoadingContent>
-    </div>
-  ),
-  ssr: false
-})
 
 export default () => (
   <Page>
