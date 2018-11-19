ff5bf400165f880505fd33976b6869cf31055038
tmerlier <tmerlier@student.42.fr>
2018-11-19 11:27:28 +0100

    Remove useless next/link

--- components/home/partners.js
+++ components/home/partners.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Link from 'next/link'
 
 import Section from '../section'
 
@@ -7,24 +6,24 @@ export default () => (
   <Section title='En partenariat avec' subtitle=''>
     <div className='main'>
       <div>
-        <Link href='https://portail.dgfip.finances.gouv.fr/portail/accueilIAM.pl/'>
+        <a href='https://portail.dgfip.finances.gouv.fr/portail/accueilIAM.pl/'>
           <a><img className='logo' src='/static/images/logos/dgfip.png' alt='DGFIP' /></a>
-        </Link>
+        </a>
       </div>
       <div>
-        <Link href='http://ign.fr/'>
+        <a href='http://ign.fr/'>
           <a><img className='logo' src='/static/images/logos/IGN.jpg' alt='IGN' /></a>
-        </Link>
+        </a>
       </div>
       <div>
-        <Link href='http://openstreetmap.fr/'>
+        <a href='http://openstreetmap.fr/'>
           <a><img className='logo' src='/static/images/logos/OSM.png' alt='OpenStreetMap France' /></a>
-        </Link>
+        </a>
       </div>
       <div>
-        <Link href='http://www.laposte.fr/'>
-          <a><img className='logo' src='/static/images/logos/laposte.jpg' alt='La Poste' /></a>
-        </Link>
+        <a href='http://www.laposte.fr/'>
+          <img className='logo' src='/static/images/logos/laposte.jpg' alt='La Poste' />
+        </a>
       </div>
     </div>
 
