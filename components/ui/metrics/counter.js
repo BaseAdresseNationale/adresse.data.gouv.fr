ae520546e943e6dc47f1c6b315c9e8625ce90be1
tmerlier <tmerlier@student.42.fr>
2018-12-12 11:49:57 +0100

    Move format function to /lib/format-numbers

--- components/ui/metrics/counter.js
+++ components/ui/metrics/counter.js
@@ -3,49 +3,7 @@ import PropTypes from 'prop-types'
 
 import theme from '../../../styles/theme'
 
-const MAX_SAFE_INTEGER = 9007199254740991
-
-function formatInteger(number) {
-  const absoluteValue = Math.abs(number)
-
-  if (absoluteValue > MAX_SAFE_INTEGER) {
-    throw new Error('Integer is out of bounds')
-  }
-
-  if (absoluteValue < 10000) {
-    return absoluteValue.toString()
-  }
-
-  const chars = String(absoluteValue).split('')
-  const uncompleteGroupSize = chars.length % 3
-  const numCompleteGroups = (chars.length - uncompleteGroupSize) / 3
-  const groups = []
-
-  if (uncompleteGroupSize > 0) {
-    groups.push(chars.slice(0, uncompleteGroupSize).join(''))
-  }
-
-  let i = 0
-  while (i < numCompleteGroups) {
-    const from = uncompleteGroupSize + (i * 3)
-    const to = from + 3
-    const group = chars.slice(from, to).join('')
-
-    groups.push(group)
-    i++
-  }
-
-  return groups.join(' ')
-}
-
-function formatPercent(percent) {
-  if (Number(percent) === percent && percent % 1 !== 0) {
-    const fixed = Number.parseFloat(percent).toFixed(2)
-    return String(fixed).replace('.', ',')
-  }
-
-  return percent
-}
+import {formatPercent, formatInteger} from '../../../lib/format-numbers'
 
 const Counter = ({value, label, unit, size, color, title}) => (
   <div>
