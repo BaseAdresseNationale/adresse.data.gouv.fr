fa2ef41d76891b099b481206b884f780f009559f
tmerlier <tmerlier@student.42.fr>
2018-12-20 15:20:48 +0100

    Use concave to more accurately detect voie

--- components/bases-locales/editor/edit-bal/map-mode/bal-map.js
+++ components/bases-locales/editor/edit-bal/map-mode/bal-map.js
@@ -1,8 +1,61 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import computeBbox from '@turf/bbox'
+import {uniqBy, orderBy} from 'lodash'
+import computeDistance from '@turf/distance'
 
 import theme from '../../../../../styles/theme'
+
+const opacityByZoom = (start, end, asc = true) => {
+  return [
+    'interpolate',
+    ['linear'],
+    ['zoom'],
+    start,
+    asc ? 0 : 1,
+    end,
+    asc ? 1 : 0
+  ]
+}
+
+function createPoint(coordinates) {
+  return {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates
+    }
+  }
+}
+
+const getNearestVoie = (map, point, zoom, margin) => {
+  let voie = null
+
+  if (map.getZoom() >= zoom) {
+    const bbox = [
+      [point.x - margin, point.y - margin],
+      [point.x + margin, point.y + margin]
+    ]
+    const features = uniqBy(map.queryRenderedFeatures(bbox, {layers: ['voies-concave']}), f => f.properties.id)
+
+    if (features.length > 0) {
+      const center = map.getCenter()
+      const centerPoint = createPoint([center.lng, center.lat])
+
+      const orderByDistance = orderBy(features, feature => {
+        const coordinates = feature.properties.coordinates.slice(0, -1).substr(1).split(',').map(str => parseFloat(str))
+        const point = createPoint(coordinates)
+        const distance = computeDistance(centerPoint, point)
+
+        return distance
+      })
+
+      voie = orderByDistance[0]
+    }
+  }
+
+  return voie
+}
 
 class BalMap extends React.Component {
   static propTypes = {
@@ -11,6 +64,9 @@ class BalMap extends React.Component {
       features: PropTypes.array.isRequired
     }).isRequired,
     addresses: PropTypes.shape({
+      features: PropTypes.array.isRequired
+    }).isRequired,
+    concaveVoie: PropTypes.shape({
       features: PropTypes.array.isRequired
     }).isRequired,
     bounds: PropTypes.object,
@@ -31,39 +87,26 @@ class BalMap extends React.Component {
 
     map.on('styledata', this.onStyleData)
 
-    map.on('mousemove', 'focus', this.onMouseMove.bind(this, 'focus'))
-    map.on('mouseleave', 'focus', this.onMouseLeave.bind(this, 'focus'))
-    map.on('click', 'focus', this.onClick.bind(this, 'focus'))
+    // Voies
+    map.on('click', 'voies', this.onClick.bind(this))
+    map.on('mouseenter', 'voies', this.onMouseEnter.bind(this))
+    map.on('mouseleave', 'voies', this.onMouseLeave.bind(this))
 
-    map.on('mousemove', 'unfocus', this.onMouseMove.bind(this, 'unfocus'))
-    map.on('mouseleave', 'unfocus', this.onMouseLeave.bind(this, 'unfocus'))
-    map.on('click', 'unfocus', this.onClick.bind(this, 'unfocus'))
+    map.on('zoomend', this.zoomEnd.bind(this))
+    map.on('dragend', this.dragEnd.bind(this))
   }
 
-  componentDidUpdate(prevProps) {
-    const {map, voies, addresses, selected} = this.props
+  componentDidUpdate() {
+    const {map, selected} = this.props
 
-    if (voies !== prevProps.voies) {
-      const source = map.getSource('voies')
-
-      source.setData(voies)
-    }
-
-    if (addresses !== prevProps.addresses) {
-      const source = map.getSource('addresses')
-
-      source.setData(addresses)
-    }
-
-    if (!selected && map.isStyleLoaded()) {
-      map.setFilter('selected', ['any'])
-      this.fitBounds()
-    }
-
-    if (selected && selected !== prevProps.selected) {
-      map.setFilter('selected', ['==', ['get', 'id'], selected.id])
-      map.setCenter(selected.positions[0].coords)
-      map.setZoom(16)
+    if (map.isStyleLoaded()) {
+      if (selected) {
+        map.setFilter('numeros', ['!=', ['get', 'codeVoie'], selected.id])
+        map.setFilter('selected-numeros', ['==', ['get', 'codeVoie'], selected.id])
+      } else {
+        map.setFilter('numeros', ['all'])
+        map.setFilter('selected-numeros', ['==', ['get', 'codeVoie'], 'empty'])
+      }
     }
   }
 
@@ -72,18 +115,18 @@ class BalMap extends React.Component {
 
     map.off('styledata', this.onStyleData)
 
-    map.off('mousemove', 'focus', this.onMouseMove.bind(this, 'focus'))
-    map.off('mouseleave', 'focus', this.onMouseLeave.bind(this, 'focus'))
-    map.off('click', 'focus', this.onClick.bind(this, 'focus'))
+    // Voies
+    map.off('click', 'voies', this.onClick.bind(this))
+    map.off('mouseenter', 'voies', this.onMouseEnter.bind(this))
+    map.off('mouseleave', 'voies', this.onMouseLeave.bind(this))
 
-    map.off('mousemove', 'unfocus', this.onMouseMove.bind(this, 'unfocus'))
-    map.off('mouseleave', 'unfocus', this.onMouseLeave.bind(this, 'unfocus'))
-    map.off('click', 'unfocus', this.onClick.bind(this, 'unfocus'))
+    map.off('zoomend', this.zoomEnd.bind(this))
+    map.off('dragend', this.dragEnd.bind(this))
   }
 
-  fitBounds = () => {
+  fitBounds = forceBounds => {
     const {map, voies, addresses, bounds} = this.props
-    const bbox = computeBbox(bounds || voies || addresses)
+    const bbox = computeBbox(forceBounds || (bounds || voies || addresses))
 
     map.fitBounds(bbox, {
       padding: 30,
@@ -97,7 +140,9 @@ class BalMap extends React.Component {
     const {map} = this.props
 
     if (map.isStyleLoaded()) {
-      if (!map.getSource('voies') || !map.getSource('addresses')) {
+      if (!map.getSource('voies') ||
+      !map.getSource('addresses') ||
+      !map.getSource('concave')) {
         this.onLoad()
       }
     } else {
@@ -106,11 +151,16 @@ class BalMap extends React.Component {
   }
 
   onLoad = () => {
-    const {map, voies, addresses} = this.props
+    const {map, voies, addresses, concaveVoie} = this.props
 
     map.addSource('voies', {
       type: 'geojson',
       data: voies
+    })
+
+    map.addSource('concave', {
+      type: 'geojson',
+      data: concaveVoie
     })
 
     map.addSource('addresses', {
@@ -119,20 +169,52 @@ class BalMap extends React.Component {
     })
 
     map.addLayer({
-      id: 'addresses-circle',
-      type: 'circle',
+      id: 'numeros',
+      type: 'symbol',
       source: 'addresses',
       paint: {
-        'circle-color': theme.colors.lightGrey,
-        'circle-radius': 5,
-        'circle-stroke-color': theme.colors.darkGrey
+        'text-color': '#fff',
+        'text-halo-color': '#000',
+        'text-halo-width': 1,
+        'text-opacity': opacityByZoom(15, 18)
+      },
+      layout: {
+        'text-field': '{numero}'
       }
     })
 
     map.addLayer({
-      id: 'cluster-count',
+      id: 'selected-numeros',
+      type: 'symbol',
+      source: 'addresses',
+      paint: {
+        'text-color': '#fff',
+        'text-halo-color': theme.primaryDark,
+        'text-halo-width': 2,
+        'text-opacity': opacityByZoom(15, 18)
+      },
+      layout: {
+        'text-field': '{numero}'
+      }
+    })
+
+    map.addLayer({
+      id: 'voies-concave',
+      type: 'fill',
+      source: 'concave',
+      paint: {
+        'fill-opacity': 0,
+        'fill-color': theme.primary
+      }
+    })
+
+    map.addLayer({
+      id: 'voies',
       type: 'symbol',
       source: 'voies',
+      paint: {
+        'text-opacity': opacityByZoom(15, 18, false)
+      },
       layout: {
         'text-field': [
           'format',
@@ -151,44 +233,93 @@ class BalMap extends React.Component {
     })
   }
 
-  onMouseMove = (layer, event) => {
-    const {map} = this.props
-    const canvas = map.getCanvas()
-    canvas.style.cursor = 'pointer'
+  zoomEnd = e => {
+    const {map, select} = this.props
 
-    const [feature] = event.features
+    if (e.originalEvent) {
+      const points = {
+        x: e.originalEvent.x,
+        y: e.originalEvent.y
+      }
 
-    if (this.highlighted) {
-      map.setFeatureState({source: 'data', id: this.highlighted}, {hover: false})
+      const voie = getNearestVoie(map, points, 14, 400)
+
+      select(voie)
+    }
+  }
+
+  dragEnd = e => {
+    const {map, select} = this.props
+
+    const points = {
+      x: e.originalEvent.x,
+      y: e.originalEvent.y
     }
 
-    this.highlighted = feature.id
-    map.setFeatureState({source: 'data', id: this.highlighted}, {hover: true})
+    const voie = getNearestVoie(map, points, 14, 400)
+
+    select(voie)
+  }
+
+  onMouseEnter = () => {
+    const {map} = this.props
+    map.getCanvas().style.cursor = 'pointer'
   }
 
   onMouseLeave = () => {
     const {map} = this.props
-    const canvas = map.getCanvas()
-    canvas.style.cursor = ''
-
-    if (this.highlighted) {
-      map.setFeatureState({source: 'data', id: this.highlighted}, {hover: false})
-    }
+    map.getCanvas().style.cursor = ''
   }
 
-  onClick = (layer, event) => {
+  onClick = event => {
     const {map, select} = this.props
-    const [feature] = event.features
-    const {codeCommune, codeVoie, numeroComplet} = feature.properties
+    const voie = event.features[0]
 
-    map.setCenter(event.lngLat)
-    map.setZoom(16)
-
-    select(codeCommune, codeVoie, numeroComplet)
+    map.flyTo({center: voie.geometry.coordinates, zoom: 18})
+    select(voie)
   }
 
   render() {
-    return null
+    return (
+      <div>
+        <div className='map-center centered' />
+
+        <style jsx>{`
+          .centered {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+          }
+
+          .map-center {
+            z-index: 1;
+            margin-top: 50px;
+            width: 40px;
+            height: 40px;
+          }
+
+          .map-center:before, .map-center:after {
+            content: "";
+            position: absolute;
+            z-index: 1;
+            background: ${theme.border};
+          }
+
+          .map-center:before {
+            left: 50%;
+            width: 2px;
+            height: 100%;
+          }
+
+          .map-center:after {
+            top: 50%;
+            height: 2px;
+            width: 100%;
+          }
+        `}</style>
+      </div>
+    )
   }
 }
 
--- components/bases-locales/editor/edit-bal/map-mode/index.js
+++ components/bases-locales/editor/edit-bal/map-mode/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import computeCentroid from '@turf/centroid'
+import computeConcave from '@turf/concave'
 import {compact, groupBy} from 'lodash'
 
 import Mapbox from '../../../../mapbox'
@@ -16,7 +17,76 @@ const createCentroid = features => {
   return computeCentroid(points)
 }
 
+const createConcave = features => {
+  const points = {
+    type: 'FeatureCollection',
+    features
+  }
+
+  return computeConcave(points)
+}
+
+const getNumerosByVoie = voies => {
+  const numerosVoies = Object.keys(voies).map(voie => {
+    let result = null
+    const numeroFeatures = voies[voie].map(numero => numero)
+
+    if (numeroFeatures.length >= 2) {
+      result = createCentroid(numeroFeatures)
+    } else {
+      result = numeroFeatures[0]
+    }
+
+    if (result) {
+      result.properties = {
+        id: voie,
+        coordinates: result.geometry.coordinates,
+        numerosCount: String(numeroFeatures.length),
+        voieName: numeroFeatures[0].properties.nomVoie
+      }
+    }
+
+    return result
+  })
+
+  return {
+    type: 'FeatureCollection',
+    features: compact(numerosVoies)
+  }
+}
+
+const getConcaveVoie = voies => {
+  const numerosVoies = Object.keys(voies).map(voie => {
+    let result = null
+    const numeroFeatures = voies[voie].map(numero => numero)
+
+    if (numeroFeatures.length >= 3) {
+      result = createConcave(numeroFeatures)
+    }
+
+    if (result) {
+      result.properties = {
+        id: voie,
+        coordinates: createCentroid(numeroFeatures).geometry.coordinates,
+        numerosCount: String(numeroFeatures.length),
+        voieName: numeroFeatures[0].properties.nomVoie
+      }
+    }
+
+    return result
+  })
+
+  return {
+    type: 'FeatureCollection',
+    features: compact(numerosVoies)
+  }
+}
+
 class MapMode extends React.Component {
+  state = {
+    voieFocused: null
+  }
+
   static propTypes = {
     addresses: PropTypes.object
   }
@@ -25,44 +95,42 @@ class MapMode extends React.Component {
     addresses: null
   }
 
+  focusVoie = voie => {
+    this.setState({voieFocused: voie ? voie.properties : null})
+  }
+
   render() {
+    const {voieFocused} = this.state
     const {addresses} = this.props
 
     const voies = groupBy(addresses.features, feature => feature.properties.codeVoie)
-    const numerosByVoie = Object.keys(voies).map(voie => {
-      let result = null
-      const numeroFeatures = voies[voie].map(numero => numero)
-
-      if (numeroFeatures.length >= 2) {
-        result = createCentroid(numeroFeatures)
-      } else {
-        result = numeroFeatures[0]
-      }
-
-      if (result) {
-        result.properties = {
-          id: voie,
-          numerosCount: String(numeroFeatures.length),
-          voieName: numeroFeatures[0].properties.nomVoie
-        }
-      }
-
-      return result
-    })
+    const numerosByVoie = getNumerosByVoie(voies)
+    const concaveVoie = getConcaveVoie(voies)
 
     return (
-      <Mapbox>
-        {map => (
-          <BalMap
-            map={map}
-            voies={{
-              type: 'FeatureCollection',
-              features: compact(numerosByVoie)
-            }}
-            addresses={addresses}
-          />
-        )}
-      </Mapbox>
+      <div>
+        <div className='head'>
+          <h1>{voieFocused ? voieFocused.voieName : ''}</h1>
+        </div>
+        <Mapbox>
+          {map => (
+            <BalMap
+              map={map}
+              voies={numerosByVoie}
+              concaveVoie={concaveVoie}
+              addresses={addresses}
+              selected={voieFocused}
+              select={this.focusVoie}
+            />
+          )}
+        </Mapbox>
+
+        <style jsx>{`
+          .head {
+            min-height: 35px;
+          }
+        `}</style>
+      </div>
     )
   }
 }
