ef36a7c299f9c5bea4ec003c78a79360d0b39f0f
Bertrand Marron <bertrand.marron@gmail.com>
2019-02-04 17:29:39 +0100

    Fix regenerator runtime issues

--- components/bases-locales/editor/index.js
+++ components/bases-locales/editor/index.js
@@ -1,5 +1,3 @@
-// eslint-disable-next-line import/no-unassigned-import
-import 'regenerator-runtime/runtime'
 import React from 'react'
 import PropTypes from 'prop-types'
 import Router from 'next/router'
--- components/bases-locales/editor/uploader.js
+++ components/bases-locales/editor/uploader.js
@@ -1,5 +1,3 @@
-// eslint-disable-next-line import/no-unassigned-import
-import 'regenerator-runtime/runtime'
 import React from 'react'
 import PropTypes from 'prop-types'
 import {validate, extractAsTree} from '@etalab/bal'
