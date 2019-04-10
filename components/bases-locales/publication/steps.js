74498892398580d873c834054a510fb6295b9aeb
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2019-04-10 21:22:50 +0200

    Improve steps wording

--- components/bases-locales/publication/steps.js
+++ components/bases-locales/publication/steps.js
@@ -8,31 +8,31 @@ const Steps = React.memo(({step}) => {
     <div className='steps'>
       <Step
         index={1}
-        title='Déposez vos adresses'
+        title='Dépôt des adresses'
         validTitle='Adresses conformes'
         validate={step > 1}
       />
 
       <Step
         index={2}
-        title='Vérification de l’habilitation'
-        validTitle='Authentifié'
+        title='Choix de la méthode d’habilitation'
+        validTitle='Méthode d’habilitation sélectionnée'
         validate={step > 2}
         disable={step < 2}
       />
 
       <Step
         index={3}
-        title='Authorisation de publication'
-        validTitle='Authorisé'
+        title='Obtention de l’habilitation'
+        validTitle='Habilitation obtenue'
         validate={step > 3}
         disable={step < 3}
       />
 
       <Step
         index={4}
-        title='Publication'
-        validTitle='Publiée'
+        title='Publication des adresses'
+        validTitle='Adresses publiées'
         validate={step > 4}
         disable={step < 4}
       />
