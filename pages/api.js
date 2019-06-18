763745fdaebcff169f3c3ebde359f09b8f0b029a
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2019-06-18 13:49:08 +0200

    Fix linting

--- pages/api.js
+++ pages/api.js
@@ -23,16 +23,16 @@ export default () => (
           </div>
           <div className='details'>
             <p>Utiliser le paramètre <b>q</b> pour faire une recherche plein texte:</p>
-            <pre><code>curl https://api-adresse.data.gouv.fr/search/?q=8+bd+du+port</code></pre>
+            <pre><code>curl &quot;https://api-adresse.data.gouv.fr/search/?q=8+bd+du+port&quot;</code></pre>
             <p>Avec <b>limit</b> on peut contrôler le nombre d’éléments retournés:</p>
-            <pre><code>curl "https://api-adresse.data.gouv.fr/search/?q=8+bd+du+port&limit=15"</code></pre>
+            <pre><code>curl &quot;https://api-adresse.data.gouv.fr/search/?q=8+bd+du+port&limit=15&quot;</code></pre>
             <p>Avec <b>autocomplete</b> on peut désactiver les traitements d’auto-complétion:</p>
-            <pre><code>curl "https://api-adresse.data.gouv.fr/search/?q=8+bd+du+port&autocomplete=0"</code></pre>
+            <pre><code>curl &quot;https://api-adresse.data.gouv.fr/search/?q=8+bd+du+port&autocomplete=0&quot;</code></pre>
             <p>Avec <b>lat</b> et <b>lon</b> on peut donner une priorité géographique:</p>
-            <pre><code>curl "https://api-adresse.data.gouv.fr/search/?q=8+bd+du+port&lat=48.789&lon=2.789"</code></pre>
+            <pre><code>curl &quot;https://api-adresse.data.gouv.fr/search/?q=8+bd+du+port&lat=48.789&lon=2.789&quot;</code></pre>
             <p>Les filtres <b>type</b>, <b>postcode</b> (code Postal) et <b>citycode</b> (code INSEE) permettent de restreindre la recherche:</p>
-            <pre><code>curl "https://api-adresse.data.gouv.fr/search/?q=8+bd+du+port&postcode=44380"</code></pre>
-            <pre><code>curl "https://api-adresse.data.gouv.fr/search/?q=paris&type=street"</code></pre>
+            <pre><code>curl &quot;https://api-adresse.data.gouv.fr/search/?q=8+bd+du+port&postcode=44380&quot;</code></pre>
+            <pre><code>curl &quot;https://api-adresse.data.gouv.fr/search/?q=paris&type=street&quot;</code></pre>
             <p>Le retour est un geojson <i>FeatureCollection</i> respectant la spec <a href='https://github.com/yohanboniface/geocodejson-spec'>GeoCodeJSON</a>:</p>
             <pre>
               <code>{`{
@@ -120,9 +120,9 @@ export default () => (
           </div>
           <div className='details'>
             <p>Les paramètres <b>lat</b> et <b>lon</b> sont obligatoires:</p>
-            <pre><code>curl "https://api-adresse.data.gouv.fr/reverse/?lon=2.37&lat=48.357"</code></pre>
+            <pre><code>curl &quot;https://api-adresse.data.gouv.fr/reverse/?lon=2.37&lat=48.357&quot;</code></pre>
             <p>Le paramètre <b>type</b> permet forcer le type de retour:</p>
-            <pre><code>curl "https://api-adresse.data.gouv.fr/reverse/?lon=2.37&lat=48.357&type=street"</code></pre>
+            <pre><code>curl &quot;https://api-adresse.data.gouv.fr/reverse/?lon=2.37&lat=48.357&type=street&quot;</code></pre>
             <p>Même format de réponse que pour le point d’entrée <a href='#search'><b>/search/</b></a>.</p>
           </div>
         </div>
@@ -134,14 +134,14 @@ export default () => (
             <p>Point d’entrée pour le géocodage de masse à partir d’un fichier CSV.</p>
           </div>
           <div className='details'>
-            <p>Le fichier csv, encodé en UTF-8 et limité actuellement à 8Mo, doit être passé via le paramètre <b>data</b>. Veuillez noter l'arobase après <b>data=</b>.</p>
+            <p>Le fichier csv, encodé en UTF-8 et limité actuellement à 8Mo, doit être passé via le paramètre <b>data</b>. Veuillez noter l’arobase après <b>data=</b>.</p>
             <pre><code>curl -X POST -F data=@path/to/file.csv https://api-adresse.data.gouv.fr/search/csv/</code></pre>
             <p>Par défaut, toutes les colonnes sont concaténées pour constituer l’adresse qui sera géocodée. On peut définir les colonnes à utiliser via de multiples paramètres <b>columns</b>:</p>
             <pre><code>curl -X POST -F data=@path/to/file.csv -F columns=voie -F columns=ville https://api-adresse.data.gouv.fr/search/csv/</code></pre>
             <p>Il est possible de préciser le nom d’une colonne contenant le <b>code INSEE</b> ou le <b>code Postal</b> pour limiter les recherches, exemple :</p>
             <pre><code>curl -X POST -F data=@path/to/file.csv -F columns=voie -F columns=ville -F citycode=ma_colonne_code_insee https://api-adresse.data.gouv.fr/search/csv/</code></pre>
             <pre><code>curl -X POST -F data=@path/to/file.csv -F columns=voie -F columns=ville -F postcode=colonne_code_postal https://api-adresse.data.gouv.fr/search/csv/</code></pre>
-            <p>On peut utiliser le fichier <a href="https://adresse.data.gouv.fr/static/exemples/search.csv">https://adresse.data.gouv.fr/static/exemples/search.csv</a> comme exemple.</p>
+            <p>On peut utiliser le fichier <a href='https://adresse.data.gouv.fr/static/exemples/search.csv'>https://adresse.data.gouv.fr/static/exemples/search.csv</a> comme exemple.</p>
             <pre><code>curl -X POST -F data=@search.csv -F columns=adresse -F columns=postcode https://api-adresse.data.gouv.fr/search/csv/</code></pre>
           </div>
         </div>
@@ -155,7 +155,7 @@ export default () => (
           <div className='details'>
             <p>Le fichier csv, encodé en UTF-8 et limité actuellement à 8Mo, doit être passé via le paramètre <b>data</b>. Il doit contenir les colonnes <b>latitude</b> (ou <em>lat</em>) et <b>longitude</b> (ou <em>lon</em> ou <em>lng</em>).</p>
             <pre><code>curl -X POST -F data=@path/to/file.csv https://api-adresse.data.gouv.fr/reverse/csv/</code></pre>
-            <p>On peut utiliser le fichier <a href="https://adresse.data.gouv.fr/static/exemples/reverse.csv">https://adresse.data.gouv.fr/static/exemples/reverse.csv</a> comme exemple.</p>
+            <p>On peut utiliser le fichier <a href='https://adresse.data.gouv.fr/static/exemples/reverse.csv'>https://adresse.data.gouv.fr/static/exemples/reverse.csv</a> comme exemple.</p>
           </div>
         </div>
       </div>
