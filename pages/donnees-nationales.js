09fd9dc77dcfbf377b8a98acb586d84572a15421
Jérôme Desboeufs <jerome.desboeufs@data.gouv.fr>
2019-07-01 18:02:22 +0200

    Fix typo

--- pages/donnees-nationales.js
+++ pages/donnees-nationales.js
@@ -19,7 +19,7 @@ export default () => (
     <Head title={title} icon={<DownloadIcon />} />
 
     <Notification fullWidth>
-      Conformément à la <a href='https://www.ccomptes.fr/sites/default/files/2019-03/20190311-refere-S2018-3287-valorisation-donnees-IGN-Meteo-France-Cerema-rep-PM.pdf'>demande du Premier Ministre</a>, la Base Adresse Nationale passera sous <strong>Licence Ouverte</strong> au plus tard le <strong style={{textDecoration: 'underline'}}>1er janvier 2020</strong>.
+      Conformément à la <a href='https://www.ccomptes.fr/sites/default/files/2019-03/20190311-refere-S2018-3287-valorisation-donnees-IGN-Meteo-France-Cerema-rep-PM.pdf'>décision du Premier Ministre</a>, la Base Adresse Nationale passera sous <strong>Licence Ouverte</strong> au plus tard le <strong style={{textDecoration: 'underline'}}>1er janvier 2020</strong>.
     </Notification>
 
     <Section title='Base Adresse Nationale' subtitle='Données de référence issues d’une coopération entre l’État, l’IGN, La Poste et OpenStreetMap France' >
