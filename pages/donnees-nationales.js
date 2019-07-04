0e0c3e2d1136707c908c6f91811c09d180391168
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2019-07-04 14:45:08 +0200

    Fix wording and add Adresses LO

--- pages/donnees-nationales.js
+++ pages/donnees-nationales.js
@@ -19,37 +19,32 @@ export default () => (
     <Head title={title} icon={<DownloadIcon />} />
 
     <Notification fullWidth>
-      Conformément à la <a href='https://www.ccomptes.fr/sites/default/files/2019-03/20190311-refere-S2018-3287-valorisation-donnees-IGN-Meteo-France-Cerema-rep-PM.pdf'>décision du Premier Ministre</a>, la Base Adresse Nationale passera sous <strong>Licence Ouverte</strong> au plus tard le <strong style={{textDecoration: 'underline'}}>1er janvier 2020</strong>.
+      Conformément à la <a href='https://www.ccomptes.fr/sites/default/files/2019-03/20190311-refere-S2018-3287-valorisation-donnees-IGN-Meteo-France-Cerema-rep-PM.pdf'>décision du Premier Ministre</a>, la Base Adresse Nationale passera intégralement sous <strong>Licence Ouverte</strong> au plus tard le <strong style={{textDecoration: 'underline'}}>1er janvier 2020</strong>.
     </Notification>
 
-    <Section title='Base Adresse Nationale' subtitle='Données de référence issues d’une coopération entre l’État, l’IGN, La Poste et OpenStreetMap France' >
-      <div style={{textAlign: 'center'}}>
-        <p>Les données listées ci-dessous sont issues du rapprochement des données et traitements adresses des partenaires de la BAN.</p>
-        <p>Elles <strong>peuvent être téléchargées gratuitement</strong>,
-        et sont <strong>disponibles sous deux formes</strong>.</p>
-      </div>
-
+    <Section>
       <div className='datasets'>
+
         <div className='dataset'>
           <h3>Produit gratuit issu de la BAN</h3>
 
           <div className='divider' />
 
           <p className='block description'>
-            Export des données de l’API de gestion de la Base Adresse Nationale.
+            Export des données de la Base Adresse Nationale.
           </p>
 
           <div className='block characteristics'>
             <h6>Caractéristiques</h6>
             <ul>
-              <li>Producteur : <strong>IGN</strong></li>
+              <li>Producteur : <strong>Partenaires BAN</strong></li>
               <li>Licence : <a href='/static/docs/licence-produit-gratuit-ban.pdf'>licence spécifique</a></li>
               <li>Fréquence de mise à jour : <strong>hebdomadaire</strong></li>
-              <li>Identifiant : <strong>identifiant technique de l’API de gestion</strong></li>
             </ul>
             <ul>
               <li><strong>1 ligne par position</strong></li>
-              <li>Table de passage identifiants IGN vers identifiants API</li>
+              <li>48 millions de positions</li>
+              <li>Table de passage identifiants IGN</li>
             </ul>
             <p><a href='/static/docs/BAN_Descriptif_Donnees.pdf'>Documentation produit</a></p>
           </div>
@@ -68,7 +63,7 @@ export default () => (
           <div className='divider' />
 
           <p className='block description'>
-            Données prêtes à l’emploi, issues de la Base Adresse Nationale, retravaillées et enrichies des <Link href='/bases-locales'><a>Bases Adresses Locales</a></Link>
+            Adresses issues de la <strong>Base Adresse Nationale</strong>, <strong>retravaillées</strong> et enrichies des <Link href='/bases-locales'>Bases Adresses Locales</Link>.
           </p>
 
           <div className='block characteristics'>
@@ -77,23 +72,51 @@ export default () => (
               <li>Producteur : <strong>Etalab</strong></li>
               <li>Licence : <a href='https://opendatacommons.org/licenses/odbl/summary/'>Open Database License (ODbL) 1.0</a></li>
               <li>Fréquence de mise à jour : <strong>hebdomadaire</strong></li>
-              <li>Identifiant : <strong>clé d’interopérabilité</strong></li>
             </ul>
             <ul>
               <li><strong>1 ligne par adresse</strong></li>
-              <li>Environ 23,4 millions d’adresses</li>
-              <li>Libellés normalisés AFNOR</li>
-              <li>&nbsp;</li>
-              <li>Utilisé par le <Link href='/csv'>géocodeur adresse.data.gouv.fr</Link> <span className='soon'>Bientôt</span></li>
-              <li>Lien avec la parcelle cadastrale <span className='soon'>Bientôt</span></li>
+              <li>24 millions d’adresses</li>
+              <li>Contient les libellés normalisés AFNOR</li>
             </ul>
+            <p><a href='https://github.com/etalab/adresse.data.gouv.fr/blob/master/static/schemas/adresses-csv.md'>Schéma des données</a></p>
           </div>
 
           <div className='block downloads'>
             <h6>Télécharger les données</h6>
             <ul>
-              <li><a href='/data/ban/adresses-odbl/latest/csv'>Format CSV<MdFileDownload /></a> [<a href='https://github.com/etalab/adresse.data.gouv.fr/blob/master/static/schemas/adresses-csv.md'>documentation</a>] <span className='new'>Amélioré !</span></li>
-              <li>Exports historiques OSM <span className='soon'>Bientôt</span></li>
+              <li><a href='/data/ban/adresses-odbl/latest/csv'>Format CSV<MdFileDownload /></a> <span className='new'>Amélioré !</span></li>
+            </ul>
+          </div>
+        </div>
+
+        <div className='dataset'>
+          <h3>Adresses LO</h3>
+
+          <div className='divider' />
+
+          <p className='block description'>
+            Adresses d’ores et déjà disponibles sous <a href='https://www.etalab.gouv.fr/licence-ouverte-open-licence'>Licence Ouverte</a>.
+          </p>
+
+          <div className='block characteristics'>
+            <h6>Caractéristiques</h6>
+            <ul>
+              <li>Producteur : <strong>Etalab</strong></li>
+              <li>Licence : <a href='https://www.etalab.gouv.fr/licence-ouverte-open-licence'>Licence Ouverte 2.0</a></li>
+              <li>Fréquence de mise à jour : <strong>hebdomadaire</strong></li>
+            </ul>
+            <ul>
+              <li><strong>1 ligne par adresse</strong></li>
+              <li>20,3 millions d’adresses</li>
+              <li>Contient les libellés normalisés AFNOR</li>
+            </ul>
+            <p><a href='https://github.com/etalab/adresse.data.gouv.fr/blob/master/static/schemas/adresses-csv.md'>Schéma des données</a></p>
+          </div>
+
+          <div className='block downloads'>
+            <h6>Télécharger les données</h6>
+            <ul>
+              <li><a href='/data/ban/adresses-lo/latest/csv'>Format CSV<MdFileDownload /></a> <span className='new'>Nouveau !</span></li>
             </ul>
           </div>
         </div>
@@ -161,6 +184,10 @@ export default () => (
           }
         }
 
+        .description {
+          font-size: 0.9em;
+        }
+
         .partners {
           display: grid;
           grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
@@ -190,10 +217,8 @@ export default () => (
         }
 
         .block {
-          display: flex;
           align-items: center;
           justify-content: center;
-          flex-direction: column;
           min-height: 40px;
           margin: 1em 0;
         }
