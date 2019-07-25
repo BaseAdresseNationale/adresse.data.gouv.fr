a56520b4b701c24d369d291b0e64b0f00916d7c2
tmerlier <tmerlier@student.42.fr>
2019-07-25 15:16:24 +0200

    Update UNSAFE React methods

--- components/explorer/table-list/index.js
+++ components/explorer/table-list/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useCallback, useEffect} from 'react'
 import PropTypes from 'prop-types'
 
 import {byTags, byText} from '../../../lib/filters'
@@ -8,67 +8,39 @@ import Title from './title'
 import TableControl from './table-control'
 import Filters from './filters'
 
-class TableList extends React.Component {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    list: PropTypes.array.isRequired,
-    headers: PropTypes.array.isRequired,
-    genItems: PropTypes.func.isRequired,
-    subtitle: PropTypes.string,
-    initialSort: PropTypes.object,
-    selected: PropTypes.object,
-    handleSelect: PropTypes.func
+const TableList = ({title, list, headers, genItems, subtitle, initialSort, selected, handleSelect}) => {
+  const [text, setText] = useState('')
+  const [selectedTags, setSelectedTags] = useState([])
+  const [sources, setSources] = useState([])
+  const [destinations, setDestinations] = useState([])
+  const [filteredList, setFilteredList] = useState([])
+
+  const getSources = sources => {
+    return getTypeByPriority(unionTypes(sources))
   }
 
-  static defaultProps = {
-    subtitle: '',
-    initialSort: null,
-    selected: null,
-    handleSelect: () => {}
+  const getDestinations = destinations => {
+    return getTypeByPriority(unionTypes(destinations))
   }
 
-  constructor(props) {
-    super(props)
+  const handleTextFilter = text => {
+    setText(text)
+  }
 
-    this.state = {
-      text: '',
-      selectedTags: [],
-      getSources: sources => getTypeByPriority(unionTypes(sources)),
-      getDestination: destination => getTypeByPriority(unionTypes(destination))
+  const handleTags = useCallback(tag => {
+    const tags = [...selectedTags]
+
+    if (tags.includes(tag)) {
+      const index = tags.indexOf(tag)
+      tags.splice(index, 1)
+    } else {
+      tags.push(tag)
     }
 
-    this.baseState = this.state
-  }
+    setSelectedTags(tags)
+  }, [selectedTags])
 
-  UNSAFE_componentWillReceiveProps() {
-    this.setState(this.baseState)
-  }
-
-  handleTextFilter = text => {
-    this.setState({
-      text
-    })
-  }
-
-  handleTags = tag => {
-    this.setState(state => {
-      const selectedTags = [...state.selectedTags]
-
-      if (selectedTags.includes(tag)) {
-        const index = selectedTags.indexOf(tag)
-        selectedTags.splice(index, 1)
-      } else {
-        selectedTags.push(tag)
-      }
-
-      return {selectedTags}
-    })
-  }
-
-  filterList = () => {
-    const {text, selectedTags} = this.state
-    const list = [...this.props.list]
-
+  const filterList = useCallback(() => {
     return list.filter(item => {
       const {sources, destination, nomVoie, numero} = item
       let tags = sources
@@ -82,52 +54,74 @@ class TableList extends React.Component {
         byText((nomVoie || numero), text) // Filter text
       )
     })
-  }
+  }, [list, selectedTags, text])
 
-  render() {
-    const {title, subtitle} = this.props
-    const {selected, headers, genItems, initialSort, handleSelect} = this.props
-    const {text, selectedTags, getSources, getDestination} = this.state
+  useEffect(() => {
+    const filteredList = filterList()
+    setFilteredList(filteredList)
+  }, [filterList, list, selectedTags])
 
-    const filteredList = this.filterList()
-    const availableSources = getSources(filteredList.map(item => item.sources))
-    const availableDestination = getDestination(filteredList.map(item => item.destination))
+  useEffect(() => {
+    if (filteredList) {
+      const sources = getSources(filteredList.map(({sources}) => sources))
+      const destinations = getDestinations(filteredList.map(({destination}) => destination))
+      setSources(sources)
+      setDestinations(destinations)
+    }
+  }, [filteredList])
 
-    return (
-      <div>
-        <Title title={title} subtitle={subtitle} />
+  return (
+    <div>
+      <Title title={title} subtitle={subtitle} />
 
-        <Filters
-          text={text}
-          onChange={this.handleTextFilter}
-          sources={availableSources}
-          destinations={availableDestination}
-          selectedTags={selectedTags}
-          onFilterTags={this.handleTags}
+      <Filters
+        text={text}
+        onChange={handleTextFilter}
+        sources={sources}
+        destinations={destinations}
+        selectedTags={selectedTags}
+        onFilterTags={handleTags}
+      />
+
+      {filteredList.length === 0 ? (
+        <div className='no-result'>Aucun résultat</div>
+      ) : (
+        <TableControl
+          list={filteredList}
+          selected={selected}
+          headers={headers}
+          genItems={genItems}
+          initialSort={initialSort}
+          handleSelect={handleSelect}
         />
+      )}
 
-        {filteredList.length === 0 ? (
-          <div className='no-result'>Aucun résultat</div>
-        ) : (
-          <TableControl
-            list={filteredList}
-            selected={selected}
-            headers={headers}
-            genItems={genItems}
-            initialSort={initialSort}
-            handleSelect={handleSelect}
-          />
-        )}
+      <style jsx>{`
+        .no-result {
+          text-align: center;
+          margin: 2em;
+        }
+      `}</style>
+    </div>
+  )
+}
 
-        <style jsx>{`
-          .no-result {
-            text-align: center;
-            margin: 2em;
-          }
-        `}</style>
-      </div>
-    )
-  }
+TableList.propTypes = {
+  title: PropTypes.string.isRequired,
+  list: PropTypes.array.isRequired,
+  headers: PropTypes.array.isRequired,
+  genItems: PropTypes.func.isRequired,
+  subtitle: PropTypes.string,
+  initialSort: PropTypes.object,
+  selected: PropTypes.object,
+  handleSelect: PropTypes.func
+}
+
+TableList.defaultProps = {
+  subtitle: '',
+  initialSort: null,
+  selected: null,
+  handleSelect: () => { }
 }
 
 export default TableList
