63a8a235e9cb1fc2bebf7537d41667ce1e80c856
Jérôme Desboeufs <jerome.desboeufs@data.gouv.fr>
2019-10-22 15:20:23 +0200

    Fix className typo

--- components/ie-warning.js
+++ components/ie-warning.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const IEWarning = () => (
-  <div className='ei-warning'>
+  <div className='ie-warning'>
     <p>
       Votre navigateur <b>Internet Explorer</b> n’est plus supporté par notre service.<br />
       <b>Nous vous recommandons d’utiliser un autre navigateur</b>
     </p>
 
     <style jsx>{`
-      .ei-warning {
+      .ie-warning {
         display: none;
         padding: 10px;
         background-color: #F00;
@@ -18,7 +18,7 @@ const IEWarning = () => (
 
       @media all and (-ms-high-contrast: none), (-ms-high-contrast: active) {
         /* IE10+ CSS */
-        .ei-warning {
+        .ie-warning {
           display: block;
         }
       }
