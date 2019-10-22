ac3b7abb2537f0bee0230bab4f78328a77f68263
Jérôme Desboeufs <jerome.desboeufs@data.gouv.fr>
2019-10-22 15:32:54 +0200

    Fix typo

--- components/ie-warning.js
+++ components/ie-warning.js
@@ -3,8 +3,8 @@ import React from 'react'
 const IEWarning = () => (
   <div className='ie-warning'>
     <p>
-      Votre navigateur <b>Internet Explorer</b> n’est plus supporté par notre service.<br />
-      <b>Nous vous recommandons d’utiliser un autre navigateur</b>
+      Votre navigateur <b>Internet Explorer</b> n’est plus supporté par ce site.<br />
+      <b>Nous vous recommandons d’utiliser un autre navigateur.</b>
     </p>
 
     <style jsx>{`
