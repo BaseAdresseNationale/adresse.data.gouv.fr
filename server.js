a3280d721e4044ac6e6d630d226e6f05fac5a506
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2019-12-15 14:39:15 +0100

    Remove unnecessary code

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const next = require('next')
 const compression = require('compression')
 
-const port = parseInt(process.env.PORT, 10) || 3000
+const port = process.env.PORT || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({dev})
 const handle = app.getRequestHandler()
