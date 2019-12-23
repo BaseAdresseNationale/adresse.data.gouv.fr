7a78b3afdab702a092b4e92d2ffc828f31c48d0b
Cédric FAMIBELLE-PRONZOLA <c.pronzola@live.fr>
2019-12-23 11:54:29 +0100

    add logo-licence-ouverte.svg

--- components/open-licence-ribbon.js
+++ components/open-licence-ribbon.js
@@ -2,9 +2,17 @@ import React from 'react'
 import theme from '../styles/theme'
 
 const OpenLicenceRibbon = () => (
-  <div className='ribbon'>
-    <p className='ribbon-content'>La Base Adresse Nationale est désormais disponible sous licence ouverte</p>
+  <div>
+    <div className='ribbon'>
+      <p className='ribbon-content'>La Base Adresse Nationale est désormais disponible sous licence ouverte</p>
+    </div>
+    <img className='ribbon-img' src='/images/logos/logo-licence-ouverte.svg' title='Licence Ouverte' alt='Licence Ouverte' />
     <style jsx>{`
+
+      .ribbon-img{
+        width: 70px;
+      }
+
       .ribbon{
         display: flex;
         background-color: rgb(0,59,128);
@@ -15,7 +23,7 @@ const OpenLicenceRibbon = () => (
         border: 1px solid ${theme.borderActive};
         box-shadow: 1px 2px 5px ${theme.primary};
       }
-      
+
       .ribbon-content{
         margin: auto;
         text-align: center; 
