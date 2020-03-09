ca23e29c3447a35121322b05d79a1d34f3d7e499
tmerlier <theophile.merliere@gmail.com>
2020-03-09 17:07:27 +0100

    search commune contact

---
+++ components/search-commune-contact/index.js
@@ -0,0 +1,75 @@
+import React, {useState, useCallback, useEffect} from 'react'
+import {debounce} from 'lodash'
+
+import {getCommunes} from '../../lib/api-geo'
+
+import SearchInput from '../search-input'
+import RenderCommune from '../search-input/render-commune'
+
+import {useInput} from '../../hooks/input'
+import Notification from '../notification'
+import {getMairie} from '../../lib/api-etablissements-public'
+import MairieContact from './mairie-contact'
+
+const SearchCommuneContact = () => {
+  const [input, setInput] = useInput('')
+  const [results, setResults] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const [commune, setCommune] = useState(null)
+  const [mairie, setMarie] = useState(null)
+
+  const fetchCommuneContact = useCallback(async () => {
+    const results = await getMairie(commune.code)
+    setMarie(results.features[0].properties)
+  }, [commune])
+
+  const handleSearch = useCallback(debounce(async input => {
+    try {
+      const results = await getCommunes({q: input})
+      setResults(results)
+    } catch (err) {
+      setError(err)
+    }
+
+    setLoading(false)
+  }, 300), [])
+
+  useEffect(() => {
+    if (input) {
+      setResults([])
+      setError(null)
+      setLoading(true)
+      handleSearch(input)
+    }
+  }, [handleSearch, input])
+
+  useEffect(() => {
+    if (commune) {
+      fetchCommuneContact()
+    }
+  }, [commune, fetchCommuneContact])
+
+  return (
+    <div>
+      <SearchInput
+        value={input}
+        results={results}
+        loading={loading}
+        placeholder='Recherchez votre commune'
+        onSelect={setCommune}
+        onSearch={setInput}
+        renderItem={RenderCommune}
+        getItemValue={commune => commune.nom} />
+
+      {error &&
+        <div className='error'>
+          <Notification message={error.message} type='error' />
+        </div>}
+
+      {mairie && <MairieContact {...mairie} />}
+    </div>
+  )
+}
+
+export default SearchCommuneContact
---
+++ components/search-commune-contact/mairie-contact.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import {Mail, Phone} from 'react-feather'
+import colors from '../../styles/colors'
+
+const getHours = time => {
+  return new Date('1970-01-01T' + time + 'Z').getHours()
+}
+
+const MairieContact = ({nom, horaires, email, telephone}) => {
+  const defaultValue = 'Non renseigné'
+
+  return (
+    <div className='mairie-contact'>
+      <div><b>{nom}</b></div>
+      <div className='infos'>
+        <div><Mail style={{marginRight: '3px'}} /> Email: {email || defaultValue}</div>
+        <div><Phone style={{marginRight: '3px'}} /> Téléphone: {telephone || defaultValue}</div>
+
+        <div className='horaires'>
+          Horaires d’ouverture :
+          <div>
+            {horaires ? (
+              horaires.map(horaire => (
+                <div key={`${horaire.du}-${horaire.au}`}>
+                  <div>
+                    <b>{horaire.du === horaire.au ? `Le ${horaire.du}` : `Du ${horaire.du} au ${horaire.au}}`}</b> :
+                  </div>
+                  <ul>
+                    {horaire.heures.map(heure => (
+                      <li key={`${heure.de}-${heure.a}`}>
+                        De <b>{getHours(heure.de)}h</b> à <b>{getHours(heure.a)}h</b>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ))
+            ) : defaultValue}
+          </div>
+        </div>
+      </div>
+
+      <style jsx>{`
+        .mairie-contact {
+          margin: 1em 0;
+          background-color: ${colors.lighterGrey};
+        }
+
+        .mairie-contact > div {
+          display: flex;
+          flex-direction: column;
+        }
+
+        .infos {
+          display: flex;
+          padding: 1em;
+        }
+
+        .infos > div {
+          display: flex;
+        }
+
+        .horaires {
+          display: flex;
+          flex-direction: column;
+        }
+
+        .horaires > div {
+          padding: 0 1em;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+MairieContact.defaultProps = {
+  email: null,
+  telephone: null,
+  horaires: null
+}
+
+MairieContact.propTypes = {
+  nom: PropTypes.string.isRequired,
+  email: PropTypes.string,
+  telephone: PropTypes.string,
+  horaires: PropTypes.array
+}
+
+export default MairieContact
