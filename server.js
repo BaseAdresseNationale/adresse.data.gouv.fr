c41a939d46eb8905147e4c34f09d2a0874c3922d
gllmr <gllmr@tutanota.com>
2020-09-25 15:28:33 +0200

    add api-users/me / login / logout / create-user

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
 const {User} = require('./lib/api-user/models')
 const {
-  connexion,
+  login,
   createUser,
   emailVerification,
   tokenRenewal,
@@ -24,10 +24,48 @@ const {
   w
 } = require('./lib/api-user/user-functions')
 
+mongoose.connect('mongodb://localhost:27017/api-user', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+})
+
 const port = process.env.PORT || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({dev})
 const handle = app.getRequestHandler()
+
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password'
+},
+async (email, password, cb) => {
+  try {
+    const user = await User.findOne({email})
+
+    if (!user) {
+      return cb(null, false, {msg: 'Cet email n’existe pas'})
+    }
+
+    if (await bcrypt.compare(password, user.password)) {
+      cb(null, user)
+    } else {
+      cb(null, false, {msg: 'Le mot de passe est incorrect'})
+    }
+  } catch (error) {
+    cb(error)
+  }
+}))
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id)
+})
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    cb(err, user)
+  })
+})
 
 app.prepare().then(() => {
   const server = express()
@@ -35,6 +73,49 @@ app.prepare().then(() => {
   if (!dev) {
     server.use(compression())
   }
+
+  server.use(express.json())
+  server.use(morgan('dev'))
+
+  server.use(
+    session({
+      secret: 'Ceci n’est pas une phrase secrète...',
+      resave: false,
+      saveUninitialized: false,
+      store: new MongoStore({mongooseConnection: mongoose.connection})
+    })
+  )
+
+  server.use(passport.initialize())
+  server.use(passport.session())
+
+  // - - - - - || Api-user : || - - - - - //
+
+  server.get('/api-users/me', showUser)
+
+  server.post('/api-users/login',
+    [
+      check('email', 'Problème dans l’adresse électronique').isEmail(),
+      check('password', 'Problème dans le mote de passe').not().isEmpty()
+    ],
+    passport.authenticate('local'),
+    login
+  )
+
+  server.post('/api-users/logout', logout)
+
+  server.post('/api-users/create-user',
+    [
+      check('name').not().isEmpty().withMessage('Le nom est nécessaire'),
+      check('email', 'Une adresse électronique valide est nécessaire').isEmail(),
+      check('password', 'Un mot de passe de cinq caractères ou plus est nécessaire').isLength({min: 5})
+    ],
+    w(createUser)
+  )
+
+  server.get('/api-users/confirmation/:token', w(emailVerification))
+
+  // - - - - - || End Api-user || - - - - - //
 
   server.get('/nous-contacter', (req, res) => {
     app.render(req, res, '/contact')
