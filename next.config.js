eda9425afb5560fa2c3376d2c801e63760ef312a
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2020-11-24 00:17:58 +0100

    Improve webpack require

--- next.config.js
+++ next.config.js
@@ -3,12 +3,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 module.exports = withBundleAnalyzer({
-  webpack(config, {dev, isServer}) {
-    // eslint-disable-next-line import/no-extraneous-dependencies
-    const {ContextReplacementPlugin} = require('webpack')
-
+  webpack(config, {webpack}) {
     config.plugins.push(
-      new ContextReplacementPlugin(/moment[/\\]locale$/, /fr/)
+      new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /fr/)
     )
 
     return config
