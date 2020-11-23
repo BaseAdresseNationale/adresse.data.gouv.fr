2d78cd1186eade8cd9a932fdd2d16b857b6ae55a
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2020-11-24 00:17:37 +0100

    Drop custom bundle optimization

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
-const {join} = require('path')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
-
-const commonModules = [
-  '/node_modules/fbjs/',
-  '/node_modules/next/',
-  '/node_modules/lodash/',
-  '/node_modules/webpack/',
-
-  '/components/hoc/',
-
-  '/pages/_error.js'
-]
 
 module.exports = withBundleAnalyzer({
   webpack(config, {dev, isServer}) {
@@ -22,15 +10,6 @@ module.exports = withBundleAnalyzer({
     config.plugins.push(
       new ContextReplacementPlugin(/moment[/\\]locale$/, /fr/)
     )
-
-    if (!dev && !isServer) {
-      config.optimization.splitChunks.cacheGroups.shared = {
-        name: 'commons',
-        test: m => m.resource && commonModules.some(c =>
-          m.resource.startsWith(join(__dirname, c))
-        )
-      }
-    }
 
     return config
   }
