94fca3340757a78ec60ee3e884c886d4b59f0e7a
tmerlier <theophile.merliere@gmail.com>
2020-11-26 11:25:33 +0100

    Remove useless getInitialProps in _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,9 @@
-import React from 'react'
 import App from 'next/app'
 
 const PIWIK_URL = process.env.NEXT_PUBLIC_PIWIK_URL
 const PIWIK_SITE_ID = process.env.NEXT_PUBLIC_PIWIK_SITE_ID
 
 class MyApp extends App {
-  static async getInitialProps({Component, ctx}) {
-    let pageProps = {}
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
-    }
-
-    return {pageProps}
-  }
-
   logPageView() {
     if (window.Piwik) {
       const tracker = window.Piwik.getTracker(`${PIWIK_URL}/piwik.php`, PIWIK_SITE_ID)
@@ -33,12 +22,6 @@ class MyApp extends App {
     setTimeout(() => {
       this.logPageView()
     }, 400)
-  }
-
-  render() {
-    const {Component, pageProps} = this.props
-
-    return <Component {...pageProps} />
   }
 }
 
