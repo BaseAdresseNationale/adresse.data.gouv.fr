0880b6f32fca15cc10192a43fa58617499aadadd
gllmr <gllmr@tutanota.com>
2020-12-02 17:51:14 +0100

    Add condition to avoid invalid .env

--- ghost/posts.js
+++ ghost/posts.js
@@ -1,27 +1,31 @@
 import GhostContentAPI from '@tryghost/content-api'
 
-const api = new GhostContentAPI({
+const apiInfos = {
   url: process.env.NEXT_PUBLIC_GHOST_URL,
   key: process.env.NEXT_PUBLIC_GHOST_KEY,
   version: 'v3'
-})
+}
 
 export async function getPosts() {
-  return api.posts
-    .browse({
-      limit: 'all'
-    })
-    .catch(err => {
-      console.error(err)
-    })
+  if (process.env.NEXT_PUBLIC_GHOST_URL && process.env.NEXT_PUBLIC_GHOST_KEY) {
+    return new GhostContentAPI(apiInfos).posts
+      .browse({
+        limit: 'all'
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
 }
 
 export async function getSinglePost(postSlug) {
-  return api.posts
-    .read({
-      slug: postSlug
-    })
-    .catch(err => {
-      console.error(err)
-    })
+  if (process.env.NEXT_PUBLIC_GHOST_URL && process.env.NEXT_PUBLIC_GHOST_KEY) {
+    return new GhostContentAPI(apiInfos).posts
+      .read({
+        slug: postSlug
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
 }
