22c999e1a94e8203fba0aaee483ca7a50ee7309b
Marine Le Pennec <lepennec.marine@gmail.com>
2021-02-11 09:49:40 +0100

    Add metadata to temoignage page

--- pages/bases-locales/temoignages.js
+++ pages/bases-locales/temoignages.js
@@ -8,10 +8,13 @@ import TemoignagesComponent from '@/components/temoignages'
 import {MessageCircle} from 'react-feather'
 
 function Temoignages() {
+  const title = 'Témoignages sur les Bases Adresses Locales'
+  const description = 'Découvrez des témoignages de terrain sur les solutions à adopter pour conserver toute la richesse de vos adresses dans une Base Adresse Locale'
+
   return (
-    <Page>
-      <Head title='Bonnes pratiques des Bases Adresses Locales' icon={<MessageCircle size={56} />} />
-      <Section title='Élus et agents apportent leurs témoignages' >
+    <Page title={title} description={description}>
+      <Head title={title} icon={<MessageCircle size={56} />} />
+      <Section title={description} >
         <TemoignagesComponent />
       </Section>
     </Page>
