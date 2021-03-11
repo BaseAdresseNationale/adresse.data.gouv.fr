10a2f32d3420db483e211bcf593d1b8a192611c8
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2021-03-11 22:35:19 +0100

    Update info incendie

--- components/incendie-ovh.js
+++ components/incendie-ovh.js
@@ -8,26 +8,11 @@ function IncendieOvh() {
         <div className='intro'>
           <h4>Incident en cours</h4>
           <div>
-            <b>
-              Plusieurs de nos outils et services sont actuellement indisponibles suite à un incendie majeur chez notre hébergeur.
-              Nous travaillons à les rétablir dans les meilleurs délais.
-            </b>
+            <p>
+              En raison d’un incendie majeur ayant affecté le centre de données hébergeant une partie de notre infrastructure, <b>l’outil “Mes Adresses” est actuellement inaccessible</b>.
+            </p>
+            <p>Il sera rétabli en <b>début de semaine prochaine</b>. Merci de votre compréhension.</p>
           </div>
-        </div>
-        <div className='bold'>Services indisponibles :</div>
-        <ul>
-          <li>Éditeur de Bases Adresses Locales mes-adresses.data.gouv.fr</li>
-          <li>Moissonnage et consolidation automatique des Bases Adresses Locales</li>
-          <li>Mise à jour en continu de la Base Adresse Nationale</li>
-        </ul>
-
-        <div className='bold'>
-          L’API Adresse (géocodage) et l’API Découpage administratif n’ont pas été impactées.
-          Les fonds de carte, indisponibles une partie de la journée du 10 mars, ont été rétablis.
-        </div>
-
-        <div className='tweeter'>
-          Suivre la résolution de l’incident : <a href='https://twitter.com/AdresseDataGouv'>https://twitter.com/AdresseDataGouv</a>
         </div>
       </div>
 
