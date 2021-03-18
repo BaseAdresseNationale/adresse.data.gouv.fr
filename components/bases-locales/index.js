b41242eb8164c62f5097bdc5c317a4c1a09d38d3
Marine Le Pennec <lepennec.marine@gmail.com>
2021-03-18 11:52:49 +0100

    Add an id to map section in bases-locales

--- components/bases-locales/index.js
+++ components/bases-locales/index.js
@@ -125,7 +125,7 @@ const BasesLocales = React.memo(({datasets, stats}) => {
       </Section>
 
       <Section title='État du déploiement des Bases Adresses Locales'>
-        <div className='map-stats-container'>
+        <div className='map-stats-container' id='anchor-dashboard'>
           <div className='stats'>
             <Counter
               value={stats.count}
