92f31c2a3c8d7af39e9a1abf86b40719b2f511c5
Marine Le Pennec <lepennec.marine@gmail.com>
2021-03-24 11:07:18 +0100

    Improve map popup css

--- components/mapbox/ban-map/popups.js
+++ components/mapbox/ban-map/popups.js
@@ -15,9 +15,14 @@ function popupNumero({numero, suffixe, lieuDitComplementNom, nomVoie, nomCommune
         <b>{numero}{suffixe} {nomVoie} {lieuDitComplementNom ? `(${lieuDitComplementNom})` : ''}</b>
         <Tag type='numero' />
       </div>
-      <div>{nomCommune} {codeCommune}</div>
-      <div>Nom : <span style={{color: nom.color}}>{nom.name}</span></div>
-      <div>Position : <span style={{color: position.color}}>{position.name}</span></div>
+      <div className='commune'>{nomCommune} - {codeCommune}</div>
+      <div className='infos-container'>
+        <div className='separator' />
+        <div className='infos'>
+          <div>Nom : <b style={{color: nom.color}}>{nom.name}</b></div>
+          <div>Position : <b style={{color: position.color}}>{position.name}</b></div>
+        </div>
+      </div>
 
       <style jsx>{`
         .heading {
@@ -25,7 +30,29 @@ function popupNumero({numero, suffixe, lieuDitComplementNom, nomVoie, nomCommune
           grid-template-columns: 3fr 1fr;
           grid-gap: .5em;
           align-items: center;
-          margin-bottom: 1em;
+        }
+
+        .commune {
+          font-style: italic;
+          color: rgba(0, 0, 0, 0.81);
+          margin-bottom: 2em;
+        }
+
+        .infos-container {
+          
+          display: grid;
+          grid-template-columns: 10px 1fr;
+        }
+
+        .separator {
+          width: 0px;
+          border: 1px solid rgba(32, 83, 179, 0.36);
+        }
+
+        .infos {
+          display: grid;
+          align-items: center;
+          grid-template-rows: 1fr 1fr;
         }
       `}</style>
     </div>
