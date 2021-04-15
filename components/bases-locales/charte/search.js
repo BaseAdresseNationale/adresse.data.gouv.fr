e993a1a9935739cca552af3d3026d13ac97cf53b
Marine Le Pennec <lepennec.marine@gmail.com>
2021-04-15 11:14:34 +0200

    Change line breaks

--- components/bases-locales/charte/search.js
+++ components/bases-locales/charte/search.js
@@ -83,12 +83,14 @@ function PartnersSearchbar() {
         getItemValue={commune => commune.nom}
       />
 
-      {commune && <Tags
-        onSelectTags={handleSelectedTags}
-        selectedTags={selectedTags}
-        filteredPartners={filteredPartners}
-        allPartners={partners}
-      />}
+      {commune && (
+        <Tags
+          onSelectTags={handleSelectedTags}
+          selectedTags={selectedTags}
+          filteredPartners={filteredPartners}
+          allPartners={partners}
+        />
+      )}
 
       {commune && (
         filteredPartners.length === 0 ? (
