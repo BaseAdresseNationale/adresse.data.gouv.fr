c5beb6c4f26b810a608de1c1295459c34ee64075
Marine Le Pennec <lepennec.marine@gmail.com>
2021-04-29 16:00:38 +0200

    Fixe space

--- pages/gerer-mes-adresses.js
+++ pages/gerer-mes-adresses.js
@@ -76,7 +76,7 @@ function GererMesAdresses() {
 
       </Section>
 
-      <Section title='Outils disponibles sur votre territoire' subtitle=' De nombreux partenaires de la Charte de la Base Adresse Locale proposent un accompagnement et/ou des outils adaptés à votre territoire' background='grey'>
+      <Section title='Outils disponibles sur votre territoire' subtitle='De nombreux partenaires de la Charte de la Base Adresse Locale proposent un accompagnement et/ou des outils adaptés à votre territoire' background='grey'>
         <div>
           <Search />
         </div>
