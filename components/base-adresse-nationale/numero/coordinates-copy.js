7057794dc1276c5025a254ceead90ce14cf0e770
Cédric FAMIBELLE-PRONZOLA <c.pronzola@live.fr>
2021-06-10 18:45:15 +0200

    Create CoordinatesCopy component

---
+++ components/base-adresse-nationale/numero/coordinates-copy.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {Clipboard} from 'react-feather'
+
+import Button from '@/components/button'
+
+function CoordinatesCopy({lat, lon, setCopyError, setIsCopySucceded, setIsCopyAvailable, isMobile}) {
+  const handleClick = async () => {
+    if (navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(`${lat}, ${lon}`)
+        setIsCopySucceded(true)
+      } catch (error) {
+        setCopyError(error)
+      }
+    } else {
+      setIsCopyAvailable(false)
+    }
+
+    const copyAlertElement = document.querySelector('#copy-alert')
+
+    if (copyAlertElement) {
+      setTimeout(() => {
+        copyAlertElement.scrollIntoView({
+          behavior: 'smooth'
+        })
+      }, 100)
+    }
+  }
+
+  return (
+    <div>
+      {isMobile ? (
+        <div className='mobile-button'>
+          <a className='text-button' href={`geo:${lat}, ${lon}`}>
+            <Button
+              type='button'
+              style={{marginRight: 1, borderRadius: '3px 0 0 3px', width: '99.5%'}}
+            >
+              Ouvrir dans le GPS
+            </Button>
+          </a>
+
+          <Button
+            type='button'
+            size='small'
+            style={{borderRadius: '0 3px 3px 0'}}
+            onClick={handleClick}
+          >
+            <Clipboard />
+          </Button>
+        </div>
+      ) : (
+        <Button
+          type='button'
+          size='small'
+          style={{width: '100%'}}
+          onClick={handleClick}
+        >
+          Copier la position GPS
+        </Button>
+      )}
+      <style jsx>{`
+        .mobile-button {
+          display: flex;
+          justify-content: space-between;
+        }
+
+        .text-button {
+          flex-grow: 1
+        }
+      `}</style>
+    </div>
+  )
+}
+
+CoordinatesCopy.defaultProps = {
+  isMobile: false
+}
+
+CoordinatesCopy.propTypes = {
+  lat: PropTypes.number.isRequired,
+  lon: PropTypes.number.isRequired,
+  setCopyError: PropTypes.func.isRequired,
+  setIsCopySucceded: PropTypes.func.isRequired,
+  setIsCopyAvailable: PropTypes.func.isRequired,
+  isMobile: PropTypes.bool
+}
+
+export default CoordinatesCopy
