6a6b37a43b5b412ac9b008db202adf385f4f6a63
Cédric FAMIBELLE-PRONZOLA <c.pronzola@live.fr>
2021-06-10 18:46:28 +0200

    Add CoordinatesCopy to Numero and use Notification

--- components/base-adresse-nationale/numero/index.js
+++ components/base-adresse-nationale/numero/index.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 import Link from 'next/link'
 import colors from '@/styles/colors'
 
+import Notification from '@/components/notification'
 import {getNumeroComplet} from '@/lib/ban'
 
 import Certification from '../certification'
 import ParcellesList from '../parcelles-list'
 
-function Numero({numero, suffixe, lieuDitComplementNom, sourcePosition, commune, voie, libelleAcheminement, parcelles, codePostal, cleInterop}) {
+import CoordinateCopy from './coordinates-copy'
+
+function Numero({numero, suffixe, lieuDitComplementNom, sourcePosition, commune, voie, libelleAcheminement, parcelles, codePostal, cleInterop, lat, lon, isMobile}) {
+  const [copyError, setCopyError] = useState(null)
+  const [isCopyAvailable, setIsCopyAvailable] = useState(true)
+  const [isCopySucceded, setIsCopySucceded] = useState(false)
+
   return (
     <>
       <div className='heading'>
@@ -35,6 +42,36 @@ function Numero({numero, suffixe, lieuDitComplementNom, sourcePosition, commune,
         <div>Parcelles cadastrales : <ParcellesList parcelles={parcelles} /></div>
       </div>
 
+      <CoordinateCopy
+        isMobile={isMobile}
+        lat={lat}
+        lon={lon}
+        setCopyError={setCopyError}
+        setIsCopySucceded={setIsCopySucceded}
+        setIsCopyAvailable={setIsCopyAvailable}
+      />
+
+      <div id='copy-alert' className='copy-alert'>
+        {isCopySucceded && (
+          <Notification type='success' message='Coordonnées GPS copiées' onClose={() => setIsCopySucceded(false)} />
+        )}
+
+        {copyError && (
+          <Notification type='error' message={copyError} onClose={() => setCopyError(null)} />
+        )}
+
+        {!isCopyAvailable && (
+          <Notification type='warning' onClose={() => setIsCopyAvailable(true)}>
+            <p>
+              Votre navigateur est incompatible avec la copie des coordonnées GPS.<br />{}
+              Vous les trouverez ci-dessous :
+            </p>
+            <p>
+              <b>{lat}, {lon}</b>
+            </p>
+          </Notification>
+        )}
+      </div>
       <style jsx>{`
         .heading {
           display: grid;
@@ -69,9 +106,17 @@ function Numero({numero, suffixe, lieuDitComplementNom, sourcePosition, commune,
           font-style: italic;
           color: ${colors.almostBlack};
         }
+
+        .copy-alert {
+          margin-top: 1em;
+        }
       `}</style>
     </>
   )
+}
+
+Numero.defaultProps = {
+  isMobile: false
 }
 
 Numero.propTypes = {
@@ -93,7 +138,10 @@ Numero.propTypes = {
   }),
   libelleAcheminement: PropTypes.string.isRequired,
   codePostal: PropTypes.string.isRequired,
-  cleInterop: PropTypes.string.isRequired
+  cleInterop: PropTypes.string.isRequired,
+  lat: PropTypes.number.isRequired,
+  lon: PropTypes.number.isRequired,
+  isMobile: PropTypes.bool
 }
 
 export default Numero
