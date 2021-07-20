323fa96663c1ddc48aef5f74277f3b533ab30c3e
Cédric FAMIBELLE-PRONZOLA <c.pronzola@live.fr>
2021-07-20 10:14:14 +0200

    Use Tooltip component in Certification

--- components/base-adresse-nationale/certification.js
+++ components/base-adresse-nationale/certification.js
@@ -4,76 +4,38 @@ import {CheckCircle, XOctagon} from 'react-feather'
 
 import theme from '@/styles/theme'
 
-function Certification({isCertified, certifiedMessage, notCertifiedMessage, iconSize}) {
+import Tooltip from './tooltip'
+
+function Certification({isCertified, certifiedMessage, notCertifiedMessage, iconSize, tooltipDirection}) {
   return (
-    <>
+    <div>
       {isCertified ? (
-        <div className='certified tooltip'>
+        <Tooltip isCertified message={certifiedMessage} direction={tooltipDirection}>
           <CheckCircle style={{marginLeft: '.5em', verticalAlign: 'sub'}} color={theme.successBorder} size={iconSize} />
-          <span className='tooltip-text'>{certifiedMessage}</span>
-        </div >
+        </Tooltip>
       ) : (
-        <div className='certified tooltip'>
+        <Tooltip message={notCertifiedMessage} direction={tooltipDirection}>
           <XOctagon style={{marginLeft: '.5em', verticalAlign: 'sub'}} color={theme.warningBorder} size={iconSize} />
-          <span className='tooltip-text'>{notCertifiedMessage}</span>
-        </div>
+        </Tooltip>
       )}
-      <style jsx>{`
-        .certified {
-          display: flex;
-          align-items: center;
-        }
-
-        .tooltip {
-          position: relative;
-          display: inline-block;
-          cursor: help;
-        }
-
-        .tooltip .tooltip-text {
-          visibility: hidden;
-          width: 154px;
-          background-color: #000000d1;
-          color: #fff;
-          text-align: center;
-          border-radius: 4px;
-          padding: 5px 0;
-          left: -106px;
-          top: 60px;
-
-          /* Position the tooltip */
-          position: absolute;
-          z-index: 1;
-        }
-
-        .tooltip .tooltip-text::after {
-          content: " ";
-          position: absolute;
-          bottom: 100%;  /* At the top of the tooltip */
-          left: 82%;
-          margin-left: -5px;
-          border-width: 5px;
-          border-style: solid;
-          border-color: transparent transparent black transparent;
-        }
-
-        .tooltip:hover .tooltip-text {
-          visibility: visible;
-        }
-      `}</style>
-    </>
+    </div>
   )
 }
 
 Certification.defaultProps = {
-  iconSize: 34
+  iconSize: 34,
+  tooltipDirection: 'bottom'
 }
 
 Certification.propTypes = {
   isCertified: PropTypes.bool.isRequired,
   certifiedMessage: PropTypes.string.isRequired,
   notCertifiedMessage: PropTypes.string.isRequired,
-  iconSize: PropTypes.number
+  iconSize: PropTypes.number,
+  tooltipDirection: PropTypes.oneOf([
+    'bottom',
+    'left'
+  ])
 }
 
 export default Certification
