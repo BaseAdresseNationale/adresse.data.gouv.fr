973f7378e4bda862867a586dbbcaaafa26c4d704
Cédric FAMIBELLE-PRONZOLA <c.pronzola@live.fr>
2021-07-20 10:15:29 +0200

    Add Certification to Numero component

--- components/base-adresse-nationale/voie/numero.js
+++ components/base-adresse-nationale/voie/numero.js
@@ -5,19 +5,37 @@ import Link from 'next/link'
 import theme from '@/styles/theme'
 
 import {getNumeroComplet} from '@/lib/ban'
+import Certification from '../certification'
 
-function Numero({id, numero, suffixe, lieuDitComplementNom}) {
+function Numero({id, numero, suffixe, lieuDitComplementNom, certifie}) {
   return (
     <Link href={`/base-adresse-nationale?id=${id}`} as={`/base-adresse-nationale/${id}`}>
       <a>
         <div className='numero'>
           <b>{getNumeroComplet({numero, suffixe})}</b> {lieuDitComplementNom}
         </div>
+        <div className='certification'>
+          <Certification
+            isCertified={certifie}
+            certifiedMessage='Ce numéro est certifié par la commune'
+            notCertifiedMessage='Ce numéro n’est pas certifié par la commune'
+            iconSize={18}
+            tooltipDirection='left'
+          />
+        </div>
 
         <style jsx>{`
           a {
             text-decoration: none;
             color: ${theme.darkText};
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+          }
+
+          a:hover {
+            background-color: ${theme.primary};
+            color: #fff;
           }
 
           .numero {
@@ -29,9 +47,8 @@ function Numero({id, numero, suffixe, lieuDitComplementNom}) {
             cursor: pointer;
           }
 
-          .numero:hover {
-            background-color: ${theme.primary};
-            color: #fff;
+          .certification {
+            margin-right: 0.2em;
           }
           `}</style>
       </a>
@@ -48,7 +65,8 @@ Numero.propTypes = {
   id: PropTypes.string.isRequired,
   suffixe: PropTypes.string,
   numero: PropTypes.string.isRequired,
-  lieuDitComplementNom: PropTypes.string
+  lieuDitComplementNom: PropTypes.string,
+  certifie: PropTypes.bool.isRequired
 }
 
 export default Numero
