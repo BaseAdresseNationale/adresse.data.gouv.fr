ac55c5c4c424fe2d31bfc920e46a7e69fcc163e1
gllmr <guillaume@livingdata.co>
2021-09-21 17:31:57 +0200

    Path: class to function

--- components/api-doc/technical-doc/path.js
+++ components/api-doc/technical-doc/path.js
@@ -17,65 +17,62 @@ const getMethodColor = method => {
   }
 }
 
-class Path extends React.Component {
-  static defaultProps = {
-    method: 'get',
-    body: null
-  }
-
-  static propTypes = {
-    name: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    params: PropTypes.array.isRequired,
-    method: PropTypes.oneOf(['get', 'post', 'put']),
-    body: PropTypes.object
-  }
-
-  render() {
-    const {name, description, params, method, body} = this.props
-    const title = (
-      <div className='get'>
-        <div className='method'>{method}</div>
-        <div className='description'>
-          <div><b>{name}</b></div>
-          <div>{description}</div>
-        </div>
-        <style jsx>{`
-          .get {
-            display: flex;
-            align-items: center;
-          }
-
-          .method {
-            background-color: ${getMethodColor(method)};
-            color: ${theme.colors.white};
-            font-weight: 600;
-            border-radius: 3px;
-            padding: 0.3em 1em;
-            margin-right: 1em;
-            width: 75px;
-            text-align: center;
-            text-transform: uppercase;
-          }
-
-          .description {
-            width: 100%;
-            display: flex;
-            justify-content: space-between;
-            flex-wrap: wrap;
-            font-weight: 500;
-          }
-          `}</style>
+function Path({name, description, params, method, body}) {
+  const title = (
+    <div className='get'>
+      <div className='method'>{method}</div>
+      <div className='description'>
+        <div><b>{name}</b></div>
+        <div>{description}</div>
       </div>
-    )
+      <style jsx>{`
+        .get {
+          display: flex;
+          align-items: center;
+        }
 
-    return (
-      <ExpandableMenu title={title}>
-        <ParamsTable params={params} />
-        {body && <ParamsTable label='body' params={[body]} />}
-      </ExpandableMenu>
-    )
-  }
+        .method {
+          background-color: ${getMethodColor(method)};
+          color: ${theme.colors.white};
+          font-weight: 600;
+          border-radius: 3px;
+          padding: 0.3em 1em;
+          margin-right: 1em;
+          width: 75px;
+          text-align: center;
+          text-transform: uppercase;
+        }
+
+        .description {
+          width: 100%;
+          display: flex;
+          justify-content: space-between;
+          flex-wrap: wrap;
+          font-weight: 500;
+        }
+        `}</style>
+    </div>
+  )
+
+  return (
+    <ExpandableMenu title={title}>
+      <ParamsTable params={params} />
+      {body && <ParamsTable label='body' params={[body]} />}
+    </ExpandableMenu>
+  )
+}
+
+Path.defaultProps = {
+  method: 'get',
+  body: null
+}
+
+Path.propTypes = {
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  params: PropTypes.array.isRequired,
+  method: PropTypes.oneOf(['get', 'post', 'put']),
+  body: PropTypes.object
 }
 
 export default Path
