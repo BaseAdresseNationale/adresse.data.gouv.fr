52e9f0b7f4056aea8d83bce7f5755abecd8f67be
gllmr <guillaume@livingdata.co>
2021-09-22 10:36:12 +0200

    Tuto: class to function + renanming + cleaning

--- components/api-doc/tuto/index.js
+++ components/api-doc/tuto/index.js
@@ -1,92 +1,77 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import copy from 'copy-to-clipboard'
 
 import Result from './result'
-
 import Presentation from './presentation'
 
-class Tuto extends React.Component {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    icon: PropTypes.element.isRequired,
-    exemple: PropTypes.string.isRequired,
-    side: PropTypes.PropTypes.oneOf([
-      'right', 'left'
-    ]).isRequired,
-    results: PropTypes.oneOfType([
-      PropTypes.array,
-      PropTypes.object
-    ]),
-    tips: PropTypes.string,
-    warning: PropTypes.string,
-    isLoading: PropTypes.bool,
-    children: PropTypes.node
-  }
+function Tuto({title, description, icon, example, results, tips, warning, side, isLoading, children}) {
+  return (
+    <div className='tuto-container'>
 
-  static defaultProps = {
-    tips: '',
-    warning: '',
-    results: null,
-    isLoading: true,
-    children: null
-  }
-
-  constructor(props) {
-    super(props)
-    this.copyToClipboard = this.copyToClipboard.bind(this)
-  }
-
-  copyToClipboard(value) {
-    copy(value)
-  }
-
-  render() {
-    const {title, description, icon, exemple, results, tips, warning, side, isLoading, children} = this.props
-
-    return (
-      <div className='tuto-container'>
-
-        <div className='presentation'>
-          <Presentation title={title} description={description} icon={icon} tips={tips} warning={warning}>
-            {children}
-          </Presentation>
-        </div>
-
-        <div className='result'>
-          <Result exemple={exemple} results={results} side={side} isLoading={isLoading} />
-        </div>
-
-        <style jsx>{`
-          .tuto-container {
-            display: flex;
-            justify-content: space-around;
-            flex-direction: ${side === 'right' ? 'row-reverse' : 'row'};
-          }
-
-          .result {
-            width: 50%;
-          }
-
-          .presentation {
-            width: 40%;
-            margin-right: 1em;
-          }
-
-          @media (max-width: 768px) {
-            .tuto-container {
-              flex-direction: column;
-            }
-
-            .tuto-container div {
-              width: 100%;
-            }
-          }
-        `}</style>
+      <div className='presentation'>
+        <Presentation title={title} description={description} icon={icon} tips={tips} warning={warning}>
+          {children}
+        </Presentation>
       </div>
-    )
-  }
+
+      <div className='result'>
+        <Result example={example} results={results} side={side} isLoading={isLoading} />
+      </div>
+
+      <style jsx>{`
+        .tuto-container {
+          display: flex;
+          justify-content: space-around;
+          flex-direction: ${side === 'right' ? 'row-reverse' : 'row'};
+        }
+
+        .result {
+          width: 50%;
+        }
+
+        .presentation {
+          width: 40%;
+          margin-right: 1em;
+        }
+
+        @media (max-width: 768px) {
+          .tuto-container {
+            flex-direction: column;
+          }
+
+          .tuto-container div {
+            width: 100%;
+          }
+        }
+      `}</style>
+    </div>
+  )
+}
+
+Tuto.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  icon: PropTypes.element.isRequired,
+  example: PropTypes.string.isRequired,
+  side: PropTypes.PropTypes.oneOf([
+    'right', 'left'
+  ]).isRequired,
+  results: PropTypes.oneOfType([
+    PropTypes.array,
+    PropTypes.object
+  ]),
+  tips: PropTypes.string,
+  warning: PropTypes.string,
+  isLoading: PropTypes.bool,
+  children: PropTypes.node
+}
+
+Tuto.defaultProps = {
+  tips: '',
+  warning: '',
+  results: null,
+  isLoading: true,
+  children: null
 }
 
 export default Tuto
--- components/api-doc/tuto/result.js
+++ components/api-doc/tuto/result.js
@@ -7,79 +7,45 @@ import theme from '../../../styles/theme'
 import Code from '../code'
 
 import Loader from '../../loader'
-import InputExemple from './input-exemple'
+import InputExample from './input-example'
 
-class Result extends React.Component {
-  static propTypes = {
-    exemple: PropTypes.string.isRequired,
-    results: PropTypes.oneOfType([
-      PropTypes.array,
-      PropTypes.object
-    ]),
-    isLoading: PropTypes.bool
-  }
+function Result({example, results, isLoading}) {
+  return (
+    <div className='result-container'>
+      <InputExample value={example} copy={copy} />
+      {isLoading ?
+        <div className='loading-pre'>
+          <div className='loading-code'>
+            <Loader />
+          </div>
+        </div> :
+        <Code code={JSON.stringify(results, null, 2)} />}
+      <style jsx>{`
+        .result-container {
+          display: flex;
+          flex-direction: column;
+        }
 
-  static defaultProps = {
-    isLoading: true,
-    results: null
-  }
+        .loading-pre {
+          background: ${theme.colors.white};
+          border: 1px solid ${theme.backgroundGrey};
+          border-radius: 5px;
+          width: 100%;
+          padding: 1em;
+          margin-bottom: 1em;
+        }
 
-  constructor(props) {
-    super(props)
-    this.copyToClipboard = this.copyToClipboard.bind(this)
-    this.handleDisplay = this.handleDisplay.bind(this)
-  }
-
-  copyToClipboard(value) {
-    copy(value)
-  }
-
-  handleDisplay() {
-    this.setState(state => ({
-      raw: !state.raw
-    }))
-  }
-
-  render() {
-    const {exemple, results, isLoading} = this.props
-
-    return (
-      <div className='result-container'>
-        <InputExemple value={exemple} copy={this.copyToClipboard} />
-        {isLoading ?
-          <div className='loading-pre'>
-            <div className='loading-code'>
-              <Loader />
-            </div>
-          </div> :
-          <Code code={JSON.stringify(results, null, 2)} />}
-        <style jsx>{`
-          .result-container {
-            display: flex;
-            flex-direction: column;
-          }
-
-          .loading-pre {
-            background: ${theme.colors.white};
-            border: 1px solid ${theme.backgroundGrey};
-            border-radius: 5px;
-            width: 100%;
-            padding: 1em;
-            margin-bottom: 1em;
-          }
-
-          .loading-code {
-            width: 100%;
-            height: 360px;
-            background: ${theme.backgroundGrey};
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-          }
-        `}</style>
-      </div>
-    )
-  }
+        .loading-code {
+          width: 100%;
+          height: 360px;
+          background: ${theme.backgroundGrey};
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+        }
+      `}</style>
+    </div>
+  )
 }
 
 export default Result
