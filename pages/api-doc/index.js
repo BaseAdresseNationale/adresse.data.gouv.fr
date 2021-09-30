d0917405d622a16fd67884ec203b2e171b12f93c
gllmr <guillaume@livingdata.co>
2021-09-30 15:33:50 +0200

    Typo in page title

--- pages/api-doc/index.js
+++ pages/api-doc/index.js
@@ -6,7 +6,7 @@ import Page from '@/layouts/main'
 import Head from '@/components/head'
 import Apis from '@/components/apis'
 
-const title = 'Documentation des APIs'
+const title = 'Documentation des API'
 const description = 'Découvrez l’API Adresse, l’API Base Adresse Locale et l’API de dépôt'
 
 function ApiPage() {
