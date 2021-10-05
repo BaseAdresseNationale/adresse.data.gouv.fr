a54071540b51acec6fa426aef102adf1295e1b30
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2021-10-06 00:13:38 +0200

    Remove polyfill.io

--- pages/_document.js
+++ pages/_document.js
@@ -22,7 +22,6 @@ class MyDocument extends Document {
 
         <body>
           <Main />
-          <script src='https://cdn.polyfill.io/v2/polyfill.min.js?features=Array.prototype.includes,modernizr:es6string,modernizr:es6array,Promise,fetch' />
           {PIWIK_URL && PIWIK_SITE_ID && <script defer async src={`${PIWIK_URL}/piwik.js`} />}
           <NextScript />
         </body>
