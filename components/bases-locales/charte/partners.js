27787888cb5bd5e61b607ffe2f3e77b36b9e8a1c
Marine Le Pennec <lepennec.marine@gmail.com>
2021-10-14 11:30:30 +0200

    Fix partners component content

--- components/bases-locales/charte/partners.js
+++ components/bases-locales/charte/partners.js
@@ -1,170 +1,49 @@
-import {useState, useCallback, useEffect} from 'react'
-import {debounce, intersection} from 'lodash'
+import Partner from '@/components/bases-locales/charte/partner'
 
-import {getCommunes, getByCode} from '@/lib/api-geo'
-import theme from '@/styles/theme'
+import colors from '@/styles/colors'
+import allPartners from 'partners.json'
 
-import partners from 'partners.json'
-
-import SearchedPartners from '@/components/bases-locales/charte/searched-partners'
-import SearchInput from '@/components/search-input'
-import Tags from '@/components/bases-locales/charte/tags'
-import RenderCommune from '@/components/search-input/render-commune'
-
-function PartnersSearchbar() {
-  const [input, setInput] = useState('')
-  const [results, setResults] = useState([])
-  const [commune, setCommune] = useState(null)
-  const [filteredPartners, setFilteredPartners] = useState([])
-  const [selectedTags, setSelectedTags] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState(null)
-
-  const [isCompaniesVisible, setIsCompaniesVisible] = useState(false)
-
-  const companyPartners = filteredPartners.filter(partner => partner.isCompany)
-  const organizationPartners = filteredPartners.filter(partner => !partner.isCompany)
-
-  const onCompaniesVisible = () => {
-    setIsCompaniesVisible(!isCompaniesVisible)
-  }
-
-  const handleSelectedTags = tag => {
-    setSelectedTags(prevTags => {
-      return selectedTags.includes(tag) ?
-        selectedTags.filter(selectedTag => selectedTag !== tag) :
-        [...prevTags, tag]
-    })
-  }
-
-  const getAvailablePartners = useCallback((communeCodeDepartement, tags) => {
-    const filteredByPerimeter = partners.filter(({codeDepartement, isPerimeterFrance}) => (codeDepartement.includes(communeCodeDepartement) || isPerimeterFrance))
-    const filteredByTags = filteredByPerimeter.filter(({services}) => intersection(tags, services).length === tags.length)
-
-    const organizationsLength = filteredByTags.filter(partner => !partner.isCompany).length
-    if (organizationsLength === 0) {
-      setIsCompaniesVisible(true)
-    } else {
-      setIsCompaniesVisible(false)
-    }
-
-    return filteredByTags.sort((a, b) => {
-      return a.echelon - b.echelon
-    })
-  }, [])
-
-  useEffect(() => {
-    if (commune) {
-      const availablePartners = getAvailablePartners(commune.codeDepartement, selectedTags)
-      setFilteredPartners(availablePartners)
-    } else {
-      setFilteredPartners([])
-    }
-  }, [selectedTags, getAvailablePartners, commune])
-
-  useEffect(() => {
-    setInput(commune ? commune.nom : '')
-  }, [commune])
-
-  const handleSearch = useCallback(debounce(async input => {
-    setError(null)
-    setIsLoading(true)
-    try {
-      const inputToNumber = Number.parseInt(input, 10)
-      if (Number.isNaN(inputToNumber)) {
-        setResults(await getCommunes({q: input, limit: 5, boost: 'population'}))
-      } else {
-        setResults(await getByCode({postalCode: input}))
-      }
-    } catch {
-      setError('Impossible d’effectuer la recherche, veuillez rééssayer ultérieurement')
-    }
-
-    setIsLoading(false)
-  }, 300), [])
-
-  useEffect(() => {
-    if (input) {
-      handleSearch(input)
-    } else {
-      setCommune(null)
-      setFilteredPartners([])
-      setSelectedTags([])
-    }
-  }, [handleSearch, input])
+function Partners() {
+  const companyPartners = allPartners.filter(partner => partner.isCompany === true)
+  const partners = allPartners.filter(partner => partner.isCompany === false)
 
   return (
-    <div style={{marginTop: '2em'}}>
-      <p className='searchbar-label'>Recherchez un partenaire de la Charte de la Base Adresse Locale sur votre territoire</p>
-      <SearchInput
-        value={input}
-        results={results}
-        isLoading={isLoading}
-        placeholder='Recherchez une commune'
-        onSelect={setCommune}
-        onSearch={setInput}
-        renderItem={RenderCommune}
-        getItemValue={commune => commune.nom}
-      />
-
-      {commune && (
-        <Tags
-          onSelectTags={handleSelectedTags}
-          selectedTags={selectedTags}
-          filteredPartners={filteredPartners}
-          allPartners={partners}
-        />
-      )}
-
-      {commune && (
-        filteredPartners.length === 0 ? (
-          <div className='results'>Aucune structure n’a été trouvée sur votre territoire </div>
-        ) : (
-          <div className='results'>
-            <div> <b>{filteredPartners.length} </b>
-              {filteredPartners.length === 1 ?
-                'partenaire de la Charte de la Base Adresse Locale trouvé sur votre territoire' :
-                'partenaires de la Charte de la Base Adresse Locale trouvés sur votre territoire'}
-            </div>
-
-            <div className='organizations'>
-              <div><b>{organizationPartners.length}</b> {`${organizationPartners.length > 1 ? 'organismes' : 'organisme'} de mutualisation`}</div>
-              <div><b>{companyPartners.length}</b> {companyPartners.length > 1 ? 'entreprises' : 'entreprises'}</div>
-            </div>
+    <>
+      <div>
+        <div className='partners-container'>
+          {partners.map(partner => <Partner key={partner.name} partnerInfos={partner} />)}
+        </div>
+        <div className='compagny'>
+          <h3 className='subtitle'>Sociétés</h3>
+          <div className='partners-container'>
+            {companyPartners.map(partner => <Partner key={partner.name} partnerInfos={partner} />)}
           </div>
-        )
-      )}
-
-      {error ? (
-        <div className='error'>{error}</div>
-      ) : (
-        filteredPartners.length > 0 && <SearchedPartners companies={companyPartners} organizations={organizationPartners} isCompaniesVisible={isCompaniesVisible} onCompaniesVisible={onCompaniesVisible} />
-      )}
+        </div>
+      </div>
 
       <style jsx>{`
-        .searchbar-label {
-          font-size: 1.1em;
-          margin-bottom: 5px;
+        .partners-container {
+          display: grid;
+          grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+          justify-items: center;
+          margin-top: 4em;
+          gap: 6em 5em;
         }
-
-        .results {
-          margin: 0.5em 0 4em 0;
+        .compagny {
+          margin-top: 8em;
+          border-top: ${colors.lighterGrey} solid;
+          display: grid;
+          grid-template-rows: 50px 1fr;
         }
-
-        .organizations {
-          margin-top: 5px;
-          padding-left: 10px;
-          border-left: ${theme.border} solid 3px;
+        .subtitle {
+          margin-top: 2em;
+          color: ${colors.black};
+          font-weight: bold;
+          align-self: center;
         }
-
-        .error {
-          text-align: center;
-          font-style: italic;
-          color: ${theme.colors.red}
-        }
-        `}</style>
-    </div>
+      `}</style>
+    </>
   )
 }
 
-export default PartnersSearchbar
+export default Partners
