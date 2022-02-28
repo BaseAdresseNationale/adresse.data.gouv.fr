6fd1a8aac9d9b1d6cdb404f7b176f67f41f22a77
Marine Le Pennec <lepennec.marine@gmail.com>
2022-02-28 16:23:12 +0100

    Use loader before display data

--- pages/evenements.js
+++ pages/evenements.js
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types'
+import {useEffect, useState} from 'react'
 import {orderBy} from 'lodash'
 import {Calendar} from 'react-feather'
 
@@ -11,6 +11,7 @@ import Head from '@/components/head'
 import Section from '@/components/section'
 import SectionText from '@/components/section-text'
 import Event from '@/components/evenement/event'
+import Loader from '@/components/loader'
 
 function sortEventsByDate(events, order) {
   return orderBy(events, [
@@ -20,59 +21,98 @@ function sortEventsByDate(events, order) {
   ], [order])
 }
 
-const today = new Date().setHours(0, 0, 0, 0)
+function Evenements() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [passedEvents, setPassedEvents] = useState([])
+  const [futureEvents, setFutureEvents] = useState([])
 
-function Evenements({passedEvents, futureEvents}) {
+  useEffect(() => {
+    setIsLoading(true)
+    if (events) {
+      const today = new Date().setHours(0, 0, 0, 0)
+      const sortedPassedEvents = sortEventsByDate(events, 'desc').filter(event => new Date(event.date).setHours(0, 0, 0, 0) < today)
+      const sortedFutureEvents = sortEventsByDate(events, 'asc').filter(event => new Date(event.date).setHours(0, 0, 0, 0) >= today)
+
+      setPassedEvents(sortedPassedEvents)
+      setFutureEvents(sortedFutureEvents)
+    }
+
+    setIsLoading(false)
+  }, [])
+
   return (
     <Page>
       <Head title='Les évènements autour de l’adresse' icon={<Calendar size={56} />} />
-      <Section title='Évènements à venir'>
-        <SectionText>
-          Que vous soyez maire ou élu, agent municipal, géomaticien, producteur ou utilisateur... vous pouvez consulter ici la référence à un événement à venir adapté à vos besoins sur l’adresse.
-        </SectionText>
-        <div className='events-container'>
-          {futureEvents.length > 0 ? (
-            futureEvents.map(event => {
-              const id = `${event.title}-${event.date}`
-              return (
-                <Event
-                  key={id}
-                  event={event}
-                  id={id}
-                />
-              )
-            })
-          ) : (
-            <div>Aucun évènement n’est actuellement programmé</div>
-          )}
-        </div>
-      </Section>
+      <>
+        <Section title='Évènements à venir'>
+          <SectionText>
+            Que vous soyez maire ou élu, agent municipal, géomaticien, producteur ou utilisateur... vous pouvez consulter ici la référence à un événement à venir adapté à vos besoins sur l’adresse.
+          </SectionText>
 
-      <Section title='Évènements passés' background='grey'>
-        <SectionText>
-          Cette section référence les événements passés organisés sur l’adresse par les équipes Base Adresse Locale - Base Adresse Nationale.
-        </SectionText>
-        <div className='events-container'>
-          {futureEvents.length > 0 ? (
-            passedEvents.map(event => {
-              const id = `${event.title}-${event.date}`
-              return (
-                <Event
-                  key={id}
-                  id={id}
-                  event={event}
-                  background='grey'
-                  isPassed
-                />
-              )
-            })
+          {isLoading ? (
+            <div className='loader-container'>
+              <Loader />
+            </div>
           ) : (
-            <div>Aucun évènement n’est actuellement programmé</div>
+            <div className='events-container'>
+              {futureEvents.length > 0 ? (
+                futureEvents.map(event => {
+                  const id = `${event.title}-${event.date}`
+                  return (
+                    <Event
+                      key={id}
+                      event={event}
+                      id={id}
+                    />
+                  )
+                })
+              ) : (
+                <div>Aucun évènement n’est actuellement programmé</div>
+              )}
+            </div>
           )}
-        </div>
-      </Section>
+        </Section>
+
+        <Section title='Évènements passés' background='grey'>
+          <SectionText>
+            Cette section référence les événements passés organisés sur l’adresse par les équipes Base Adresse Locale - Base Adresse Nationale.
+          </SectionText>
+
+          {isLoading ? (
+            <div className='loader-container'>
+              <Loader />
+            </div>
+          ) : (
+            <div className='events-container'>
+              {futureEvents.length > 0 ? (
+                passedEvents.map(event => {
+                  const id = `${event.title}-${event.date}`
+                  return (
+                    <Event
+                      key={id}
+                      id={id}
+                      event={event}
+                      background='grey'
+                      isPassed
+                    />
+                  )
+                })
+              ) : (
+                <div>Aucun évènement n’a eu lieu</div>
+              )}
+            </div>
+          )}
+        </Section>
+      </>
 
       <style jsx>{`
+        .loader-container {
+          width: 100%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
         .events-container {
           display: flex;
           flex-wrap: wrap;
@@ -88,23 +128,6 @@ function Evenements({passedEvents, futureEvents}) {
       `}</style>
     </Page>
   )
-}
-
-export async function getStaticProps() {
-  const passedEvents = sortEventsByDate(events, 'desc').filter(event => new Date(event.date).setHours(0, 0, 0, 0) < today)
-  const futureEvents = sortEventsByDate(events, 'asc').filter(event => new Date(event.date).setHours(0, 0, 0, 0) >= today)
-
-  return {
-    props: {
-      passedEvents,
-      futureEvents
-    }
-  }
-}
-
-Evenements.propTypes = {
-  passedEvents: PropTypes.array.isRequired,
-  futureEvents: PropTypes.array.isRequired
 }
 
 export default Evenements
