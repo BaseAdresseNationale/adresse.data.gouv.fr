62e57e5d7c69b4d8681d05e654ff3d7318ac60bb
gllmr <guillaume@livingdata.co>
2022-03-07 12:15:46 +0100

    Update blog's links

--- components/social-media.js
+++ components/social-media.js
@@ -14,7 +14,7 @@ function SocialMedia() {
   return (
     <>
       <div className='socials'>
-        <Link href='https://blog.geo.data.gouv.fr/'>
+        <Link href='/blog'>
           <a>
             <Image src='/images/logos/blog.svg' height={88} width={88} alt='Blog' />
             <div>En lisant notre blog</div>
