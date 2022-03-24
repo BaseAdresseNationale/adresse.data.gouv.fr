f8610bada81d06c6db5fd71c7bf1b5f53e4a0ef0
gllmr <guillaume@livingdata.co>
2022-03-24 10:41:10 +0100

    Add space under tagList

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -83,6 +83,7 @@ function BlogIndex({posts}) {
       </Section>
       <style jsx>{`
         .blog-section {
+          padding-top: 1em;
           display: grid;
           grid-template-columns: repeat(auto-fit, minmax(260px, 1fr));
           grid-gap: 3em 5em;
