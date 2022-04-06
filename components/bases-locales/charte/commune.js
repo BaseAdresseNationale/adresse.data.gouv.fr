9c852f26cb3a3475d0dc31be72e21ea2f275d954
tmerlier <theophile.merliere@gmail.com>
2022-04-06 11:25:01 +0200

    Add charte URL to commune

--- components/bases-locales/charte/commune.js
+++ components/bases-locales/charte/commune.js
@@ -1,19 +1,26 @@
 import PropTypes from 'prop-types'
 import Image from 'next/image'
 import Link from 'next/link'
+import {ExternalLink} from 'react-feather'
 
 import theme from '@/styles/theme'
+import ButtonLink from '@/components/button-link'
 
-function Commune({name, codeCommune, picture, alt, signatureDate}) {
+function Commune({name, codeCommune, picture, alt, signatureDate, charteURL}) {
   const date = new Date(signatureDate).toLocaleDateString('fr-FR', {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'})
 
   return (
-    <div className='commune-container'>
+    <div className='commune-container' onClick={event => event.stopPropagation()}>
       <div className='commune-infos'>
         <Image src={picture} height={70} width={56} alt={alt} />
         <Link href={`/commune/${codeCommune}`}>{`${name} - ${codeCommune}`}</Link>
       </div>
       <div className='signature-date'>Partenaire depuis le <b>{date}</b></div>
+      {charteURL && (
+        <ButtonLink href={charteURL} isExternal target='_blank'>
+          Voir la charte <ExternalLink size={20} style={{verticalAlign: 'sub'}} />
+        </ButtonLink>
+      )}
 
       <style jsx>{`
         .commune-container {
@@ -55,7 +62,8 @@ Commune.propTypes = {
   codeCommune: PropTypes.string.isRequired,
   picture: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
-  signatureDate: PropTypes.string.isRequired
+  signatureDate: PropTypes.string.isRequired,
+  charteURL: PropTypes.string
 }
 
 export default Commune
