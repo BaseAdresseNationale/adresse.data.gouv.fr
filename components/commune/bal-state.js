3226f5f83695e25d812de0d00e59a4f896e04aaa
Théophile Merlière <theophile.merliere@gmail.com>
2022-11-17 17:22:36 +0100

    Remove subtitle when no numero

--- components/commune/bal-state.js
+++ components/commune/bal-state.js
@@ -15,6 +15,7 @@ import ButtonLink from '@/components/button-link'
 import Statistics from './statistics'
 
 function sanitedSources(adressesSources) {
+  console.log('🚀 ~ file: bal-state.js ~ line 18 ~ sanitedSources ~ adressesSources', adressesSources)
   const sources =
     {
       cadastre: 'cadastre',
@@ -48,7 +49,11 @@ function BALState({communeInfos, mairieInfos, revision, typeComposition, hasMigr
   const subtitle = useMemo(() => {
     // Aucune BAL créée
     if (typeComposition === 'assemblage') {
-      return `Les données sont actuellement construites à partir des sources historiques suivantes : ${sanitedSources(adressesSources)}`
+      if (adressesSources.length > 0) {
+        return `Les données sont actuellement construites à partir des sources historiques suivantes : ${sanitedSources(adressesSources)}`
+      }
+
+      return null
     }
 
     // BAL non disponible (en migration)
