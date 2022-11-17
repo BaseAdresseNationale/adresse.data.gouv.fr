a71c692888410247d23b2339c3e3571efec716ce
Florian Loreau <florian.loreau@ign.fr>
2022-11-17 17:50:00 +0100

    New page "reutilisateurs"

---
+++ pages/donnees-nationales/reutilisateurs.js
@@ -0,0 +1,50 @@
+import {Users, Mail} from 'react-feather'
+
+import Page from '@/layouts/main'
+
+import Head from '@/components/head'
+import Section from '@/components/section'
+import ButtonLink from '@/components/button-link'
+import Partners from '@/components/bases-locales/charte/partners'
+
+import partners from 'partners.json'
+
+function Reutilisateurs() {
+  const title = 'Données nationales'
+  const description = 'Fichiers nationaux contenant les adresses du territoire.'
+
+  const {reUsersBan} = partners
+  return (
+    <Page title={title} description={description}>
+      <Head title={title} icon={<Users size={56} alt aria-hidden='true' />} />
+
+      <Section title='Qui réutilise la BAN&nbsp;?'>
+        <div className='partners-section'>
+          <Partners data={reUsersBan} />
+        </div>
+      </Section>
+
+      <Section subtitle='Pour apparaître dans cette liste de réutilisateurs, contactez-nous :' background='grey'>
+        <div className='contact-button'>
+          <ButtonLink href='mailto:adresse@data.gouv.fr' isExternal>
+            Contactez-nous
+            <Mail style={{verticalAlign: 'bottom', marginLeft: '4px'}} alt aria-hidden='true' />
+          </ButtonLink>
+        </div>
+      </Section>
+
+      <style jsx>{`
+        .contact-button {
+          padding-top: 2em;
+          text-align: center;
+        }
+
+        .partners-section {
+          padding-top: 2em;
+        }
+      `}</style>
+    </Page>
+  )
+}
+
+export default Reutilisateurs
