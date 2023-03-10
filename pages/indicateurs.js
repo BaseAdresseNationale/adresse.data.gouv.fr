9826b158b867b18481997dc2e607b9c695e29cd7
Florian Loreau <florian.loreau@ign.fr>
2023-03-10 14:04:52 +0100

    Add 02/2023 indicators

--- pages/indicateurs.js
+++ pages/indicateurs.js
@@ -1,8 +1,10 @@
+import Image from 'next/legacy/image'
 import {PieChart} from 'react-feather'
 
 import Page from '@/layouts/main'
 
 import Head from '@/components/head'
+import Section from '@/components/section'
 
 function Indicateurs() {
   const title = 'Indicateurs d’impact'
@@ -11,6 +13,18 @@ function Indicateurs() {
   return (
     <Page title={title} description={description}>
       <Head title={title} icon={<PieChart size={56} alt='' aria-hidden='true' />} />
+
+      <Section title='Nombre de requêtes' subtitle='en février 2023' >
+        <Image src='/images/indicateurs/nb_requetes.png' height={250} width={1000} />
+      </Section>
+
+      <Section title='Part public-privé' subtitle='en février 2023' background='grey'>
+        <Image src='/images/indicateurs/part_public.png' height={550} width={1000} />
+      </Section>
+
+      <Section title='Ils nous font confiance' subtitle='Les 5 plus gros utilisateurs publics en février 2023'>
+        <Image src='/images/indicateurs/top5.png' height={590} width={1000} />
+      </Section>
 
     </Page>
   )
