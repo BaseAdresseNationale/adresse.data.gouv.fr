e049ddb92b94ac27265d9d9a98308d72e5303b7f
Florian Loreau <florian.loreau@ign.fr>
2023-06-13 14:45:18 +0200

    Update indicators to may 2023

--- pages/indicateurs.js
+++ pages/indicateurs.js
@@ -20,20 +20,20 @@ function Indicateurs() {
           Cette page d’indicateurs ne reflète qu’une partie des usages. Sont ici représentés les usages de l’API adresse et non les usages de téléchargement des fichiers adresse. Néanmoins, les exploitations de l’API sont une illustration relativement fidèle des usages de l’adresse et de la typologie des utilisateurs.<br />
           Les statistiques sont présentées sur une périodicité mensuelle et sont issues des données brutes d’exploitation.<br />
           Ces données seront régulièrement enrichies afin de donner une lecture la plus fidèle des usages de la BAN.<br />
-          Date de dernière mise à jour : Avril 2023
+          Date de dernière mise à jour : Juin 2023
         </SectionText>
       </Section>
 
-      <Section title='Nombre de requêtes' subtitle='en mars 2023' >
+      <Section title='Nombre de requêtes' subtitle='en mai 2023' >
         <Image src='/images/indicateurs/nb_requetes.png' height={200} width={1000} />
       </Section>
 
-      <Section title='Part public-privé' subtitle='en mars 2023' background='grey'>
-        <Image src='/images/indicateurs/part_public.png' height={550} width={1000} />
+      <Section title='Part public-privé' subtitle='en mai 2023' background='grey'>
+        <Image src='/images/indicateurs/part_public.png' height={575} width={1000} />
       </Section>
 
-      <Section title='Ils nous font confiance' subtitle='Les 5 plus gros utilisateurs publics en mars 2023'>
-        <Image src='/images/indicateurs/top5.png' height={520} width={1000} />
+      <Section title='Ils nous font confiance' subtitle='Les 5 plus gros utilisateurs publics en mai 2023'>
+        <Image src='/images/indicateurs/top5.png' height={530} width={1000} />
       </Section>
 
     </Page>
