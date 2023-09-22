30fc886e474734cd8fbe45b3c49f2226f6227b43
Nicolas KOKLA <n.kokla@koaji.fr>
2023-09-22 10:08:35 +0200

    Data explorer: add symlink support

--- pages/data/[[...path]].js
+++ pages/data/[[...path]].js
@@ -18,36 +18,69 @@ const rootLink = {
   label: 'Données nationales',
 }
 
-const getDirectories = path => (
+const autorizedPath = (_path, rootPath = PATH) => {
+  const realPath = fs.realpathSync(_path)
+  const isRealPathIsInRoot = realPath.startsWith(rootPath)
+  const isRealPathIsVisibleFile = !path.basename(realPath).startsWith('.')
+  const isPathIsVisibleFile = !path.basename(_path).startsWith('.')
+
+  return {
+    path: realPath,
+    auth: (
+      isRealPathIsInRoot &&
+      isRealPathIsVisibleFile &&
+      isPathIsVisibleFile
+    )
+  }
+}
+
+const getDirectories = _path => (
   fs
-    .readdirSync(path, {withFileTypes: true})
-    .filter(({name}) => !name.startsWith('.')) // Hide hidden files
-    .filter(entry => !entry.isSymbolicLink()) // Hide Symbolic links
-    .map(entry => ({
-      name: entry.name,
-      isDirectory: entry.isDirectory()
-    }))
+    .readdirSync(_path, {withFileTypes: true})
+    .filter(entry => autorizedPath(path.join(_path, entry.name)).auth)
+    .map(entry => {
+      const isSymbolicLink = entry.isSymbolicLink()
+      const computedPath = isSymbolicLink ? path.resolve(
+        _path,
+        fs.readlinkSync(path.join(_path, entry.name))
+      ) : path.join(_path, entry.name)
+      const target = isSymbolicLink ? fs.lstatSync(computedPath) : entry
+      const isDirectory = target.isDirectory()
+      const targetName = isSymbolicLink ? path.basename(computedPath) : entry.name
+
+      return ({
+        name: entry.name,
+        targetName,
+        isDirectory,
+        isSymbolicLink,
+      })
+    })
 )
 
 export function getServerSideProps(context) {
+  let stat
+  let targetFileName
+
   const {path: paramPath = []} = context.params
   const fileName = `${paramPath.join('/')}`
-  const filePath = path.join(PATH, fileName)
+  const filePath = path.resolve(PATH, fileName)
   const date = new Date()
   const formattedDate = new Intl.DateTimeFormat('fr', {year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(date).replace(/,/g, '\'').replace(/ /g, ' ')
 
-  let stat
-
-  if (!filePath.startsWith(PATH)) {
-    console.warn(`[${formattedDate} - WARNING]`, `Attempted illegal access to ${filePath}`)
-    context.res.statusCode = 404
-    return {
-      props: {errorCode: 404},
-    }
-  }
-
   try {
-    stat = fs.lstatSync(filePath)
+    const authPath = autorizedPath(filePath)
+    const realPath = authPath.path
+
+    if (!authPath.auth) {
+      console.warn(`[${formattedDate} - WARNING]`, `Attempted illegal access to ${authPath.path}`)
+      context.res.statusCode = 404
+      return {
+        props: {errorCode: 404},
+      }
+    }
+
+    stat = fs.lstatSync(realPath)
+    targetFileName = path.basename(realPath)
   } catch (err) {
     console.warn(`[${formattedDate} - ERROR]`, 'File access error:', err)
     context.res.statusCode = 404
@@ -67,13 +100,13 @@ export function getServerSideProps(context) {
   }
 
   try {
-    const fileExtension = path.extname(fileName)
+    const fileExtension = path.extname(targetFileName)
     const contentType = mime.getType(fileExtension) || 'application/octet-stream'
     const fileContents = fs.readFileSync(filePath)
     const sendToTracker = getAnalyticsPusher()
 
     context.res.setHeader('Content-Type', contentType)
-    context.res.setHeader('Content-Disposition', `attachment; filename="${fileName}"`)
+    context.res.setHeader('Content-Disposition', `attachment; filename="${targetFileName}"`)
     context.res.statusCode = 200
     context.res.end(fileContents)
 
