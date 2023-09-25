0ff50be10d4e145cd4184310f546d797f8dc6aaf
Fufeck <fabien.tafforeau@gmail.com>
2023-09-25 16:25:35 +0400

    deploiement-bal v2

--- components/bases-locales/deploiement-bal/panel-bal.js
+++ components/bases-locales/deploiement-bal/panel-bal.js
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types'
+import {useEffect, useState, useMemo, useCallback} from 'react'
 import {groupBy} from 'lodash'
 import {Doughnut} from 'react-chartjs-2'
 import {Chart as ChartJS, ArcElement, Tooltip, Legend} from 'chart.js'
 
+import {getBals} from '@/lib/mes-adresse-api'
 import CommuneBALList from './commune-bal-list'
 
 ChartJS.register(ArcElement, Tooltip, Legend)
@@ -21,47 +23,81 @@ const options = {
   }
 }
 
-function PanelBal({filteredCodesCommmune, bals}) {
-  const balsByCommune = groupBy(bals, 'commune')
-
-  const data = {
-    labels: [
-      'Publiée',
-      'Prête à être publiée',
-      'Brouillon'
+const initialStats = {
+  labels: [
+    'Publiée',
+    'Prête à être publiée',
+    'Brouillon'
+  ],
+  datasets: [{
+    data: [0, 0, 0],
+    backgroundColor: [
+      '#228833',
+      '#AA3377',
+      '#EE6677'
     ],
-    datasets: [{
-      data: [
-        bals?.filter(bal => bal.status === 'published')?.length,
-        bals?.filter(bal => bal.status === 'ready-to-publish')?.length,
-        bals?.filter(bal => bal.status === 'draft')?.length
-      ],
-      backgroundColor: [
-        '#228833',
-        '#AA3377',
-        '#EE6677'
-      ],
-      hoverOffset: 4
-    }]
-  }
+    hoverOffset: 4
+  }]
+}
+
+function PanelBal({filteredCodesCommmune}) {
+  const [bals, setBals] = useState([])
+  const [dataStats, setDataStats] = useState(initialStats)
+
+  useEffect(() => {
+    async function getBalsFiltered() {
+      const fields = ['_id', 'commune', 'status', 'nom', '_updated', 'sync']
+      const balsFiltered = await getBals(fields, filteredCodesCommmune)
+      setDataStatsWithBal(balsFiltered)
+      if (filteredCodesCommmune.length > 0) {
+        setBals(balsFiltered)
+      }
+    }
+
+    getBalsFiltered()
+  }, [filteredCodesCommmune, setDataStatsWithBal])
+
+  const balsByCommune = useMemo(() => {
+    return groupBy(bals, 'commune')
+  }, [bals])
+
+  const setDataStatsWithBal = useCallback(balsFiltered => {
+    const data = [
+      balsFiltered.filter(bal => bal.status === 'published').length,
+      balsFiltered.filter(bal => bal.status === 'ready-to-publish').length,
+      balsFiltered.filter(bal => bal.status === 'draft').length
+    ]
+
+    setDataStats(dataStats => {
+      return {
+        ...dataStats,
+        datasets: [
+          {
+            ...dataStats.datasets[0],
+            data
+          }
+        ]
+      }
+    })
+  }, [setDataStats, dataStats])
 
   return (
     <div>
       <br />
+      <h3>Statut des BAL(s)</h3>
+      <div className='stats'>
+        <div className='donut'>
+          <Doughnut data={dataStats} options={options} />
+        </div>
+      </div>
+      <br />
+      <h3>Liste des Communes</h3>
       {filteredCodesCommmune.length > 0 ? (
         <div>
-          <h3>Statut des BAL(s)</h3>
-          <div className='donut'>
-            <Doughnut data={data} options={options} />
-          </div>
-          <br />
-          <h3>Liste des Communes</h3>
-
-          {filteredCodesCommmune.map(codeCommune => {
+          {Object.keys(balsByCommune).map(codeCommune => {
             const balsCommune = balsByCommune[codeCommune] || []
             return <CommuneBALList key={codeCommune} codeCommune={codeCommune} balsCommune={balsCommune} />
           })}
-
         </div>
       ) : (
         <p>Aucun Département ou EPCI sélectionné</p>
@@ -70,11 +106,15 @@ function PanelBal({filteredCodesCommmune, bals}) {
         h3 {
           text-align: left;
         }
-        .donut {
+        .stats {
           border: 1px solid lightgrey;
           padding: 1em;
           border-radius: 5px;
-          max-width: 300px;
+          display: flex;
+          justify-content: center;
+        }
+        .donut {
+          width: 60%;
         }
       `}</style>
     </div>
@@ -83,7 +123,6 @@ function PanelBal({filteredCodesCommmune, bals}) {
 
 PanelBal.propTypes = {
   filteredCodesCommmune: PropTypes.array.isRequired,
-  bals: PropTypes.array.isRequired
 }
 
 export default PanelBal
