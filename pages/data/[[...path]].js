7f97c8fba54d3273116ffbf006f950b8d10c07be
Nicolas KOKLA <n.kokla@koaji.fr>
2024-02-26 17:10:23 +0100

    Connect data page to S3 server

--- pages/data/[[...path]].js
+++ pages/data/[[...path]].js
@@ -1,8 +1,6 @@
-import fs from 'node:fs'
-import path from 'node:path'
-import send from 'send'
 import PropTypes from 'prop-types'
 import {Download} from 'react-feather'
+import {S3} from '@aws-sdk/client-s3'
 
 import Head from '@/components/head'
 import Section from '@/components/section'
@@ -12,145 +10,174 @@ import sendToTracker, {getDownloadToEventTracker} from '@/lib/util/analytics-tra
 
 import ErrorPage from '../_error'
 
-const PATH = process.env.PATH_STATIC_FILE
+const {
+  S3_CONFIG_ACCESS_KEY_ID,
+  S3_CONFIG_SECRET_ACCESS_KEY,
+  S3_CONFIG_REGION,
+  S3_CONFIG_ENDPOINT,
+} = process.env
+
 const rootLink = {
   href: '/donnees-nationales',
   label: 'Données nationales',
 }
 
-const autorizedPath = (_path, rootPath = PATH) => {
-  let realPath
-  try {
-    realPath = fs.realpathSync(_path)
-  } catch (err) {
-    console.warn('[WARNING]', 'File access error:', err)
-    return {
-      path: _path,
-      auth: false
-    }
-  }
-
-  const isRealPathIsInRoot = realPath.startsWith(rootPath)
-  const isRealPathIsVisibleFile = !path.basename(realPath).startsWith('.')
-  const isPathIsVisibleFile = !path.basename(_path).startsWith('.')
-
-  return {
-    path: realPath,
-    auth: (
-      isRealPathIsInRoot &&
-      isRealPathIsVisibleFile &&
-      isPathIsVisibleFile
-    )
-  }
-}
-
-const getDirectories = _path => (
-  fs
-    .readdirSync(_path, {withFileTypes: true})
-    .filter(entry => autorizedPath(path.join(_path, entry.name)).auth)
-    .map(entry => {
-      const isSymbolicLink = entry.isSymbolicLink()
-      const computedPath = isSymbolicLink ? path.resolve(
-        _path,
-        fs.readlinkSync(path.join(_path, entry.name))
-      ) : path.join(_path, entry.name)
-      const target = isSymbolicLink ? fs.lstatSync(computedPath) : fs.lstatSync(path.resolve(_path, entry.name))
-      const isDirectory = target.isDirectory()
-      const targetName = isSymbolicLink ? path.basename(computedPath) : entry.name
-      const {size = 0, mtime = null} = target
-
-      return ({
-        name: entry.name,
-        targetName,
-        isDirectory,
-        isSymbolicLink,
-        fileInfo: isDirectory ? null : {
-          size,
-          date: mtime.toUTCString()
-        }
-      })
-    })
-)
+const bucketName = 'prd-ign-mut-ban'
+const rootDir = ['adresse-data']
+const clientS3 = new S3({
+  accessKeyId: S3_CONFIG_ACCESS_KEY_ID,
+  secretAccessKey: S3_CONFIG_SECRET_ACCESS_KEY,
+  region: S3_CONFIG_REGION,
+  endpoint: S3_CONFIG_ENDPOINT,
+})
 
 const getFormatedDate = () => {
   const date = new Date()
-  return new Intl.DateTimeFormat('fr', {year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(date).replace(/,/g, '\'').replace(/ /g, ' ')
+  const dateFormatOptions = {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  }
+  return new Intl.DateTimeFormat('fr', dateFormatOptions)
+    .format(date)
+    .replace(/,/g, '\'')
+    .replace(/ /g, ' ')
 }
 
-const asyncSend = (req, res, filePath) => new Promise((resolve, reject) => {
-  const targetFileName = path.basename(filePath)
-  function headers(res) {
-    res.setHeader('Content-Disposition', `attachment; filename="${targetFileName}"`)
+const autorizedPathS3 = path => {
+  const isPathIsVisibleFile = !path.startsWith('.')
+
+  return {
+    path,
+    auth: isPathIsVisibleFile
+  }
+}
+
+const asyncSendS3 = (req, res, {params, fileName, metadata}) => new Promise(
+  (resolve, reject) => {
+    res.attachment(params.key)
+    res.setHeader('Content-Length', metadata.ContentLength)
+    res.setHeader('Content-Disposition', `attachment; filename="${fileName}"`)
+
+    /* eslint-disable promise/prefer-await-to-then */
+    clientS3.getObject(params)
+      .then(({Body}) => {
+        Body
+          .on('error', err => {
+            const formattedDate = getFormatedDate()
+            console.warn(`[${formattedDate} - ERROR]`, 'File access error:', err)
+            reject(err)
+          })
+          .on('end', () => {
+            resolve()
+          })
+          .pipe(res)
+      })
+      .catch(err => console.error(err))
+  /* eslint-enable promise/prefer-await-to-then */
+  }
+)
+
+// Reccursivly GET S3 Objects
+async function listObjectsRecursively(prefix, continuationToken) {
+  const params = {
+    Bucket: bucketName,
+    Prefix: prefix || undefined,
+    Delimiter: '/',
+    ContinuationToken: continuationToken,
   }
 
-  send(req, encodeURI(filePath), {index: false})
-    .on('headers', headers)
-    .on('error', err => {
-      const formattedDate = getFormatedDate()
-      console.warn(`[${formattedDate} - ERROR]`, 'File access error:', err)
-      reject(err)
-    })
-    .on('end', () => {
-      resolve()
-    })
-    .pipe(res)
-})
+  try {
+    const data = await clientS3.listObjectsV2(params)
+
+    if (data.KeyCount <= 0) {
+      return undefined
+    }
+
+    const filesList = (data.Contents || []).map(({Key, LastModified, ETag, Size}) => ({
+      name: Key.replace(new RegExp(`^${prefix}`), ''),
+      path: Key,
+      fileInfo: {
+        date: LastModified.toString(),
+        hash: ETag,
+        size: Size,
+      },
+      isDirectory: false,
+    }))
+    const dirsList = (data.CommonPrefixes || []).map(({Prefix}) => ({
+      name: Prefix.replace(new RegExp(`^${prefix}`), '').replace(/\/$/, ''),
+      path: Prefix,
+      isDirectory: true,
+    }))
+    const nextList = (data.IsTruncated) ? listObjectsRecursively(prefix, data.NextContinuationToken) : []
+    return [...filesList, ...dirsList, ...nextList]
+  } catch (err) {
+    console.error('Erreur lors de la récupération de la liste des objets :', err)
+  }
+}
 
 export async function getServerSideProps(context) {
-  let stat
-  let realPath
-
   const {params, res, req} = context
   const {path: paramPath = []} = params
   const fileName = `${paramPath.join('/')}`
-  const filePath = path.resolve(PATH, fileName)
   const formattedDate = getFormatedDate()
+  const s3ObjectPath = [...rootDir, ...paramPath].join('/')
 
   try {
-    const authPath = autorizedPath(filePath)
+    const s3Head = await clientS3.headObject({
+      Bucket: bucketName,
+      Key: s3ObjectPath,
+    })
 
-    if (!authPath.auth) {
-      console.warn(`[${formattedDate} - WARNING]`, `Attempted illegal access to ${authPath.path}`)
-      res.statusCode = 404
+    // NO ERROR > PATH IS A FILE
+    try {
+      sendToTracker(getDownloadToEventTracker({
+        downloadDataType: `${paramPath[0]}${req?.headers?.range ? ' (Partial)' : ''}`,
+        downloadFileName: fileName,
+        nbDownload: 1
+      }))
+      await asyncSendS3(req, res, {
+        params: {
+          Bucket: bucketName,
+          Key: s3ObjectPath,
+        },
+        fileName: paramPath[paramPath.length - 1],
+        metadata: s3Head,
+      })
+    } catch (err) {
+      console.warn(`[${formattedDate} - ERROR]`, 'File access error:', err)
       return {
-        props: {errorCode: 404},
+        props: {
+          errorCode: 502,
+          errorMessage: 'Une erreur est survenue lors de la génération du téléchargement.',
+        },
+      }
+    }
+  } catch {
+    // ERROR > PATH SHOULD BE DIRECTORY
+    const s3DirPath = `${s3ObjectPath}/`
+    const s3Objects = (await listObjectsRecursively(s3DirPath))
+    if (s3Objects) {
+      const s3data = (s3Objects || [])
+        .filter(({name}) => autorizedPathS3(name).auth)
+
+      return {
+        props: {
+          title: ['data', ...paramPath].join('/') || '',
+          path: paramPath || [],
+          data: s3data || []
+        }
       }
     }
 
-    realPath = authPath.path
-    stat = fs.lstatSync(realPath)
-  } catch (err) {
-    console.warn(`[${formattedDate} - ERROR]`, 'File access error:', err)
+    // OBJECT DO NOT EXIST > IS UNKNOWN PATH
+    console.warn(`[${formattedDate} - ERROR]`, 'S3 Object - Try access to unknown object:', s3DirPath)
     res.statusCode = 404
     return {
       props: {errorCode: 404},
-    }
-  }
-
-  if (stat.isDirectory()) {
-    return {
-      props: {
-        title: ['data', ...paramPath].join('/') || '',
-        path: paramPath || [],
-        data: getDirectories(filePath) || [],
-      }
-    }
-  }
-
-  try {
-    sendToTracker(getDownloadToEventTracker({
-      downloadDataType: `${path.dirname(fileName).split('/')[0]}${req?.headers?.range ? ' (Partial)' : ''}`,
-      downloadFileName: fileName,
-      nbDownload: 1
-    }))
-    await asyncSend(req, res, realPath)
-  } catch (err) {
-    console.warn(`[${formattedDate} - ERROR]`, 'File access error:', err)
-    return {
-      props: {
-        errorCode: 502,
-        errorMessage: 'Une erreur est survenue lors de la génération du téléchargement.',
-      },
     }
   }
 
