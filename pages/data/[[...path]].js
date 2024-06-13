0d95955aa2e78fbc3d5db3a2218a705a57364fa1
Nicolas KOKLA <n.kokla@koaji.fr>
2024-06-13 15:51:08 +0200

    Fix:  S3 env config variables are not loaded on runtime

--- pages/data/[[...path]].js
+++ pages/data/[[...path]].js
@@ -1,9 +1,10 @@
+import getConfig from 'next/config'
 import DataPageLegacy, {getServerSideProps as getServerSidePropsLegacy} from './data-legacy'
 import DataPageS3, {getServerSideProps as getServerSidePropsS3} from './data-s3'
 
-const {NEXT_PUBLIC_TOGGLER_DATA_SOURCES} = process.env
-const getServerSideProps = NEXT_PUBLIC_TOGGLER_DATA_SOURCES === 'S3' ? getServerSidePropsS3 : getServerSidePropsLegacy
-const DataPage = NEXT_PUBLIC_TOGGLER_DATA_SOURCES === 'S3' ? DataPageS3 : DataPageLegacy
+const {NEXT_PUBLIC_TOGGLER_DATA_SOURCES: TOGGLER_DATA_SOURCES} = getConfig().publicRuntimeConfig
+const getServerSideProps = TOGGLER_DATA_SOURCES === 'S3' ? getServerSidePropsS3 : getServerSidePropsLegacy
+const DataPage = TOGGLER_DATA_SOURCES === 'S3' ? DataPageS3 : DataPageLegacy
 
 export {getServerSideProps}
 export default DataPage
