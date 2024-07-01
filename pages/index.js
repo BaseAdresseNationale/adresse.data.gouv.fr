2890c2568a43bb775ba45eb916c8a0883d995537
Jugurtha Bouhadoun <jugurtha.bouhadoun@ign.fr>
2024-07-01 10:44:57 +0200

    remove hour information

--- pages/index.js
+++ pages/index.js
@@ -187,9 +187,7 @@ const fetchLastUpdatedDate = async () => {
     const formattedDate = `${date.toLocaleDateString('fr-FR', {
       day: '2-digit',
       month: '2-digit',
-      year: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit',
+      year: 'numeric'
     })}`
 
     return formattedDate
