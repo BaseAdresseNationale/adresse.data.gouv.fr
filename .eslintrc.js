f936892949209eef054ffac2acc2f4ad882b2b25
Nicolas KOKLA <n.kokla@koaji.fr>
2024-07-16 00:21:03 +0100

    Add Storybook in Stack project

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: [
     'plugin:@stylistic/recommended-extends',
     'next/core-web-vitals',
+    'plugin:storybook/recommended',
   ],
   rules: {
     '@stylistic/jsx-one-expression-per-line': 'off',
