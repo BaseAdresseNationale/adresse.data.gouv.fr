4b39ce81f9f3d0191290dd94ea29a63ca925a534
Jugurtha Bouhadoun <jugurtha.bouhadoun@ign.fr>
2024-09-04 17:18:06 +0200

    ajout d'un tableau pour les parcelles

--- components/document/numerotation/certificat.js
+++ components/document/numerotation/certificat.js
@@ -1,4 +1,3 @@
-
 import {
   Document,
   Link,
@@ -7,23 +6,25 @@ import {
   StyleSheet,
   Font,
   Image,
-  View} from '@react-pdf/renderer'
+  View
+} from '@react-pdf/renderer'
 import PropTypes from 'prop-types'
 import getConfig from 'next/config'
 
 const fontSource = './public/dsfr/fonts/Marianne-Regular.woff'
 const fontSourceBold = './public/dsfr/fonts/Marianne-Bold.woff'
-const fontFamily = 'Marianne' // Devrait etre Arial Regular mais pas de source
+const fontFamily = 'Marianne'
 const {NEXT_PUBLIC_ADRESSE_URL} = getConfig().publicRuntimeConfig
 
 Font.register({
   family: 'Marianne',
   fonts: [
-    {src: fontSource}, // Font-style: normal, font-weight: normal
+    {src: fontSource},
     {src: fontSourceBold, fontWeight: 700},
     {src: './public/dsfr/fonts/Marianne-Regular_Italic.woff', fontStyle: 'italic'},
     {src: './public/dsfr/fonts/Marianne-Bold_Italic.woff', fontStyle: 'italic', fontWeight: 700}
-  ]})
+  ]
+})
 
 const stylesDSFR = StyleSheet.create({
   page: {padding: '17mm', fontFamily, fontSize: '10pt'},
@@ -34,18 +35,27 @@ const stylesDSFR = StyleSheet.create({
   qrCode: {width: '25mm', height: '25mm', alignSelf: 'flex-end'},
   footer: {position: 'absolute', bottom: '20mm', left: '20mm', right: '20mm', fontSize: 10, flexDirection: 'column', alignItems: 'flex-start'},
   footerText: {marginBottom: '5mm', textAlign: 'left'},
-  logoBloc: {width: '39mm', height: '30mm'},
+  logoBloc: {width: '40mm', height: 'auto'},
   logoBanBloc: {width: '20mm', height: '20mm'},
   logoContainer: {flexDirection: 'row', alignItems: 'center'},
-  boldText: {fontFamily: 'Marianne', fontWeight: '700'}
+  boldText: {fontFamily: 'Marianne', fontWeight: '700'},
+  headerContainer: {flexDirection: 'row', alignItems: 'center', marginBottom: '12mm'},
+  textContainer: {marginLeft: '10mm'},
+  table: {display: 'table', width: 'auto', marginTop: '10mm', borderStyle: 'solid', borderWidth: 1, borderColor: '#000'},
+  tableRow: {flexDirection: 'row'},
+  tableCol: {width: '50%', borderStyle: 'solid', borderWidth: 1, borderColor: '#000'},
+  tableCell: {margin: 5, fontSize: 10},
+  tableHeader: {backgroundColor: '#f0f0f0', fontWeight: 'bold'}
 })
+
 function CertificatNumerotation({data, qrCodeDataURL, mairie}) {
   const nomCommune = data.full_address.districtDefaultLabel
   const libelleVoie = data.full_address.commonToponymDefaultLabel
   const numero = data.full_address.number
   const suffix = data.full_address.suffix ? data.full_address.suffix : ''
   const {cog} = data.full_address
-  const parcelleCadastral = data.cadastre_ids.map(id => id.replace(/(\d+)([A-Z])/, '$1 $2')).join(', ')
+  const parcelles = data.cadastre_ids.map(id => id.replace(/(\d+)([A-Z])/, '$1 $2'))
+
   const logoUrl = `public/logos/certificat/${cog}.png`
 
   const dateObj = new Date(data.createdAt)
@@ -54,34 +64,54 @@ function CertificatNumerotation({data, qrCodeDataURL, mairie}) {
   const year = dateObj.getFullYear()
   const etabliLe = `${day} ${month} ${year}`
   const certificatUrl = `${NEXT_PUBLIC_ADRESSE_URL}/certificat/${data.id}`
+
   return (
-    <Document tile="Certificat d'adressage">
+    <Document title="Certificat d'adressage">
       <Page size='A4' style={stylesDSFR.page}>
-        <Image src={logoUrl} style={stylesDSFR.logoBloc} />
-        <Text>Ville de {nomCommune}</Text>
-        <Text>{mairie?.telephone}</Text>
-        <Text>{mairie?.email}</Text>
+        <View style={stylesDSFR.headerContainer}>
+          <Image src={logoUrl} style={stylesDSFR.logoBloc} />
+          <View style={stylesDSFR.textContainer}>
+            <Text>Ville de {nomCommune}</Text>
+            <Text>{mairie?.telephone}</Text>
+            <Text>{mairie?.email}</Text>
+          </View>
+        </View>
+
         <Text style={stylesDSFR.titre}>Certificat d&apos;adressage</Text>
         <View style={stylesDSFR.contenu}>
           <Text>
-            La commune de {nomCommune} atteste que l&apos;adresse ci-dessous est certifiée dans la Base Adresse
-            Nationale à la date du {etabliLe}
+            La ville de {nomCommune} atteste que l&apos;adresse ci-dessous est certifiée dans la Base Adresse
+            Nationale à la date du {etabliLe}.
           </Text>
           <Text> {'\n'}</Text>
-          <Text>
-            Section(s) et N° parcelle(s) cadastrale(s) :
-          </Text>
-          <Text>
-            {parcelleCadastral}
-          </Text>
-          <Text> {'\n'}</Text>
-          <Text>
-            N° de voirie et désignation de la voie :
-          </Text>
-          {suffix ?
-            <Text style={stylesDSFR.boldText}>{numero} {suffix} {libelleVoie}</Text> :
-            <Text style={stylesDSFR.boldText}>{numero} {libelleVoie}</Text>}
-          <Text style={stylesDSFR.boldText}>{nomCommune}</Text>
+
+          {/* Tableau modifié avec deux colonnes */}
+          <View style={stylesDSFR.table}>
+            <View style={stylesDSFR.tableRow}>
+              <View style={stylesDSFR.tableCol}>
+                <Text style={[stylesDSFR.tableCell, stylesDSFR.tableHeader]}>Section(s) et N° parcelle(s) cadastrale(s)</Text>
+              </View>
+              <View style={stylesDSFR.tableCol}>
+                <Text style={[stylesDSFR.tableCell, stylesDSFR.tableHeader]}>N° de voirie et désignation de la voie</Text>
+              </View>
+            </View>
+            <View style={stylesDSFR.tableRow}>
+              <View style={stylesDSFR.tableCol}>
+                {parcelles.map((parcelle, index) => (
+                  <Text key={index} style={stylesDSFR.tableCell}>{parcelle}</Text>
+                ))}
+              </View>
+              <View style={stylesDSFR.tableCol}>
+                <Text style={stylesDSFR.tableCell}>
+                  {suffix ? `${numero} ${suffix} ${libelleVoie}` : `${numero} ${libelleVoie}`}
+                </Text>
+                <Text style={stylesDSFR.tableCell}>
+                  {nomCommune}
+                </Text>
+              </View>
+            </View>
+          </View>
+
           <Text> {'\n'}</Text>
 
           <Text>
@@ -93,7 +123,7 @@ function CertificatNumerotation({data, qrCodeDataURL, mairie}) {
         <Image src={qrCodeDataURL} style={stylesDSFR.qrCode} />
         <View style={stylesDSFR.footer}>
           <Text style={stylesDSFR.footerText}>
-            Émis par les services de la Base Adresse Nationale, mandataire pour la commune de {nomCommune}.
+            Émis par les services de la Base Adresse Nationale, mandataire pour la ville de {nomCommune}.
           </Text>
 
           <View style={stylesDSFR.logoContainer}>
@@ -113,7 +143,6 @@ CertificatNumerotation.propTypes = {
   data: PropTypes.shape({
     id: PropTypes.string.isRequired,
     createdAt: PropTypes.string.isRequired,
-    address_id: PropTypes.string.isRequired,
     full_address: PropTypes.shape({
       number: PropTypes.number.isRequired,
       commonToponymDefaultLabel: PropTypes.string.isRequired,
