2539f4af33fa92f60c96459af1b338a414ff7838
Guillaume Fay <guillaume_fay@hotmail.com>
2024-09-27 14:07:24 +0200

    fix: file size limit in publication form (#1837)

--- components/bases-locales/validator/file-handler.js
+++ components/bases-locales/validator/file-handler.js
@@ -45,7 +45,7 @@ class FileHandler extends React.Component {
 
           <div className='file-handler-container'>
             <Holder
-              placeholder='Sélectionner ou glisser ici votre fichier BAL au format CSV (maximum 100 Mo)'
+              placeholder='Sélectionner ou glisser ici votre fichier BAL au format CSV (maximum 50 Mo)'
               file={file}
               isLoading={isLoading}
               onDrop={onFileDrop}
