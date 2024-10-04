a44cc73d6bb1150952083640195598b5d8946f18
Nicolas KOKLA <n.kokla@koaji.fr>
2024-10-04 16:38:25 +0200

    Fix: Update next config for support env-vars

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 
+import fs from 'node:fs'
 import NextBundleAnalyzer from '@next/bundle-analyzer'
+import dotenv from 'dotenv'
+
+const getNextEnv = envVars => Object.fromEntries(
+  Object
+    .entries(envVars)
+    .filter(([key]) => key.startsWith('NEXT_PUBLIC'))
+)
+
+const defaultEnvVarFile = '.env.default'
+const defaultEnvVarRaw = dotenv.parse(fs.readFileSync(defaultEnvVarFile))
+const defaultEnvVar = getNextEnv(defaultEnvVarRaw)
+const envVar = getNextEnv(process.env)
 
 const NEXT_PUBLIC_GHOST_URL_IMAGES_SOURCE = process.env.NEXT_PUBLIC_GHOST_URL_IMAGES_SOURCE
 const imagesDomains = ['static.data.gouv.fr']
@@ -13,6 +26,10 @@ const withBundleAnalyzer = NextBundleAnalyzer({
 })
 
 const nextConfig = withBundleAnalyzer({
+  env: {
+    ...defaultEnvVar,
+    ...envVar,
+  },
   images: {
     remotePatterns: imagesDomains.map(domain => ({
       protocol: 'https',
