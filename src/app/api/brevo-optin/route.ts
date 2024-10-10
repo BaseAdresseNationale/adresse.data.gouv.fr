17ceb54b2f5e4c09dee6559ec29bb5e492413247
Guillaume Fay <guillaume_fay@hotmail.com>
2024-10-10 18:00:27 +0200

    feat: add brevo opt-in

---
+++ src/app/api/brevo-optin/route.ts
@@ -0,0 +1,16 @@
+import { customFetch } from '@/lib/fetch'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function POST(request: NextRequest) {
+  const requestBody = await request.json()
+  const { email } = requestBody
+  const options = {
+    method: 'POST',
+    headers: { 'accept': 'application/json', 'content-type': 'application/json', 'api-key': process.env.BREVO_API_KEY } as RequestInit['headers'],
+    body: JSON.stringify({ email, listIds: [10] }),
+  }
+
+  const response = await customFetch(`${process.env.NEXT_PUBLIC_BREVO_API_URL}/contacts`, options)
+
+  return NextResponse.json(response)
+}
