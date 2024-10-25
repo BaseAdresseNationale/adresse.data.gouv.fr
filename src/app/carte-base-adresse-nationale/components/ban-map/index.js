39dff1b07e4ec11d0862e16b99076d29eee5448d
Florent Bourcier <62593305+IGNFBourcier@users.noreply.github.com>
2024-10-25 19:14:47 +0200

    WIP: map content

---
+++ src/app/carte-base-adresse-nationale/components/ban-map/index.js
@@ -0,0 +1,360 @@
+import { useCallback, useContext, useEffect, useMemo, useState } from 'react'
+import PropTypes from 'prop-types'
+import { Layer, Source, useMap, Popup } from 'react-map-gl/maplibre'
+import bboxPolygon from '@turf/bbox-polygon'
+import booleanContains from '@turf/boolean-contains'
+
+import theme from '@/theme'
+const API_BAN_URL = process.env.NEXT_PUBLIC_API_BAN_URL
+import MapLegends from '../map-legends'
+import CadastreLayerControl from '../cadastre-layer-control'
+import CenterControl from '../center-control'
+
+import {
+  adresseCircleLayer,
+  adresseLabelLayer,
+  adresseCompletLabelLayer,
+  voieLayer,
+  toponymeLayer,
+  sources,
+  sourcesLayerPaint,
+  defaultLayerPaint,
+  cadastreLayers,
+  PARCELLES_MINZOOM,
+} from './layers'
+import popupFeatures from './popups'
+import { flatten, forEach } from 'lodash'
+
+let hoveredFeature = null
+
+const SOURCES = ['adresses', 'toponymes']
+const MAX_ZOOM = 19 // Zoom maximum de la carte
+
+const ZOOM_RANGE = {
+  'commune': {
+    min: adresseCircleLayer.minzoom,
+    max: MAX_ZOOM,
+  },
+  'voie': {
+    min: voieLayer.minzoom,
+    max: voieLayer.maxzoom,
+  },
+  'numero': {
+    min: adresseLabelLayer.minzoom,
+    max: MAX_ZOOM,
+  },
+  'lieu-dit': {
+    min: toponymeLayer.minzoom,
+    max: toponymeLayer.maxzoom,
+  },
+}
+
+const certificationLegend = {
+  certified: { name: 'Certifiée', color: theme.successBorder },
+  certificationInProgress: { name: 'Certification cours', color: theme.border },
+  notCertified: { name: 'Non certifiée', color: theme.warningBorder },
+}
+
+const paintLayers = {
+  certification: {
+    name: 'Certification',
+    legend: {
+      title: 'Certification',
+      content: certificationLegend,
+    },
+    paint: defaultLayerPaint,
+  },
+}
+
+const isFeatureContained = (container, content) => {
+  const polygonA = bboxPolygon(container)
+  const polygonB = bboxPolygon(content)
+  return booleanContains(polygonA, polygonB)
+}
+
+const getPositionsFeatures = (address) => {
+  if (address?.positions?.length > 1) {
+    return address.positions.map(p => ({
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: p.position.coordinates,
+      },
+      properties: {
+        ...address,
+        type: p?.positionType || 'inconnu',
+        nomVoie: address.voie.nomVoie,
+      },
+    }))
+  }
+
+  return []
+}
+
+function BanMap({ address, bbox, onSelect, isMobile }) {
+  const map = useMap()
+  const [isCenterControlDisabled, setIsCenterControlDisabled] = useState(true)
+  const [selectedPaintLayer, setSelectedPaintLayer] = useState('certification')
+  const [isCadastreDisplayable, setIsCadastreDisplayble] = useState(true)
+  const [isCadastreLayersShown, setIsCadastreLayersShown] = useState(false)
+  const [bound, setBound] = useState()
+  const [infoPopup, setInfoPopup] = useState(null)
+
+  const highLightAdressesByProperties = useCallback((isHovered, hoveredFeature) => {
+    const { nom, id } = hoveredFeature
+    forEach(SOURCES, (sourceLayer) => {
+      forEach(map.current.querySourceFeatures('base-adresse-nationale', {
+        sourceLayer,
+        filter: [
+          'any',
+          ['in', nom, ['get', 'lieuDitComplementNom']],
+          ['in', id, ['get', 'id']],
+        ],
+      }), ({ id }) => {
+        map.current.setFeatureState({ source: 'base-adresse-nationale', sourceLayer, id }, { hover: isHovered })
+      })
+    })
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+  const onLeave = useCallback(() => {
+    if (hoveredFeature) {
+      highLightAdressesByProperties(false, hoveredFeature)
+    }
+
+    setInfoPopup(null)
+    map.current.getCanvas().style.cursor = 'grab'
+    hoveredFeature = null
+  }, [map, highLightAdressesByProperties])
+
+  const onHover = useCallback((e) => {
+    if (e.features.length > 0) {
+      if (hoveredFeature) {
+        highLightAdressesByProperties(false, hoveredFeature)
+      }
+
+      hoveredFeature = {
+        id: e.features[0].id.split('_').slice(0, 2).join('_'),
+        nom: e.features[0].properties.nomVoie,
+      }
+      highLightAdressesByProperties(true, hoveredFeature)
+
+      map.current.getCanvas().style.cursor = 'pointer'
+
+      setInfoPopup({ ...e })
+    }
+    else {
+      map.current.getCanvas().style.cursor = 'grab'
+      setInfoPopup(null)
+    }
+  }, [map, highLightAdressesByProperties])
+
+  const handleClick = (e, cb) => {
+    const feature = e.features[0]
+    cb(feature.properties)
+  }
+
+  const centerAddress = useCallback(() => {
+    if (bound && !isCenterControlDisabled) {
+      map.current.fitBounds(bound, {
+        padding: 30,
+      })
+      setIsCenterControlDisabled(true)
+    }
+  }, [bound, isCenterControlDisabled, map])
+
+  const isAddressVisible = useCallback(() => {
+    if (address) {
+      const { _sw, _ne } = map.current.getBounds()
+      const mapBBox = [
+        _sw.lng,
+        _sw.lat,
+        _ne.lng,
+        _ne.lat,
+      ]
+
+      const currentZoom = map.current.getZoom()
+      const isAddressInMapBBox = bound ? isFeatureContained(mapBBox, bound) : false
+
+      const isZoomSmallerThanMax = currentZoom <= ZOOM_RANGE[address.type].max
+      const isZoomGreaterThanMin = currentZoom >= ZOOM_RANGE[address.type].min
+      setIsCenterControlDisabled(isAddressInMapBBox && isZoomSmallerThanMax && isZoomGreaterThanMin)
+    }
+    else {
+      setIsCenterControlDisabled(true)
+    }
+  }, [map, address, bound])
+
+  const handleZoom = useCallback(() => {
+    isAddressVisible()
+    const zoom = map.current.getZoom()
+    setIsCadastreDisplayble(zoom < PARCELLES_MINZOOM)
+  }, [map, isAddressVisible])
+
+  useEffect(() => {
+    handleZoom()
+  }, [address, isAddressVisible, handleZoom])
+
+  useEffect(() => {
+    map.current.off('dragend', handleZoom)
+    map.current.off('zoomend', handleZoom)
+
+    map.current.on('dragend', handleZoom)
+    map.current.on('zoomend', handleZoom)
+
+    map.current.on('mousemove', 'adresse', onHover)
+    map.current.on('mousemove', 'adresse-label', onHover)
+    map.current.on('mousemove', 'voie', onHover)
+    map.current.on('mousemove', 'toponyme', onHover)
+
+    map.current.on('mouseleave', 'adresse', onLeave)
+    map.current.on('mouseleave', 'adresse-label', onLeave)
+    map.current.on('mouseleave', 'voie', onLeave)
+    map.current.on('mouseleave', 'toponyme', onLeave)
+
+    map.current.on('click', 'adresse', e => handleClick(e, onSelect))
+    map.current.on('click', 'adresse-label', e => handleClick(e, onSelect))
+    map.current.on('click', 'voie', e => handleClick(e, onSelect))
+    map.current.on('click', 'toponyme', e => handleClick(e, onSelect))
+    return () => {
+      map.current.off('dragend', handleZoom)
+      map.current.off('zoomend', handleZoom)
+
+      map.current.off('mousemove', 'adresse', onHover)
+      map.current.off('mousemove', 'adresse-label', onHover)
+      map.current.off('mousemove', 'voie', onHover)
+      map.current.off('mousemove', 'toponyme', onHover)
+
+      map.current.off('mouseleave', 'adresse', onLeave)
+      map.current.off('mouseleave', 'adresse-label', onLeave)
+      map.current.off('mouseleave', 'voie', onLeave)
+      map.current.off('mouseleave', 'toponyme', onLeave)
+
+      map.current.off('click', 'adresse', e => handleClick(e, onSelect))
+      map.current.off('click', 'adresse-label', e => handleClick(e, onSelect))
+      map.current.off('click', 'voie', e => handleClick(e, onSelect))
+      map.current.off('click', 'toponyme', e => handleClick(e, onSelect))
+    }
+
+    // No dependency in order to mock a didMount and avoid duplicating events.
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+  useEffect(() => {
+    if (bbox) {
+      setBound(flatten(bbox))
+    }
+    else {
+      setBound(address?.displayBBox.length === 4 ? address.displayBBox : undefined)
+    }
+  }, [map, bbox, address])
+
+  const cadastreFiltre = useMemo(() => address?.parcelles ? ['any', ...address.parcelles.map(id => ['==', ['get', 'id'], id])] : ['==', ['get', 'id'], ''], [address?.parcelles])
+
+  const dataPositionJson = useMemo(() => {
+    return {
+      type: 'FeatureCollection',
+      features: getPositionsFeatures(address),
+    }
+  }
+  , [address])
+
+  const [filtreIsAdresse, filtreIsnotAdresse] = useMemo(() =>
+    [['==', ['get', 'id'], address?.id], ['!=', ['get', 'id'], address?.id]]
+  , [address])
+
+  return (
+    <>
+      <div className="maplibregl-ctrl-group maplibregl-ctrl">
+        <CenterControl
+          isDisabled={isCenterControlDisabled}
+          handleClick={centerAddress}
+          isMultiPositions={address?.positions?.length > 1}
+        />
+        <CadastreLayerControl isDisabled={isCadastreDisplayable} isActived={isCadastreLayersShown} handleClick={() => setIsCadastreLayersShown(!isCadastreLayersShown)} />
+      </div>
+      <MapLegends
+        title={paintLayers[selectedPaintLayer].legend.title}
+        legend={paintLayers[selectedPaintLayer].legend.content}
+      />
+
+      { infoPopup?.features && (
+        <Popup closeOnClick={false} closeButton={false} latitude={infoPopup.lngLat.lat} longitude={infoPopup.lngLat.lng}>
+          {popupFeatures(infoPopup?.features || [])}
+        </Popup>
+
+      )}
+      <Source
+        id="cadastre"
+        type="vector"
+        url="https://openmaptiles.geo.data.gouv.fr/data/cadastre.json"
+      >
+        {cadastreLayers.map((cadastreLayerElt) => {
+          if (cadastreLayerElt.id === 'parcelle-highlighted') {
+            cadastreLayerElt.filter = cadastreFiltre
+          }
+
+          return <Layer key={cadastreLayerElt.id} {...cadastreLayerElt} layout={{ ...cadastreLayerElt.layout, visibility: isCadastreLayersShown ? 'visible' : 'none' }} />
+        }
+        )}
+      </Source>
+      <Source
+        id="base-adresse-nationale"
+        type="vector"
+        format="pbf"
+        tiles={[
+          `${API_BAN_URL}/tiles/ban/{z}/{x}/{y}.pbf`,
+        ]}
+        minzoom={10}
+        maxzoom={14}
+        promoteId="id"
+      >
+        <Layer {...adresseCircleLayer} paint={{ ...adresseCircleLayer.paint, 'circle-color': paintLayers[selectedPaintLayer].paint }} />
+        <Layer {...adresseLabelLayer} filter={address?.type === 'numero' ? filtreIsnotAdresse : true} paint={{ ...adresseLabelLayer.paint, 'text-color': paintLayers[selectedPaintLayer].paint }} />
+        <Layer {...adresseCompletLabelLayer} filter={address?.type === 'numero' ? filtreIsAdresse : false} paint={{ ...adresseCompletLabelLayer.paint, 'text-color': paintLayers[selectedPaintLayer].paint }} />
+        <Layer {...voieLayer} />
+        <Layer {...toponymeLayer} />
+      </Source>
+
+      <style jsx>{`
+      .maplibregl-ctrl-group {
+        position: absolute;
+        box-shadow: none;
+        border: 2px solid #dcd8d5;
+        z-index: 2;
+        right: 8px;
+        top: 80px;
+      }
+      `}
+      </style>
+    </>
+  )
+}
+
+BanMap.defaultProps = {
+  address: null,
+  onSelect: () => {},
+  isMobile: false,
+}
+
+BanMap.propTypes = {
+  address: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    position: PropTypes.object,
+    positions: PropTypes.array,
+    parcelles: PropTypes.array,
+    displayBBox: PropTypes.array,
+    lat: PropTypes.number,
+    lon: PropTypes.number,
+    voie: PropTypes.object,
+  }),
+
+  contour: PropTypes.shape({
+    features: PropTypes.array.isRequired,
+  }),
+  onSelect: PropTypes.func,
+
+  isMobile: PropTypes.bool,
+  bbox: PropTypes.arrayOf(PropTypes.number),
+}
+
+export default BanMap
---
+++ src/app/carte-base-adresse-nationale/components/ban-map/layers.js
@@ -0,0 +1,333 @@
+import dsftheme from '@/theme'
+import { fr } from '@codegouvfr/react-dsfr'
+const theme = {
+  successBorder: fr.colors.getHex({ isDark: false }).decisions.border.plain.success.default,
+  warningBorder: fr.colors.getHex({ isDark: false }).decisions.border.plain.warning.default,
+  primary: '#0053B3', // dsftheme.colors.primary.badge,
+  grey: '#ADB9C9', // dsftheme.colors.grey.border,
+}
+
+export const sources = {
+  'bal': { name: 'Base Adresse Locale', color: '#009E73' },
+  'cadastre': { name: 'Cadastre', color: '#56B4E9' },
+  'ftth': { name: 'Opérateur THD', color: '#E69F00' },
+  'ign-api-gestion-ign': { name: 'IGN', color: '#0072B2' },
+  'ign-api-gestion-municipal_administration': { name: 'Guichet Adresse', color: '#CC79A7' },
+}
+
+export const defaultLayerPaint = [
+  'case',
+  ['==', ['get', 'certifie'], true],
+  theme.successBorder,
+  ['==', ['get', 'sourcePosition'], 'bal'],
+  theme.grey,
+  theme.warningBorder,
+]
+
+const NUMEROS_POINT_MIN = 12
+const NUMEROS_MIN = 17
+const VOIE_COLOR = '#4a4a4a'
+const VOIE_MIN = 12
+const VOIE_MAX = NUMEROS_MIN
+const TOPONYME_MIN = 11
+const TOPONYME_MAX = NUMEROS_MIN + 2
+const TOPONYME_COLOR = '#7c5050'
+export const PARCELLES_MINZOOM = 14
+
+export const adresseCircleLayer = {
+  'id': 'adresse',
+  'source': 'base-adresse-nationale',
+  'source-layer': 'adresses',
+  'type': 'circle',
+  'minzoom': NUMEROS_POINT_MIN,
+  'paint': {
+    'circle-color': defaultLayerPaint,
+    'circle-stroke-color': [
+      'case',
+      ['boolean', ['feature-state', 'hover'], false],
+      theme.primary,
+      '#fff',
+    ],
+    'circle-stroke-width': [
+      'case',
+      ['boolean', ['feature-state', 'hover'], false],
+      2,
+      0,
+    ],
+    'circle-radius': {
+      stops: [
+        [12, 0.8],
+        [17, 6],
+      ],
+    },
+  },
+}
+
+export const adresseLabelLayer = {
+  'id': 'adresse-label',
+  'source': 'base-adresse-nationale',
+  'source-layer': 'adresses',
+  'type': 'symbol',
+  'minzoom': NUMEROS_MIN,
+  'paint': {
+    'text-color': defaultLayerPaint,
+    'text-halo-color': [
+      'case',
+      ['boolean', ['feature-state', 'hover'], false],
+      theme.primary,
+      '#fff',
+    ],
+    'text-halo-width': 1,
+  },
+  'layout': {
+    'text-font': ['Noto Sans Bold'],
+    'text-size': {
+      stops: [
+        [NUMEROS_MIN, 13],
+        [19, 16],
+      ],
+    },
+    'text-field': [
+      'case',
+      ['has', 'suffixe'],
+      [
+        'format',
+        ['get', 'numero'],
+        {},
+        ' ',
+        {},
+        ['get', 'suffixe'],
+        {},
+      ],
+      ['get', 'numero'],
+    ],
+    'text-ignore-placement': false,
+    'text-variable-anchor': ['bottom'],
+    'text-radial-offset': 1,
+  },
+}
+
+export const adresseCompletLabelLayer = {
+  'id': 'adresse-complet-label',
+  'source': 'base-adresse-nationale',
+  'source-layer': 'adresses',
+  'type': 'symbol',
+  'minzoom': NUMEROS_MIN,
+  'filter': ['==', ['get', 'id'], ''],
+  'paint': {
+    'text-color': defaultLayerPaint,
+    'text-halo-color': [
+      'case',
+      ['boolean', ['feature-state', 'hover'], false],
+      theme.primary,
+      '#fff',
+    ],
+    'text-halo-width': 1,
+  },
+  'layout': {
+    'text-font': ['Noto Sans Bold'],
+    'text-size': {
+      stops: [
+        [NUMEROS_MIN, 13],
+        [19, 16],
+      ],
+    },
+    'text-field': [
+      'case',
+      ['has', 'suffixe'],
+      [
+        'format',
+        ['get', 'numero'],
+        {},
+        ' ',
+        {},
+        ['get', 'suffixe'],
+        {},
+        ' ',
+        {},
+        ['get', 'nomVoie'],
+      ],
+      [
+        'format',
+        ['get', 'numero'],
+        {},
+        ' ',
+        {},
+        ['get', 'nomVoie'],
+      ],
+    ],
+    'text-ignore-placement': false,
+    'text-variable-anchor': ['bottom'],
+    'text-radial-offset': 1,
+  },
+}
+
+export const voieLayer = {
+  'id': 'voie',
+  'source': 'base-adresse-nationale',
+  'source-layer': 'toponymes',
+  'type': 'symbol',
+  'filter': ['==', ['get', 'type'], 'voie'],
+  'minzoom': VOIE_MIN,
+  'maxzoom': VOIE_MAX,
+  'paint': {
+    'text-color': [
+      'case',
+      ['boolean', ['feature-state', 'hover'], false],
+      '#fff',
+      VOIE_COLOR,
+    ],
+    'text-halo-width': 2,
+    'text-halo-color': [
+      'case',
+      ['boolean', ['feature-state', 'hover'], false],
+      theme.primary,
+      '#fff',
+    ],
+  },
+  'layout': {
+    'text-font': ['Noto Sans Bold'],
+    'text-size': [
+      'step',
+      ['get', 'nbNumeros'],
+      8,
+      20,
+      10,
+      50,
+      14,
+      100,
+      16,
+    ],
+    'text-field': ['get', 'nomVoie'],
+  },
+}
+
+export const toponymeLayer = {
+  'id': 'toponyme',
+  'source': 'base-adresse-nationale',
+  'source-layer': 'toponymes',
+  'type': 'symbol',
+  'filter': ['==', ['get', 'type'], 'lieu-dit'],
+  'minzoom': TOPONYME_MIN,
+  'maxzoom': TOPONYME_MAX,
+  'paint': {
+    'text-color': [
+      'case',
+      ['boolean', ['feature-state', 'hover'], false],
+      '#fff',
+      TOPONYME_COLOR,
+    ],
+    'text-halo-width': 1,
+    'text-halo-color': [
+      'case',
+      ['boolean', ['feature-state', 'hover'], false],
+      theme.primary,
+      '#fff',
+    ],
+  },
+  'layout': {
+    'text-font': ['Noto Sans Bold'],
+    'text-size': {
+      stops: [
+        [0, 3],
+        [10, 15],
+      ],
+    },
+    'text-field': ['get', 'nomVoie'],
+    'text-ignore-placement': false,
+    'text-variable-anchor': ['bottom', 'top', 'right', 'left'],
+    'text-radial-offset': 0.1,
+  },
+}
+
+export const cadastreLayers = [{
+  'id': 'batiments-fill',
+  'type': 'fill',
+  'source': 'cadastre',
+  'source-layer': 'batiments',
+  'minzoom': PARCELLES_MINZOOM,
+  'paint': {
+    'fill-opacity': 0.3,
+  },
+  'layout': {
+    visibility: 'none',
+  },
+},
+{
+  'id': 'batiments-line',
+  'type': 'line',
+  'source': 'cadastre',
+  'source-layer': 'batiments',
+  'minzoom': PARCELLES_MINZOOM,
+  'maxzoom': 22,
+  'layout': {
+    visibility: 'none',
+  },
+  'paint': {
+    'line-opacity': 1,
+    'line-color': 'rgba(0, 0, 0, 1)',
+  },
+},
+{
+  'id': 'parcelles',
+  'type': 'line',
+  'source': 'cadastre',
+  'source-layer': 'parcelles',
+  'minzoom': PARCELLES_MINZOOM,
+  'maxzoom': 24,
+  'layout': {
+    visibility: 'none',
+  },
+  'paint': {
+    'line-color': '#0053b3',
+    'line-opacity': 0.9,
+    'line-width': {
+      stops: [
+        [
+          16,
+          1,
+        ],
+        [
+          17,
+          2,
+        ],
+      ],
+    },
+  },
+},
+{
+  'id': 'parcelle-highlighted',
+  'type': 'fill',
+  'source': 'cadastre',
+  'source-layer': 'parcelles',
+  'filter': ['==', ['get', 'id'], ''],
+  'minzoom': PARCELLES_MINZOOM,
+  'layout': {
+    visibility: 'none',
+  },
+  'paint': {
+    'fill-color': '#0053b3',
+    'fill-opacity': 0.5,
+  },
+},
+{
+  'id': 'code-parcelles',
+  'type': 'symbol',
+  'source': 'cadastre',
+  'source-layer': 'parcelles',
+  'minzoom': PARCELLES_MINZOOM,
+  'layout': {
+    'visibility': 'none',
+    'text-field': '{numero}',
+    'text-font': [
+      'Noto Sans Bold',
+    ],
+    'text-allow-overlap': false,
+    'text-size': 16,
+  },
+  'paint': {
+    'text-halo-color': '#fff6f1',
+    'text-halo-width': 1.5,
+    'text-translate-anchor': 'map',
+  },
+}]
