5b58a234b1e468308bb7d81bbad4840ebdd1b313
Nicolas KOKLA <n.kokla@koaji.fr>
2024-11-12 02:14:29 +0100

    Add `BanMap` Component & types

---
+++ src/app/carte-base-adresse-nationale/components/ban-map/BanMap.styles.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import styled from 'styled-components'
+
+export const ControlGroup = styled.div.attrs({
+  className: 'maplibregl-ctrl-group maplibregl-ctrl',
+})`
+    position: absolute;
+    box-shadow: none;
+    border: 0px solid transparent;
+    z-index: 2;
+    right: 0.6rem;
+    top: 7rem;
+`
---
+++ src/app/carte-base-adresse-nationale/components/ban-map/BanMap.tsx
@@ -0,0 +1,182 @@
+import { useCallback, useEffect, useState, useRef } from 'react'
+import { useMap, Popup } from 'react-map-gl/maplibre'
+
+import CenterControl from './CenterControl'
+import LayerBan from './LayerBan'
+import LayerCadastre from './LayerCadastre'
+import Popups from './Popups'
+
+import { ControlGroup } from './BanMap.styles'
+
+import type { MapMouseEvent, MapGeoJSONFeature, LngLatBoundsLike } from 'react-map-gl/maplibre'
+import type { PopupFeature } from './Popups'
+import type { Address, BBox, PopupInfo } from './types'
+
+interface BanMapProps {
+  address: Address
+  bbox?: BBox
+  onSelect: (properties: any) => void
+  isCadastreLayersShown?: boolean
+}
+
+interface HoveredFeature extends PopupFeature {
+  nom?: string
+  id: string
+}
+
+interface HighLightAdressesByProperties {
+  (isHovered: boolean, hoveredFeature: HoveredFeature): void
+}
+
+const SOURCES = ['adresses', 'toponymes']
+
+function BanMap({ address, bbox, onSelect, isCadastreLayersShown }: BanMapProps) {
+  const hoveredFeature = useRef<PopupFeature | null>(null)
+  const map = useMap()
+  const [bound, setBound] = useState<LngLatBoundsLike>()
+  const [infoPopup, setInfoPopup] = useState<PopupInfo | null>(null)
+
+  const centerAddress = useCallback(() => {
+    if (bound) {
+      map.current?.fitBounds(bound, {
+        padding: 30,
+      })
+    }
+  }, [bound, map])
+
+  useEffect(() => {
+    const currentMap = map.current
+    const itemLayers = ['adresse', 'adresse-label', 'voie', 'toponyme']
+    console.log('CurrentMap Effect', currentMap)
+
+    const highLightAdressesByProperties: HighLightAdressesByProperties = (
+      isHovered: boolean,
+      hoveredFeature: HoveredFeature,
+    ) => {
+      const { nom, id } = hoveredFeature
+      if (!currentMap) return
+
+      SOURCES.forEach((sourceLayer) => {
+        currentMap.querySourceFeatures(
+          'base-adresse-nationale',
+          {
+            sourceLayer,
+            filter: [
+              'any',
+              ['in', nom as string, ['get', 'lieuDitComplementNom']],
+              ['in', id, ['get', 'id']],
+            ],
+          }
+        ).forEach(({ id }) => {
+          currentMap.setFeatureState({ source: 'base-adresse-nationale', sourceLayer, id }, { hover: isHovered })
+        })
+      })
+    }
+
+    const onHover = (evt: MapMouseEvent & { features?: MapGeoJSONFeature[] | undefined } & Object) => {
+      if (currentMap) {
+        if (evt.features && evt.features.length > 0) {
+          if (hoveredFeature.current) {
+            highLightAdressesByProperties(false, hoveredFeature.current)
+          }
+
+          hoveredFeature.current = {
+            id: `${evt.features[0].id}`?.split('_').slice(0, 2).join('_'),
+            nom: evt.features[0].properties.nomVoie,
+          }
+          highLightAdressesByProperties(true, hoveredFeature.current)
+
+          currentMap.getCanvas().style.cursor = 'pointer'
+
+          const geometry = evt.features[0].geometry
+          if (geometry.type === 'Point') {
+            setInfoPopup({
+              features: evt.features,
+              lngLat: {
+                lat: geometry.coordinates[1],
+                lng: geometry.coordinates[0],
+              },
+            })
+          }
+        }
+        else {
+          currentMap.getCanvas().style.cursor = 'grab'
+          setInfoPopup(null)
+        }
+      }
+    }
+
+    const onLeave = () => {
+      if (currentMap) {
+        console.log('onLeave')
+        if (hoveredFeature.current) {
+          highLightAdressesByProperties(false, hoveredFeature.current)
+        }
+
+        setInfoPopup(null)
+        currentMap.getCanvas().style.cursor = 'grab'
+        hoveredFeature.current = null
+      }
+    }
+
+    const handleClick = (
+      evt: MapMouseEvent & { features?: MapGeoJSONFeature[] | undefined },
+      callBack: (properties: MapGeoJSONFeature['properties']) => void
+    ) => {
+      if (evt.features && evt.features.length > 0) {
+        const feature: MapGeoJSONFeature = evt.features[0]
+        callBack(feature.properties)
+      }
+    }
+
+    if (currentMap) {
+      itemLayers.forEach((itemLayer) => {
+        currentMap.on('mouseleave', itemLayer, onLeave)
+        currentMap.on('mousemove', itemLayer, evt => onHover(evt))
+        currentMap.on('click', itemLayer, evt => handleClick(evt, onSelect))
+      })
+    }
+
+    return () => {
+      console.log('CurrentMap Stop Effect', currentMap)
+
+      if (currentMap) {
+        itemLayers.forEach((itemLayer) => {
+          currentMap.off('mouseleave', itemLayer, onLeave)
+          currentMap.off('mousemove', itemLayer, onHover)
+          currentMap.off('click', itemLayer, e => handleClick(e, onSelect))
+        })
+      }
+    }
+  }, [map, onSelect])
+
+  useEffect(() => {
+    if (bbox) {
+      setBound(bbox.flat() as LngLatBoundsLike)
+    }
+    else {
+      setBound(address?.displayBBox?.length === 4 ? (address.displayBBox as LngLatBoundsLike) : undefined)
+    }
+  }, [map, bbox, address])
+
+  return (
+    <>
+      <ControlGroup>
+        <CenterControl
+          handleClick={centerAddress}
+        />
+      </ControlGroup>
+
+      { infoPopup?.features && (
+        <Popup closeOnClick={false} closeButton={false} latitude={infoPopup.lngLat.lat} longitude={infoPopup.lngLat.lng}>
+          <Popups features={infoPopup?.features as unknown as PopupFeature[] || []} />
+        </Popup>
+      )}
+
+      <LayerBan address={address} />
+      <LayerCadastre address={address} isVisible={isCadastreLayersShown} />
+    </>
+  )
+}
+
+export default BanMap
