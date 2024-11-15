1fd174ee8b6ef00037a433fd63c66f1d9647409b
Nicolas KOKLA <n.kokla@koaji.fr>
2024-11-15 19:34:04 +0100

    Fix Code indentation, UI and Motion-design

--- src/components/Section/Section.styles.tsx
+++ src/components/Section/Section.styles.tsx
@@ -4,15 +4,17 @@ import styled, { css } from 'styled-components'
 
 export const SectionBlock = styled.section<{ $isVisible?: boolean }>`
   ${({ $isVisible }) => typeof $isVisible === 'boolean' && css`
-    max-height: 100%;
+    max-height: 3000px;
     opacity: 1;
     overflow-y: hidden;
-    transition: ease .5s;
-    transition-property: max-height opacity;
+    transition: max-height ease-in .5s,
+      opacity ease-in .5s;
   `}
   ${({ $isVisible }) => $isVisible === false && css`
     max-height: 0;
     opacity: 0;
+    transition: max-height ease-out .5s,
+      opacity ease-in-out .5s;
   `}
 `
 
