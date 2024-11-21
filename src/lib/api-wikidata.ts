5b269a35c7e7ecf3f071711f36bc09b95a2d1036
Jugurtha Bouhadoun <jugurtha.bouhadoun@ign.fr>
2024-11-21 18:01:10 +0100

    use wikidata logo

--- src/lib/api-wikidata.ts
+++ src/lib/api-wikidata.ts
@@ -43,3 +43,32 @@ export const getCommuneFlag = async (codeCommune: string): Promise<string | unde
 
   return flagUrl
 }
+
+const communeLogoQuery = (codeCommune: string) => `
+    SELECT  ?city
+            ?inseeCode
+            ?cityLabel
+            ?logo
+    WHERE
+    {
+        ?city wdt:P374 "${codeCommune}".
+        ?city wdt:P374 ?inseeCode.
+        OPTIONAL { ?city wdt:P154 ?logo. }  # Récupère le logo de la ville si disponible
+        SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+    }
+    ORDER BY ?inseeCode
+`
+
+export const getCommuneLogo = async (codeCommune: string): Promise<string | undefined> => {
+  const url = `${BASE_URL}/sparql?query=${encodeURIComponent(communeLogoQuery(codeCommune))}`
+  // Wikidata blocks requests without a user agent specific enough
+  const response = await fetch(url, {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
+    },
+  })
+  const responseBody = await response.text()
+  const flagUrl = responseBody.match(/http.*\.png/)?.[0]
+
+  return flagUrl
+}
