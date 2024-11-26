b086a12920073828cfa269dbddaeffd101749bf0
MelFonk <melvin.pichenot@ign.fr>
2024-11-26 15:25:09 +0100

    page geocodeur csv fonctionnelle

--- src/app/outils/csv/components/selectable-item-list.tsx
+++ src/app/outils/csv/components/selectable-item-list.tsx
@@ -5,10 +5,7 @@ import theme from '@/theme/theme'
 import ActionButtonNeutral from '../components/action-button-neutral'
 
 interface SelectableItemListPropType {
-  list: Array<{
-    key: string
-    value: string
-  }>
+  list: Array<{ key: string, value: string }>
   buttonIcon: '+' | '-'
   action: Function
 }
@@ -17,12 +14,12 @@ export default function SelectableItemList({ list, buttonIcon, action }: Selecta
   return (
     <div>
       <div className={`${list.length > 0 && 'list selection'}`}>
-        {list.map(item => (
+        {list.map(item => typeof (item.value) === 'string' && (
           <ActionButtonNeutral
             label={buttonIcon === '+' ? `Sélectionner ${item.value}` : `Désélectionner ${item.value}`}
             key={item.key}
             isFullSize
-            onClick={action(item)}
+            onClick={() => action(item.value)}
           >
             <div className="item">
               <div className="text">{item.value}</div>
