3e59d603f3f4d16bd1ed88af58a1f9a97e10d1b1
Guillaume Fay <guillaume_fay@hotmail.com>
2024-11-27 17:47:16 +0000

    Gfay feat page ressource bis (#1909)
    * feat: add ressource page
    
    * feat: update page ressource
    
    * fix: review jules

--- src/components/ResponsiveImage/index.tsx
+++ src/components/ResponsiveImage/index.tsx
@@ -4,15 +4,17 @@ export interface ResponsiveImageProps {
   src: string
   alt: string
   style?: React.CSSProperties
+  className?: string
 }
 
-export default function ResponsiveImage({ src, alt, style }: ResponsiveImageProps) {
+export default function ResponsiveImage({ src, alt, style, className }: ResponsiveImageProps) {
   return (
     <Image
       src={src}
       alt={alt}
       width={0}
       height={0}
+      className={className}
       sizes="100vw"
       style={{ width: '100%', height: 'auto', ...style }}
     />
