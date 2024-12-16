16b66c1b3f660cbe7f87be648bcd3f5309f51362
Vincent Sagniez <vincent.sagniez@ign.fr>
2024-12-16 09:49:45 +0100

    Correction eslint

--- src/app/ressources-et-documentations/page.tsx
+++ src/app/ressources-et-documentations/page.tsx
@@ -14,7 +14,7 @@ export default async function DocumentationPage() {
 
   return (
     <>
-       <Suspense fallback={<p>Chargement...</p>}>
+      <Suspense fallback={<p>Chargement...</p>}>
         {heroContentHtml
         && (
           <SectionHero
