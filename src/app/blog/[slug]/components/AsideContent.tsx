32596a1e924501b95ab79477d63f44444c8ac0d0
Vincent Sagniez <vincent.sagniez@ign.fr>
2024-12-19 08:37:50 +0100

    Modif texte facebook

--- src/app/blog/[slug]/components/AsideContent.tsx
+++ src/app/blog/[slug]/components/AsideContent.tsx
@@ -32,7 +32,7 @@ async function AsideContent() {
         <li><Link className="fr-link fr-link--icon-left fr-icon-twitter-x-fill" target="twitter-x" href={SOCIAL_NETWORKS_URL_XCOM ?? ''}>X.com</Link></li>
         <li><Link className="fr-link fr-link--icon-left fr-icon-mastodon-fill" target="mastodon" href={SOCIAL_NETWORKS_URL_MASTODON ?? ''}>Mastodon</Link></li>
         <li><Link className="fr-link fr-link--icon-left ri-mail-send-fill" target="newsletter" href="#newsletter">Newsletter</Link></li>
-        <li><Link className="fr-link fr-link--icon-left fr-icon-facebook-circle-fill" target="facebook" href={SOCIAL_NETWORKS_URL_FACEBOOK ?? ''}>Page Facebook du programme BAL</Link></li>
+        <li><Link className="fr-link fr-link--icon-left fr-icon-facebook-circle-fill" target="facebook" href={SOCIAL_NETWORKS_URL_FACEBOOK ?? ''}>Facebook</Link></li>
       </AsideFollowList>
     </>
   )
