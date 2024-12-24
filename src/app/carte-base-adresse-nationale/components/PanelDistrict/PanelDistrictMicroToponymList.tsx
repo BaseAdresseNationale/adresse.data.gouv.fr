ce1435596ea009809ef213aa84bb6e50cd7eb755
Nicolas KOKLA <n.kokla@koaji.fr>
2024-12-24 14:48:21 +0000

    Panel District : Add search MicroToponym

--- src/app/carte-base-adresse-nationale/components/PanelDistrict/PanelDistrictMicroToponymList.tsx
+++ src/app/carte-base-adresse-nationale/components/PanelDistrict/PanelDistrictMicroToponymList.tsx
@@ -1,12 +1,13 @@
-import { useState, useMemo } from 'react'
+import { useState, useMemo, useEffect } from 'react'
 import Link from 'next/link'
+import { env } from 'next-runtime-env'
 import { ToggleSwitch } from '@codegouvfr/react-dsfr/ToggleSwitch'
 import { Table } from '@codegouvfr/react-dsfr/Table'
+import { Input } from '@codegouvfr/react-dsfr/Input'
 
-import { DistrictMicroToponymListInfo } from './PanelDistrictMicroToponymList.styles'
+import { DistrictMicroToponymListInfo, ClearInputButton } from './PanelDistrictMicroToponymList.styles'
 
 import type { TypeDistrictExtended } from '../../types/LegacyBan.types'
-import { env } from 'next-runtime-env'
 
 const URL_CARTOGRAPHY_BAN = env('NEXT_PUBLIC_URL_CARTOGRAPHY_BAN')
 
@@ -19,31 +20,62 @@ const sortMicroToponymes = (
   { nomVoie: nomVoieB }: { nomVoie: string },
 ) => (nomVoieA.localeCompare(nomVoieB))
 
+const deburr = (str: string) => str.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
 const testMicroTopoWithAddress = ({ nbNumeros }: { nbNumeros: number }) => nbNumeros && nbNumeros > 0
 const testMicroTopoWithoutAddress = ({ nbNumeros }: { nbNumeros: number }) => !nbNumeros || nbNumeros === 0
 
 function PanelDistrictMicroToponymList({ district }: PanelDistrictMicroToponymListProps) {
+  const microToponymes = useMemo(() => (district?.voies || []).toSorted(sortMicroToponymes), [district])
   const [isMicroTopoWithAddressVisible, setIsMicroTopoWithAddressVisible] = useState(true)
   const [isMicroTopoWithoutAddressVisible, setIsMicroTopoWithoutAddressVisible] = useState(true)
+  const [filtredMicroToponymes, setFiltredMicroToponymes] = useState(microToponymes)
+  const [search, setSearch] = useState('')
 
   const MicroTopoWithAddress = useMemo(
-    () => (district?.voies || []).filter(testMicroTopoWithAddress).toSorted(sortMicroToponymes)
-    , [district?.voies])
+    () => filtredMicroToponymes.filter(testMicroTopoWithAddress)
+    , [filtredMicroToponymes])
   const MicroTopoWithoutAddress = useMemo(
-    () => (district?.voies || []).filter(testMicroTopoWithoutAddress).toSorted(sortMicroToponymes)
-    , [district?.voies])
+    () => filtredMicroToponymes.filter(testMicroTopoWithoutAddress)
+    , [filtredMicroToponymes])
 
-  const microToponymes = useMemo(() =>
-    [
+  const visibleMicroToponymes = useMemo(
+    () => [
       ...(isMicroTopoWithAddressVisible ? MicroTopoWithAddress : []),
       ...(isMicroTopoWithoutAddressVisible ? MicroTopoWithoutAddress : []),
-    ].toSorted(sortMicroToponymes)
-  , [
-    MicroTopoWithAddress,
-    MicroTopoWithoutAddress,
-    isMicroTopoWithAddressVisible,
-    isMicroTopoWithoutAddressVisible,
-  ])
+    ]
+    , [
+      MicroTopoWithAddress,
+      MicroTopoWithoutAddress,
+      isMicroTopoWithAddressVisible,
+      isMicroTopoWithoutAddressVisible,
+    ])
+
+  useEffect(() => {
+    if (search === '') {
+      setFiltredMicroToponymes(microToponymes)
+    }
+    else {
+      const voies = microToponymes.filter(
+        (voie: any) => deburr(voie.nomVoie.toLowerCase())
+          .includes(deburr(search.toLowerCase()))
+      ).sort(
+        // Sort by position of 'search value' in each word of the name
+        (voieA: any, voieB: any) => {
+          const nameA = deburr(voieA.nomVoie.toLowerCase())
+          const nameB = deburr(voieB.nomVoie.toLowerCase())
+          const searchLower = deburr(search.toLowerCase())
+          const positionA = nameA.split(' ').map((word: string) => word.indexOf(searchLower)).sort().filter((position: number) => position !== -1)
+          const positionB = nameB.split(' ').map((word: string) => word.indexOf(searchLower)).sort().filter((position: number) => position !== -1)
+          if (positionA.length === 0 && positionB.length === 0) return 0
+          if (positionA.length === 0) return 1
+          if (positionB.length === 0) return -1
+          return positionA[0] - positionB[0]
+        }
+      )
+      setFiltredMicroToponymes(voies)
+      console.log('voies', voies)
+    }
+  }, [microToponymes, search])
 
   return (
     <>
@@ -54,6 +86,18 @@ function PanelDistrictMicroToponymList({ district }: PanelDistrictMicroToponymLi
       <DistrictMicroToponymListInfo>
         Les odonymes correspondent aux voies, places, lieux-dits, et autres micro-toponymes circulables et/ou porteurs d’adresses.
       </DistrictMicroToponymListInfo>
+
+      <Input
+        label="Filtrer les odonymes"
+        iconId="fr-icon-search-line"
+        addon={search && <ClearInputButton onClick={() => setSearch('')}>X</ClearInputButton>}
+        nativeInputProps={{
+          placeholder: 'Nom de voie, place ou lieu-dit',
+          value: search,
+          onChange: (evt: React.ChangeEvent<HTMLInputElement>) => setSearch(evt.target.value),
+        }}
+      />
+
       <ToggleSwitch
         label={<>{MicroTopoWithAddress.length} odonymes avec adresses</>}
         labelPosition="right"
@@ -73,11 +117,11 @@ function PanelDistrictMicroToponymList({ district }: PanelDistrictMicroToponymLi
 
       <Table
         headers={[
-          <>Odonyme ({microToponymes.length})</>,
+          <>Odonyme ({visibleMicroToponymes.length})</>,
           'Adresse',
         ]}
         data={
-          microToponymes.map((voie: any) => (
+          visibleMicroToponymes.map((voie: any) => (
             [
               <span key={voie.id}>
                 <Link href={`${URL_CARTOGRAPHY_BAN}?id=${voie.id}`}>{voie.nomVoie}</Link>
