1cf41fc05f2cf8ee9f7707df1e0ba6d3de3277da
Nicolas KOKLA <n.kokla@koaji.fr>
2025-01-13 23:00:27 +0100

    Fix some code for HtmlViewer Style

--- src/components/HtmlViewer/HtmlViewer.styles.tsx
+++ src/components/HtmlViewer/HtmlViewer.styles.tsx
@@ -22,6 +22,35 @@ export const TextWrapper = styled.div`
       }
     }
 
+    p {
+      width: 100%;
+    }
+
+    a {
+      /* From DSFR class : 'fr-link' */
+      --text-spacing: 0;
+      --title-spacing: 0;
+      display: inline;
+      font-size: 1rem;
+      line-height: 1.5rem;
+      padding: 0;
+      color: var(--text-action-high-blue-france);
+    }
+
+    pre {
+      max-width: 100%;
+      width: calc(100vw - 2rem - 2rem);
+      margin: 0;
+      padding: 1rem;
+      overflow: auto;
+      background: #efefef;
+    }
+
+    code {
+      font-family: monospace;
+      font-size: 0.9em;
+    }
+
     img,
     video {
       max-width: 100%;
