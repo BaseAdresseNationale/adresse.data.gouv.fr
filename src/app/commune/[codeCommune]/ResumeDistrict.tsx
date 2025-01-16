e55a3cc26e6bafa4bec6c4c3cc5bb650d16c99e7
Guillaume Fay <guillaume_fay@hotmail.com>
2025-01-16 18:16:26 +0100

    feat: downloads

--- src/app/commune/[codeCommune]/ResumeDistrict.tsx
+++ src/app/commune/[codeCommune]/ResumeDistrict.tsx
@@ -1,11 +1,14 @@
 'use client'
 
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
 import { Button } from '@codegouvfr/react-dsfr/Button'
 
 import {
   ResumeDistrictWrapper,
   ResumeDistrictActionsWrapper,
+  CertificatAdressageOptInActionsWrapper,
+  StyledIframeWrapper,
+  StyledIframe,
 } from './ResumeDistrict.styles'
 import { BANCommune } from '@/types/api-ban.types'
 import Section from '@/components/Section'
@@ -18,44 +21,87 @@ interface ResumeDistrictProps {
 }
 
 function ResumeDistrict({ district, actionProps }: ResumeDistrictProps) {
+  const [isConfigDistrictVisible, setIsConfigDistrictVisible] = useState(false)
+  const iframeSRC = 'https://grist.numerique.gouv.fr/o/ban/forms/4eCgRqqpyXW5FMoZzQ3nNm/4'
+
   const { banId, codeCommune } = district
 
   return (
-    <Section>
-      <ResumeDistrictWrapper>
-        <ul>
-          <li className="ri-key-line">Code INSEE : <b>{codeCommune}</b></li>
-          <li className="ri-signpost-line">
+    <>
+      <link href={iframeSRC} rel="prefetch" />
+      <Section>
+        <ResumeDistrictWrapper>
+          <div className="resume-card">
+            <i className="ri-key-line" />
+            Code INSEE
+            <div>
+              <b>{codeCommune}</b>
+            </div>
+          </div>
+
+          <div className="resume-card">
+            <i className="ri-signpost-line" />
+
             {
               Number(district.codesPostaux.length) > 1
                 ? <><b>{formatNumber(district.codesPostaux.length)}</b>&nbsp;codes Postaux</>
                 : <>{district.codesPostaux.length || 'Aucune'} code Postal</>
             }{' '}
-            <b>({district.codesPostaux.map(formatNumber).join(', ')})</b>
-          </li>
+            <div>
+              <b>{district.codesPostaux.map(formatNumber).join(', ')}</b>
+            </div>
+          </div>
 
-          {banId && <li className="ri-key-line">Code(s) BAN : <b>{banId}</b></li>}
-        </ul>
-      </ResumeDistrictWrapper>
+          {banId && (
+            <div className="resume-card">
+              <i className="ri-key-line" />
+              Code BAN :
 
-      <ResumeDistrictActionsWrapper>
-        {actionProps && actionProps.length && actionProps.map(props => (
-          <Fragment key={props.iconId}>
+              <div><b>{banId}</b></div>
+            </div>
+          )}
+        </ResumeDistrictWrapper>
+
+        <ResumeDistrictActionsWrapper>
+          {actionProps && actionProps.length && actionProps.map(props => (
+            <Fragment key={props.iconId}>
+              <Button
+                iconId={props.iconId}
+                linkProps={{
+                  href: props.linkProps.href,
+                  target: props.linkProps.target,
+                }}
+                priority={props.priority}
+              >
+                {props.value}
+              </Button>
+              {' '}
+            </Fragment>
+          ))}
+          <CertificatAdressageOptInActionsWrapper>
             <Button
-              iconId={props.iconId}
-              linkProps={{
-                href: props.linkProps.href,
-                target: props.linkProps.target,
-              }}
-              priority={props.priority}
+              key="set-config"
+              iconId="ri-settings-line"
+              onClick={() => setIsConfigDistrictVisible(!isConfigDistrictVisible)}
             >
-              {props.value}
+              Configurer les options de la commune
             </Button>
-            {' '}
-          </Fragment>
-        ))}
-      </ResumeDistrictActionsWrapper>
-    </Section>
+          </CertificatAdressageOptInActionsWrapper>
+        </ResumeDistrictActionsWrapper>
+      </Section>
+      <Section title={`Configuration des options pour la commune de ${district.nomCommune}`} theme="grey" isVisible={isConfigDistrictVisible}>
+        <p>
+          <i>
+            Cette fonctionnalité est en développement.
+            Vous pouvez vous inscrire en liste d’attente pour l’activation
+            du certificat d’adressage pour votre commune.
+          </i>
+        </p>
+        <StyledIframeWrapper>
+          <StyledIframe src={iframeSRC} width="100%" height="800" frameBorder="0" />
+        </StyledIframeWrapper>
+      </Section>
+    </>
   )
 }
 
