16b16d18e12b7a47c0fb379b0cefd72777ae2bd1
Jugurtha Bouhadoun <jugurtha.bouhadoun@ign.fr>
2025-02-21 11:32:42 +0100

    fix error blue sky vars on prod env

--- src/app/.well-known/atproto-did/route.tsx
+++ src/app/.well-known/atproto-did/route.tsx
@@ -1,7 +1,6 @@
 import { env } from 'next-runtime-env'
 
-const BLUESKY_KEY_DNS = env('NEXT_PUBLIC_SOCIAL_NETWORKS_BLUESKY_KEY_DNS')
-
 export async function GET(request: Request) {
-  return new Response(`${BLUESKY_KEY_DNS}`)
+    const BLUESKY_KEY_DNS = env('NEXT_PUBLIC_SOCIAL_NETWORKS_BLUESKY_KEY_DNS')
+    return new Response(`${BLUESKY_KEY_DNS}`, { status: 200 })
 }
