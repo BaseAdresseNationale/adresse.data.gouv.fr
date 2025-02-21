35cdd62f5b25fb955b26d7644027c33909cee33f
Jugurtha Bouhadoun <jugurtha.bouhadoun@ign.fr>
2025-02-21 11:14:17 +0100

    debug error well-known page

--- src/app/.well-known/atproto-did/route.tsx
+++ src/app/.well-known/atproto-did/route.tsx
@@ -1,20 +1,6 @@
 import { env } from 'next-runtime-env'
 
 export async function GET(request: Request) {
-  try {
     const BLUESKY_KEY_DNS = env('NEXT_PUBLIC_SOCIAL_NETWORKS_BLUESKY_KEY_DNS')
-
-    if (!BLUESKY_KEY_DNS) {
-      throw new Error('Environment variable NEXT_PUBLIC_SOCIAL_NETWORKS_BLUESKY_KEY_DNS is not defined')
-    }
-
-    return new Response(`test:${BLUESKY_KEY_DNS}`, { status: 200 })
-  } catch (error) {
-    console.error('Error in GET /.well-known/atproto-did:', error)
-
-    return new Response(
-      JSON.stringify({ error: 'Internal Server Error', message: error instanceof Error ? error.message : 'Unknown error' }),
-      { status: 500, headers: { 'Content-Type': 'application/json' } }
-    )
-  }
+    return new Response(`${BLUESKY_KEY_DNS}`, { status: 200 })
 }
