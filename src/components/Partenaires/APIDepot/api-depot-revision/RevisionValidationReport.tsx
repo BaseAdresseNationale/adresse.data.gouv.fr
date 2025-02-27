32f19f158b5dbf9cd03f700869560ebfe35e5031
Guillaume Fay <guillaume_fay@hotmail.com>
2025-02-27 17:02:29 -0600

    review fufeck

--- src/components/Partenaires/APIDepot/api-depot-revision/RevisionValidationReport.tsx
+++ src/components/Partenaires/APIDepot/api-depot-revision/RevisionValidationReport.tsx
@@ -19,46 +19,48 @@ interface RevisionValidationReportProps {
 }
 
 export default function RevisionValidationReport({ validationReport }: RevisionValidationReportProps) {
-  return (
-    <StyledWrapper>
-      <div className="header">
-        <div>Statut : {validationReport.valid ? <Badge severity="success">Valide</Badge> : <Badge severity="error">Invalide</Badge>}</div>
-        <div>Version du validateur : <b>{validationReport.validatorVersion}</b></div>
-      </div>
-      <p>Nombre de lignes dans le fichier BAL : <b>{validationReport.rowsCount}</b></p>
-      <Alert
-        severity="error"
-        title={`Erreur(s) bloquante(s) : ${validationReport.errors.length}`}
-        description={(
-          <ul>
-            {validationReport.errors.map((error, index) => (
-              <li key={index}>{getLabel(error)}</li>
-            ))}
-          </ul>
-        )}
-      />
-      <Alert
-        severity="warning"
-        title={`Avertissement(s) : ${validationReport.warnings.length}`}
-        description={(
-          <ul>
-            {validationReport.warnings.map((warning, index) => (
-              <li key={index}>{getLabel(warning)}</li>
-            ))}
-          </ul>
-        )}
-      />
-      <Alert
-        severity="info"
-        title={`Information(s) : ${validationReport.infos.length}`}
-        description={(
-          <ul>
-            {validationReport.infos.map((info, index) => (
-              <li key={index}>{getLabel(info)}</li>
-            ))}
-          </ul>
-        )}
-      />
-    </StyledWrapper>
-  )
+  return validationReport
+    ? (
+        <StyledWrapper>
+          <div className="header">
+            <div>Statut : {validationReport.valid ? <Badge severity="success">Valide</Badge> : <Badge severity="error">Invalide</Badge>}</div>
+            <div>Version du validateur : <b>{validationReport.validatorVersion}</b></div>
+          </div>
+          <p>Nombre de lignes dans le fichier BAL : <b>{validationReport.rowsCount}</b></p>
+          <Alert
+            severity="error"
+            title={`Erreur(s) bloquante(s) : ${validationReport.errors.length}`}
+            description={(
+              <ul>
+                {validationReport.errors.map((error, index) => (
+                  <li key={index}>{getLabel(error)}</li>
+                ))}
+              </ul>
+            )}
+          />
+          <Alert
+            severity="warning"
+            title={`Avertissement(s) : ${validationReport.warnings.length}`}
+            description={(
+              <ul>
+                {validationReport.warnings.map((warning, index) => (
+                  <li key={index}>{getLabel(warning)}</li>
+                ))}
+              </ul>
+            )}
+          />
+          <Alert
+            severity="info"
+            title={`Information(s) : ${validationReport.infos.length}`}
+            description={(
+              <ul>
+                {validationReport.infos.map((info, index) => (
+                  <li key={index}>{getLabel(info)}</li>
+                ))}
+              </ul>
+            )}
+          />
+        </StyledWrapper>
+      )
+    : null
 }
