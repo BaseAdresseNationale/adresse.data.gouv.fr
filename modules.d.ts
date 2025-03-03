3f797d7e4228ad23c22dd091b7d409c6fe5f122e
Fufeck <fabien.tafforeau@gmail.com>
2025-03-03 10:04:26 +0100

    correct import

--- modules.d.ts
+++ modules.d.ts
@@ -3,4 +3,3 @@ declare module 'papaparse';
 declare module 'chardet';
 declare module 'blob-to-buffer';
 declare module 'react-js-pagination'
-declare module '@ban-team/validateur-bal'
--- src/components/ValidateurBAL/ValidationCodeTable.tsx
+++ src/components/ValidateurBAL/ValidationCodeTable.tsx
@@ -1,15 +1,12 @@
-import Accordion from '@codegouvfr/react-dsfr/Accordion'
-import { getLabel } from '@ban-team/validateur-bal'
 import Table from '@codegouvfr/react-dsfr/Table'
-import { ValidationRow } from './ValidationSummary'
+import { ValidateRowType } from '@ban-team/validateur-bal'
 import styled from 'styled-components'
-import { useCallback, useMemo, useState } from 'react'
-import Pagination from '@codegouvfr/react-dsfr/Pagination'
+import { useMemo, useState } from 'react'
 import SoftPagination from '../Pagination/soft-pagination'
 
 type ValidationCodeTableProps = {
   code: string
-  groupCode: ValidationRow[]
+  groupCode: ValidateRowType[]
 }
 
 const StyledWrapper = styled.div`
