2e4b6be75d7c7b436cfc36326dde199a724ce1dc
Guillaume Fay <guillaume_fay@hotmail.com>
2025-03-21 09:09:10 -0600

    feat: download newsletter on startup (#2087)
    * feat: download newsletter on startup
    
    * fix: 404 on scalingo
    
    * change newsletter directory to data
    
    * fix: only download newsletters in production

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,6 +1,13 @@
+import { downloadLastNewsletters } from './utils/newsletters'
 import { downloadContoursCommunes } from './utils/contours-communes'
+import { env } from 'next-runtime-env'
 
 // This function is called once when the application starts
 export async function register() {
   await downloadContoursCommunes()
+  // Only download newsletters in production
+  // to avoid slowing down the development environment
+  if (env('NODE_ENV') === 'production') {
+    await downloadLastNewsletters()
+  }
 }
