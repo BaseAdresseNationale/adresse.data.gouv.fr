dd65950e5ec4e7972295c9b8a8856bca0b751a5d
Fufeck <fabien.tafforeau@gmail.com>
2025-04-09 10:27:27 +0200

    add link to download

--- src/components/MiseEnForme/RemediationReport.tsx
+++ src/components/MiseEnForme/RemediationReport.tsx
@@ -1,7 +1,9 @@
 import { Alert } from '@codegouvfr/react-dsfr/Alert'
 import Section from '../Section'
-import { ValidateType } from '@ban-team/validateur-bal'
+import { autofix, ValidateType } from '@ban-team/validateur-bal'
 import RemediationTable from './RemediationRows'
+import Button from '@codegouvfr/react-dsfr/Button'
+import { useRef } from 'react'
 
 interface RemediationReportProps {
   file: File
@@ -9,9 +11,35 @@ interface RemediationReportProps {
 }
 
 function RemediationReport({ file, report }: RemediationReportProps) {
+  const linkRef = useRef<HTMLAnchorElement | null>(null);
   const { rows } = report
 
   const nbRowsRemediation = rows.reduce((acc, value) => Object.keys(value.remediations).length > 0 ? acc + 1 : acc, 0)
+
+  const handleClick = async () => {
+    if (!file) {
+      throw new Error('No file selected')
+    }
+    try {
+      const buffer = await autofix(file as any)
+      const blob = new Blob([buffer], { type: 'application/octet-stream' })
+
+      const url = URL.createObjectURL(blob)
+
+      if (linkRef.current) {
+        linkRef.current.href = url
+        linkRef.current.download = 'bal-mise-en-forme.csv'
+        linkRef.current.click()
+      }
+
+      setTimeout(() => {
+        URL.revokeObjectURL(url)
+      }, 1000)
+    }
+    catch (e) {
+      console.error(e)
+    }
+  }
 
   return (
     <>
@@ -25,12 +53,22 @@ function RemediationReport({ file, report }: RemediationReportProps) {
           )
         : (
             <Alert
-              description={`Il y a ${nbRowsRemediation} ligne(s) qui peuvent être mises en formes`}
+              description={(
+                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'end' }}>
+                  <p>Il y a {nbRowsRemediation} ligne(s) qui peuvent être mises en formes</p>
+                  <Button
+                    iconId="fr-icon-download-line"
+                    onClick={handleClick}
+                    title="Télécharger"
+                  />
+                </div>
+              )}
               severity="info"
               title="Mise en forme disponible"
             />
           )}
       {rows && <RemediationTable rows={rows} /> }
+      <a ref={linkRef} style={{ display: 'none' }}>Download</a>
     </>
   )
 }
