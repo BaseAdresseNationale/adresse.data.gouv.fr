80166e635a8094f2e6e7ab142c60e9a3f6b24835
anatole <anatole.hornn@gmail.com>
2025-05-12 12:42:59 +0200

    add ProConnect exchange.

---
+++ src/app/api/login-callback/route.ts
@@ -0,0 +1,44 @@
+import * as client from 'openid-client'
+import { NextRequest, NextResponse } from 'next/server'
+import { getProviderConfig, getCurrentUrl } from '@/utils/oauth'
+import { cookies } from 'next/headers'
+import { configOptions } from '../../../utils/oauth'
+
+export const dynamic = 'force-dynamic'
+
+export async function GET(req: NextRequest) {
+  try {
+    const config = await getProviderConfig()
+    const currentUrl = getCurrentUrl(req)
+    const cookieStore = cookies()
+    const nonce = cookieStore.get('nonce')
+    const state = cookieStore.get('state')
+    const tokens = await client.authorizationCodeGrant(
+      config,
+      currentUrl,
+      {
+        expectedNonce: nonce?.value,
+        expectedState: state?.value,
+      },
+      undefined,
+      configOptions as client.AuthorizationCodeGrantOptions
+    )
+    cookieStore.delete('nonce')
+    cookieStore.delete('state')
+    const claims = tokens.claims()
+
+    if (!claims) throw new Error('Claims are undefined')
+    // return NextResponse.json({ error: 'Claims are undefined' })
+    const userinfo = await client.fetchUserInfo(config, tokens.access_token, claims.sub, undefined)
+    cookieStore.set('userinfo', JSON.stringify(userinfo), { httpOnly: true })
+    cookieStore.set('idtoken', JSON.stringify(claims), { httpOnly: true })
+    cookieStore.set('id_token_hint', JSON.stringify(tokens.id_token), { httpOnly: true })
+    cookieStore.set('oauth2token', JSON.stringify(tokens), { httpOnly: true })
+    return NextResponse.redirect('/')
+  }
+  catch (error) {
+    console.error(error)
+    // return NextResponse.json({ error: 'Failed to initiate login' })
+    return NextResponse.json({ error: 'Callback failed' }, { status: 500 })
+  }
+}
