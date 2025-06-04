1cfb06b3e7fa9d88c19a8f8140c3477f97048de6
Guillaume Fay <guillaume_fay@hotmail.com>
2025-06-04 10:50:05 +0200

    remove dangerouslySetInnerHTMML

--- src/app/carte-base-adresse-nationale/components/PanelAddress/ActionComponents/ActionSignalementAddress.tsx
+++ src/app/carte-base-adresse-nationale/components/PanelAddress/ActionComponents/ActionSignalementAddress.tsx
@@ -36,7 +36,7 @@ const ActionSignalementAddress: React.FC<ActionSignalementAddressProps> = ({ add
           <ActionMessage $isVisible={isExtended}>
             {disabled
               ? (
-                  <div dangerouslySetInnerHTML={{ __html: disabledMessage || '' }} />
+                  disabledMessage
                 )
               : (
                   <>
