b822e4514b2cfc308329a3fa3dc5c500cdf8e162
anatole <anatole.hornn@gmail.com>
2025-06-10 10:12:34 +0200

    fix update commune page stored after visiting another one.

--- src/components/SaveUrlClient/SaveUrlClient.tsx
+++ src/components/SaveUrlClient/SaveUrlClient.tsx
@@ -3,21 +3,16 @@
 import { useEffect, useState } from 'react'
 
 export default function SaveUrlClient() {
-
-  const [previousSavedUrl, setPreviousSavedUrl] = useState('');
-  const [storedValue, setStoredValue] = useState('');
-
+  const [previousSavedUrl, setPreviousSavedUrl] = useState('')
+  const [storedValue, setStoredValue] = useState('')
 
   useEffect(() => {
     if (typeof window === 'undefined') return
     setPreviousSavedUrl(window.location.href)
 
-    const storedVal = localStorage.getItem('previousUrl');
-    if (!storedVal) {
-      setStoredValue(previousSavedUrl);
-      localStorage.setItem('previousUrl', window.location.href);
-    }
-  }, [])
+    setStoredValue(previousSavedUrl)
+    localStorage.setItem('previousUrl', window.location.href)
+  }, [previousSavedUrl])
 
   return null
 }
