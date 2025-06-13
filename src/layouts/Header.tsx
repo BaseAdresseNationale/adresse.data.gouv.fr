db7fc8963143c347253bd4dde56bc450310c9dd1
Vincent Sagniez <vincent.sagniez@ign.fr>
2025-06-13 10:32:49 +0200

    modif diverses

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -100,7 +100,7 @@ export const navEntries: MainNavigationProps.Item[] = [
         text: 'Télécharger les données',
         linkProps: { href: '/outils/telechargements' },
       },
-         { text: 'Service géocodage de la Géoplateforme', linkProps: { href: '/outils/api-doc/adresse' } },
+      { text: 'Service géocodage de la Géoplateforme', linkProps: { href: '/outils/api-doc/adresse' } },
       { text: 'Géocodeur CSV', linkProps: { href: '/outils/csv' } },
       { text: 'Validateur BAL', linkProps: { href: '/outils/validateur-bal' } },
       { text:
@@ -110,7 +110,6 @@ export const navEntries: MainNavigationProps.Item[] = [
             <Badge noIcon severity="info">BETA</Badge>
           </>
         ), linkProps: { href: '/outils/mise-en-forme' } },
-      
       { text: 'Formulaire de publication', linkProps: { href: '/outils/formulaire-de-publication' } },
       {
         text: 'Supervision BAN/BAL',
