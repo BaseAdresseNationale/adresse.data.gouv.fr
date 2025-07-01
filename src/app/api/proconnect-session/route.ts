ac7b652b3bd7c21e2b9a91220de198451bffc16e
anatole <anatole.hornn@gmail.com>
2025-07-01 18:56:09 +0200

    update for test on https env and runtime var env.

--- src/app/api/proconnect-session/route.ts
+++ src/app/api/proconnect-session/route.ts
@@ -2,11 +2,11 @@ import { NextRequest, NextResponse } from 'next/server'
 import { env } from 'next-runtime-env'
 
 const BAN_API_TOKEN = env('BAN_API_TOKEN')
+const NEXT_PUBLIC_API_BAN_URL = env
 
 export async function POST(req: NextRequest) {
   const body = await req.json()
-
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BAN_URL}/api/session`, {
+  const response = await fetch(`${NEXT_PUBLIC_API_BAN_URL}/api/session`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
