b3674426358a8a91efb8490e426e2fb30bb44445
anatole <anatole.hornn@gmail.com>
2025-07-03 10:08:16 +0200

    fix add session.

--- src/app/api/proconnect-session/route.ts
+++ src/app/api/proconnect-session/route.ts
@@ -2,18 +2,24 @@ import { NextRequest, NextResponse } from 'next/server'
 import { env } from 'next-runtime-env'
 
 const BAN_API_TOKEN = env('BAN_API_TOKEN')
-const NEXT_PUBLIC_API_BAN_URL = env
+const NEXT_PUBLIC_API_BAN_URL = env('NEXT_PUBLIC_API_BAN_URL')
 
 export async function POST(req: NextRequest) {
-  const body = await req.json()
-  const response = await fetch(`${NEXT_PUBLIC_API_BAN_URL}/api/session`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Token ${BAN_API_TOKEN}`,
-    },
-    body: JSON.stringify(body),
-  })
-  const data = await response.json()
-  return NextResponse.json(data, { status: response.status })
+  try {
+    const body = await req.json()
+    const response = await fetch(`${NEXT_PUBLIC_API_BAN_URL}/api/session`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Token ${BAN_API_TOKEN}`,
+      },
+      body: JSON.stringify(body),
+    })
+    const data = await response.json()
+    return NextResponse.json(data, { status: response.status })
+  }
+  catch (error) {
+    console.error('Error in POST /api/session:', error)
+    return new Response(JSON.stringify({ error: 'Internal Server Error' }), { status: 500 })
+  }
 }
