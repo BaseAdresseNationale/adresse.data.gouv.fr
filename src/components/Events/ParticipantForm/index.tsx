e05fb66ec6b2564b83ea3a620d5b05897c902f64
Guillaume Fay <guillaume_fay@hotmail.com>
2025-07-09 11:56:12 +0200

    fix: inscription modal webinaire

--- src/components/Events/ParticipantForm/index.tsx
+++ src/components/Events/ParticipantForm/index.tsx
@@ -1,12 +1,9 @@
 import { FormEvent, useState } from 'react'
-
 import Input from '@codegouvfr/react-dsfr/Input'
 import Button from '@codegouvfr/react-dsfr/Button'
-import Alert from '@codegouvfr/react-dsfr/Alert'
 import { registrationToEvent } from '@/lib/api-bal-admin'
 import { StyledForm } from './ParticipantForm.styles'
 import { ParticipantType } from '@/types/events.types'
-import Notice from '@codegouvfr/react-dsfr/Notice'
 import Link from 'next/link'
 
 interface ParticipantFormProps {
@@ -34,9 +31,6 @@ function ParticipantForm({ onClose, eventId }: ParticipantFormProps) {
     try {
       await registrationToEvent(eventId, formData)
       setSubmitStatus('success')
-      setTimeout(() => {
-        onClose()
-      }, 2000)
     }
     catch {
       setSubmitStatus('error')
@@ -59,7 +53,7 @@ function ParticipantForm({ onClose, eventId }: ParticipantFormProps) {
         />
 
         <Input
-          label="Email*"
+          label="Email* (merci de bien vérifier votre adresse de courriel)"
           nativeInputProps={{
             required: true,
             type: 'email',
@@ -83,18 +77,10 @@ function ParticipantForm({ onClose, eventId }: ParticipantFormProps) {
         />
       </section>
       <section>
-        <span>
-          <i>
-            Les informations recueillies sur ce formulaire sont enregistrées dans un fichier informatisé par le Programme Base Adresse Locale de l&apos;ANCT pour vous envoyer l&apos;invitation au webinaire. Pour en savoir plus consultez&nbsp;
-          </i>
-          <Link href="/cgu" className="fr-link--icon-right fr-icon-links-line">nos CGU</Link>
-        </span>
-      </section>
-      <section>
         {submitStatus === 'success' && (
           <div className="fr-alert fr-alert--success">
             <p>
-              Votre inscription a bien été envoyée.
+              Votre inscription est validée. Vous allez recevoir un mail de confirmation. Le webinaire se déroulera à cette <a target="_blank" href="https://webinaire.numerique.gouv.fr/meeting/signin/invite/6605/creator/940/hash/615257ce88cf93ab6af594265d5729623aab8de3">adresse</a>.
             </p>
           </div>
         )}
@@ -106,18 +92,36 @@ function ParticipantForm({ onClose, eventId }: ParticipantFormProps) {
           </div>
         )}
       </section>
-      <div className="form-controls">
-        <Button
-          disabled={!canSubmit() || submitStatus === 'loading' || submitStatus === 'success'}
-          style={{ color: 'white' }}
-          type="submit"
-        >
-          S&apos;inscrire
-        </Button>
-        <Button type="button" priority="secondary" onClick={onClose}>
-          Annuler
-        </Button>
-      </div>
+      {submitStatus !== 'success'
+        ? (
+            <div className="form-controls">
+              <Button
+                disabled={!canSubmit() || submitStatus === 'loading'}
+                style={{ color: 'white' }}
+                type="submit"
+              >
+                S&apos;inscrire
+              </Button>
+              <Button type="button" priority="secondary" onClick={onClose}>
+                Annuler
+              </Button>
+            </div>
+          )
+        : (
+            <div className="form-controls">
+              <Button type="button" priority="secondary" onClick={onClose}>
+                Fermer
+              </Button>
+            </div>
+          )}
+      <section>
+        <span>
+          <i>
+            Les informations recueillies sur ce formulaire sont enregistrées dans un fichier informatisé par le Programme Base Adresse Locale de l&apos;ANCT pour vous envoyer l&apos;invitation au webinaire. Pour en savoir plus consultez&nbsp;
+          </i>
+          <Link href="/cgu" className="fr-link--icon-right fr-icon-links-line">nos CGU</Link>
+        </span>
+      </section>
     </StyledForm>
   )
 }
