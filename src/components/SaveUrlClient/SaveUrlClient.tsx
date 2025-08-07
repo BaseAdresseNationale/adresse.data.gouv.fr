3f853c3589eb5a10c99f9a4c4b287afcfdc471ad
anatole <anatole.hornn@gmail.com>
2025-08-07 09:24:41 +0200

    add render server client diff.

--- src/components/SaveUrlClient/SaveUrlClient.tsx
+++ src/components/SaveUrlClient/SaveUrlClient.tsx
@@ -5,8 +5,10 @@ import { useEffect, useState } from 'react'
 export default function SaveUrlClient() {
   const [previousSavedUrl, setPreviousSavedUrl] = useState('')
   const [storedValue, setStoredValue] = useState('')
+  const [hydrated, setHydrated] = useState<boolean>(false)
 
   useEffect(() => {
+    setHydrated(true)
     if (typeof window === 'undefined') return
 
     const currentUrl = window.location.href
