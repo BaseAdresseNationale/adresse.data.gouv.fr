334145384ffd8445dfd6e1b99b25c6da9f906852
Alice <alice.ainsa@ign.fr>
2025-11-17 12:04:14 +0100

    Mise en valeur du terme Blog sur le site adresse

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -155,11 +155,7 @@ export default async function Home() {
       </Section>
 
       <BlogGrid
-<<<<<<< HEAD
-        title="Le blog : Nos articles et témoignages"
-=======
         title="Le blog : articles et témoignages"
->>>>>>> 12610c65 (Mise en valeur du terme Blog sur le site adresse)
         posts={highlightedPosts}
         footer={(
           <Button
@@ -170,11 +166,7 @@ export default async function Home() {
             }}
             priority="primary"
           >
-<<<<<<< HEAD
-            Accéder au Blog
-=======
             Parcourir tout le blog
->>>>>>> 12610c65 (Mise en valeur du terme Blog sur le site adresse)
           </Button>
         )}
       />
