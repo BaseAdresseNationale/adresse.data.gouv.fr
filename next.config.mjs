9c5cee98725f57b1d77bb096dbbe5a77e5e356d6
anatole <anatole.hornn@gmail.com>
2025-12-01 16:50:51 +0100

    Update to NextJS 16 & React 19 (Step 1 - Update dependencies & config)

--- next.config.mjs
+++ next.config.mjs
@@ -17,7 +17,7 @@ const envVar = getNextEnv(process.env)
 const URL_CARTOGRAPHY_BAN = '/carte-base-adresse-nationale'
 const NEXT_PUBLIC_GHOST_URL_IMAGES_SOURCES
   = process.env.NEXT_PUBLIC_GHOST_URL_IMAGES_SOURCES
-const NEXT_PUBLIC_DOC_ADRESSE_URL = process.env.NEXT_PUBLIC_DOC_ADRESSE_URL 
+const NEXT_PUBLIC_DOC_ADRESSE_URL = process.env.NEXT_PUBLIC_DOC_ADRESSE_URL
 const NEXT_PUBLIC_ANNUAIRE_COLLECTIVITES_S3 = process.env.NEXT_PUBLIC_ANNUAIRE_COLLECTIVITES_S3
 const NEXT_PUBLIC_BAL_BLASONS_BUCKET_S3 = process.env.NEXT_PUBLIC_BAL_BLASONS_BUCKET_S3
 
@@ -251,7 +251,6 @@ const nextConfig = withBundleAnalyzer({
     serverActions: {
       bodySizeLimit: '3mb',
     },
-    instrumentationHook: true,
   },
   transpilePackages: [
     '@codegouvfr/react-dsfr', // Require for the "pages-router" of nextJS
