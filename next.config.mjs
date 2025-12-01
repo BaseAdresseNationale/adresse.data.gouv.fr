cc48254a45830b0039d29a2d110d90fe7bbd135a
anatole <anatole.hornn@gmail.com>
2025-12-01 17:49:35 +0100

    fix option not available.

--- next.config.mjs
+++ next.config.mjs
@@ -233,7 +233,6 @@ const nextConfig = withBundleAnalyzer({
     serverActions: {
       bodySizeLimit: '3mb',
     },
-    instrumentationHook: true,
   },
   transpilePackages: [
     '@codegouvfr/react-dsfr', // Require for the "pages-router" of nextJS
