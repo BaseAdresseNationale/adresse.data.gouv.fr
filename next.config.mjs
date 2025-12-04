91a00248d8fe06d51e81b584b3f8ad4e854e5332
anatole <anatole.hornn@gmail.com>
2025-12-04 14:30:47 +0100

    remove runtime edge form from next config.

--- next.config.mjs
+++ next.config.mjs
@@ -238,7 +238,7 @@ const nextConfig = withBundleAnalyzer({
     '@codegouvfr/react-dsfr', // Require for the "pages-router" of nextJS
   ],
   output: 'standalone',
-  runtime: 'edge',
+  // runtime: 'edge',
 })
 
 export default nextConfig
