d95bd0687a85d44126883fe940394d8706834205
Guillaume Fay <guillaume_fay@hotmail.com>
2026-02-19 15:19:55 +0100

    retours ariane

--- src/components/Commune/CommuneDownloadSection.tsx
+++ src/components/Commune/CommuneDownloadSection.tsx
@@ -87,21 +87,21 @@ export function CommuneDownloadSection({ commune, hasRevision }: CommuneDownload
         <Table
           caption="Délibérations"
           headers={[
-            'Voie',
-            'Date',
+            'Voie(s) concernée(s)',
+            'Date de la délibération',
             'Télécharger',
           ]}
           data={[
-            ['Square Sainte-Anne', '27/01/2025', <a className="fr-btn" key="square-sainte-anne" href="https://base-adresse-locale-static-files.s3.fr-par.scw.cloud/97410/27-01-2025_square-sainte-anne.pdf" download><span className="fr-icon-download-line" aria-hidden="true" /></a>],
-            ['Chemin Cécilia', '27/01/2025', <a className="fr-btn" key="chemin-cecilia" href="https://base-adresse-locale-static-files.s3.fr-par.scw.cloud/97410/27-01-2025_chemin-cecilia.pdf" download><span className="fr-icon-download-line" aria-hidden="true" /></a>],
+            ['Square Jean Cariou', '27/01/2025', <a className="fr-btn" key="square-sainte-anne" href="https://base-adresse-locale-static-files.s3.fr-par.scw.cloud/97410/27-01-2025_square-sainte-anne.pdf" download><span className="fr-icon-download-line" aria-hidden="true" /></a>],
+            [<ul key="chemin-cecilia"><li>Chemin Cécilia</li><li>Chemin des agriculteurs</li></ul>, '27/01/2025', <a className="fr-btn" key="chemin-cecilia" href="https://base-adresse-locale-static-files.s3.fr-par.scw.cloud/97410/27-01-2025_chemin-cecilia.pdf" download><span className="fr-icon-download-line" aria-hidden="true" /></a>],
             ['Chemin Koilou', '19/06/2025', <a className="fr-btn" key="chemin-koilou" href="https://base-adresse-locale-static-files.s3.fr-par.scw.cloud/97410/19-06-2025_chemin-koilou.pdf" download><span className="fr-icon-download-line" aria-hidden="true" /></a>],
             ['Rue Daubenberger', '01/12/2025', <a className="fr-btn" key="rue-daubenberger" href="https://base-adresse-locale-static-files.s3.fr-par.scw.cloud/97410/01-12-2025_rue-daubenberger.pdf" download><span className="fr-icon-download-line" aria-hidden="true" /></a>],
-            ['Rue du Saro', '21/03/2024', <a className="fr-btn" key="rue-du-saro" href="https://base-adresse-locale-static-files.s3.fr-par.scw.cloud/97410/21-03-2024_rue-du-saro.pdf" download><span className="fr-icon-download-line" aria-hidden="true" /></a>],
-            ['Rue Gapy', '21/03/2024', <a className="fr-btn" key="rue-gapy" href="https://base-adresse-locale-static-files.s3.fr-par.scw.cloud/97410/21-03-2024_rue-gapy.pdf" download><span className="fr-icon-download-line" aria-hidden="true" /></a>],
+            [<ul key="rue-du-saro"><li>Rue du Saro</li><li>Impasse Ravintsara</li></ul>, '21/03/2024', <a className="fr-btn" key="rue-du-saro" href="https://base-adresse-locale-static-files.s3.fr-par.scw.cloud/97410/21-03-2024_rue-du-saro.pdf" download><span className="fr-icon-download-line" aria-hidden="true" /></a>],
+            [<ul key="rue-gapy"><li>Rue Gapy</li><li>Rue Dano</li></ul>, '21/03/2024', <a className="fr-btn" key="rue-gapy" href="https://base-adresse-locale-static-files.s3.fr-par.scw.cloud/97410/21-03-2024_rue-gapy.pdf" download><span className="fr-icon-download-line" aria-hidden="true" /></a>],
             ['Impasse Raphaël Bouyer', '04/09/2024', <a className="fr-btn" key="impasse-raphael-bouyer" href="https://base-adresse-locale-static-files.s3.fr-par.scw.cloud/97410/04-09-2024_impasse-raphael-bouyer.pdf" download><span className="fr-icon-download-line" aria-hidden="true" /></a>],
-            ['Rue du Rucher', '27/09/2023', <a className="fr-btn" key="rue-du-rucher" href="https://base-adresse-locale-static-files.s3.fr-par.scw.cloud/97410/27-09-2023_rue-du-rucher.pdf" download><span className="fr-icon-download-line" aria-hidden="true" /></a>],
-            ['Impasse Bilimbli', '30/09/2023', <a className="fr-btn" key="impasse-bilimbli" href="https://base-adresse-locale-static-files.s3.fr-par.scw.cloud/97410/30-09-2023_impasse-bilimbli.pdf" download><span className="fr-icon-download-line" aria-hidden="true" /></a>],
-            ['Impasse Papangue', '27/01/2022', <a className="fr-btn" key="impasse-papangue" href="https://base-adresse-locale-static-files.s3.fr-par.scw.cloud/97410/27-01-2022_impasse-papangue.pdf" download><span className="fr-icon-download-line" aria-hidden="true" /></a>],
+            [<ul key="rue-du-rucher"><li>Rue du Rucher</li><li>Rue des Abeilles</li></ul>, '27/09/2023', <a className="fr-btn" key="rue-du-rucher" href="https://base-adresse-locale-static-files.s3.fr-par.scw.cloud/97410/27-09-2023_rue-du-rucher.pdf" download><span className="fr-icon-download-line" aria-hidden="true" /></a>],
+            [<ul key="impasse-bilimbli"><li>Impasse Bilimbli</li><li>Rue des Conflores</li><li>Rue des Kalbass</li><li>Rue des Ananas</li></ul>, '30/09/2023', <a className="fr-btn" key="impasse-bilimbli" href="https://base-adresse-locale-static-files.s3.fr-par.scw.cloud/97410/30-09-2022_impasse-bilimbli.pdf" download><span className="fr-icon-download-line" aria-hidden="true" /></a>],
+            [<ul key="impasse-papangue"><li>Impasse Papangue</li><li>Rue de la Liberté</li></ul>, '27/01/2022', <a className="fr-btn" key="impasse-papangue" href="https://base-adresse-locale-static-files.s3.fr-par.scw.cloud/97410/21-05-2022_impasse-papangue.pdf" download><span className="fr-icon-download-line" aria-hidden="true" /></a>],
           ]}
         />
       )}
