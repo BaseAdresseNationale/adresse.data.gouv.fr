eda3ce9d307867d228bb85ecdd44ebb76fca957e
Jugurtha Bouhadoun <jugurtha.bouhadoun@ign.fr>
2026-02-26 18:07:13 +0100

    fix(MapBreadcrumb): autoriser le retour à la ligne pour les noms de voies longs

--- src/app/carte-base-adresse-nationale/components/MapBreadcrumb/MapBreadcrumb.tsx
+++ src/app/carte-base-adresse-nationale/components/MapBreadcrumb/MapBreadcrumb.tsx
@@ -26,9 +26,25 @@ function MapBreadcrumb({ path = [] }: MapBreadcrumbProps) {
             <ol className="fr-breadcrumb__list">
               {path.map((segment, index) => {
                 if (typeof segment === 'string') {
+                  // élément dernier
+                  const isLastSegment = index === path.length - 1
+                  // le segment est un numéro
+                  const isAddressNumberSegment = segment.startsWith('Numéro ')
+                  // élément précédent
+                  const previousSegment = index > 0 ? path[index - 1] : undefined
+                  // Vérifie si c'est un élément cliquable permet d'identifier que c'est une voie et non un nom  de commune
+                  const followsClickableSegment = previousSegment && typeof previousSegment !== 'string'
+
                   return (
                     <li key={index}>
-                      <MapBreadcrumbEntry>{segment}</MapBreadcrumbEntry>
+                      {/* Applique le style de retour à la ligne ($allowWrap) UNIQUEMENT si :
+                          1. C'est le dernier élément
+                          2. Il suit un lien cliquable (donc c'est une Voie ou un Numéro)
+                          3. Ce N'EST PAS un numéro (on veut garder les numéros sur une seule ligne)
+                      */}
+                      <MapBreadcrumbEntry $allowWrap={Boolean(isLastSegment && followsClickableSegment && !isAddressNumberSegment)}>
+                        {segment}
+                      </MapBreadcrumbEntry>
                     </li>
                   )
                 }
