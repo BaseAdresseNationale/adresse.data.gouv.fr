ad044cfa815be39b855262d56dc3083e0bcf3aa5
Jugurtha Bouhadoun <jugurtha.bouhadoun@ign.fr>
2026-03-12 12:00:51 +0100

    check du noyaux

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -4,6 +4,8 @@ import { env } from 'next-runtime-env'
 
 // This function is called once when the application starts
 export async function register() {
+  if (process.env.NEXT_RUNTIME !== 'nodejs') return
+
   await downloadContoursCommunes()
   // Only download newsletters in production
   // to avoid slowing down the development environment
