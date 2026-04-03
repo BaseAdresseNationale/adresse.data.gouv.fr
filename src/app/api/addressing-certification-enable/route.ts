5ae319797aa31384a4a0c48740568d598e7ad13b
vinsag <vincent.sagniez@ign.fr>
2026-04-03 12:10:41 +0200

    Fix : add async await required

--- src/app/api/addressing-certification-enable/route.ts
+++ src/app/api/addressing-certification-enable/route.ts
@@ -37,7 +37,7 @@ export async function POST(request: NextRequest) {
       return NextResponse.json({ error: 'Server misconfigured' }, { status: 500 })
     }
 
-    const cookieStore = cookies()
+    const cookieStore = await cookies()
     const userinfo = cookieStore.get('userinfo')
 
     if (!userinfo) {
