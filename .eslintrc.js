0f97ea7ea80a287c3889a4fe94ffeb90850e2ff0
Nicolas KOKLA <n.kokla@koaji.fr>
2026-04-09 02:44:48 +0200

    Remove storybook and attached config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   extends: [
     'plugin:@stylistic/recommended-extends',
     'next/core-web-vitals',
-    'plugin:storybook/recommended',
   ],
   rules: {
     '@stylistic/jsx-one-expression-per-line': 'off',
