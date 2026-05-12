a562ae605c063e9ecc192cbff9113dc53509703e
MelFonk <melvin.pichenot@ign.fr>
2026-05-12 19:26:41 +0200

    Adaptation de la logique de l'affichage des logos à la réponse de l'api

--- src/lib/api-blasons-communes.ts
+++ src/lib/api-blasons-communes.ts
@@ -3,20 +3,20 @@ import { env } from 'next-runtime-env'
 const BAL_BLASON_BUCKET_URL
   = `https://${env('NEXT_PUBLIC_BAL_BLASONS_BUCKET_S3') || 'base-adresse-locale-prod-blasons-communes.s3.fr-par.scw.cloud'}`
 
-const isWikimediaUrl = (url: string): boolean =>
-  /wikimedia|wikidata|wikipedia\.org/i.test(url)
-
 export const getCommuneFlagProxy = async (codeCommune: string): Promise<string> => {
   const base = env('NEXT_PUBLIC_API_ANNUAIRE_DES_COLLECTIVITES') || 'https://api.collectivite.fr/api'
   if (base) {
     try {
       const response = await fetch(`${base}/commune/logo/${codeCommune}`, { cache: 'force-cache' })
-      const url = await response.text()
-      const isValidUrl = Boolean(
-        url && (url.startsWith('http') || url.startsWith('data:image')),
-      )
-      if (response.ok && isValidUrl && !isWikimediaUrl(url)) {
-        return url
+      const url = await response.json()
+      const { logo: urlLogo, blason: urlBlason } = url
+      // On récupère le logo de la commune
+      if (response.ok && urlLogo) {
+        return urlLogo
+      }
+      // Sinon le blason de la commune
+      if (response.ok && urlBlason) {
+        return urlBlason
       }
     }
     catch {
